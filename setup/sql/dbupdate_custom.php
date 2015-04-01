5b08c23b4f80c5331def11382727ff8f7319b6e6
Richard Klees <richard.klees@concepts-and-training.de>
2015-04-01 13:06:35 +0000

    spx: removed superfluous udf plugin.
    git-svn-id: http://svn.ilias.de/svn/ilias/branches/CaT/releases/4_4_seepex@58627 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- setup/sql/dbupdate_custom.php
+++ setup/sql/dbupdate_custom.php
@@ -1960,11 +1960,7 @@ ilCustomInstaller::activatePlugin(IL_COMP_SERVICE, "EventHandling", "evhk", "GEV
 
 <#52>
 <?php
-// init helper class
-require_once "Customizing/class.ilCustomInstaller.php";
 
-ilCustomInstaller::initPluginEnv();
-ilCustomInstaller::activatePlugin(IL_COMP_SERVICE, "User", "udfc", "GEVUserData");
 ?>
 
 
