0769bdc3a86e41ba09d88bf59c05c36a67bb6562
Richard Klees <richard.klees@concepts-and-training.de>
2015-04-08 10:04:09 +0000

    spx: superiors need to get noticed
    git-svn-id: http://svn.ilias.de/svn/ilias/branches/CaT/releases/4_4_seepex@58727 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/GEV/Mailing/classes/CrsMails/class.gevSuperiorCancelWaitingToCancelledWithoutCosts.php
+++ Services/GEV/Mailing/classes/CrsMails/class.gevSuperiorCancelWaitingToCancelledWithoutCosts.php
@@ -24,7 +24,7 @@ class gevSuperiorCancelWaitingToCancelledWithoutCosts extends gevCrsAutoMail {
 	}
 	
 	public function getCC($a_recipient) {
-		return array();
+		return $this->maybeSuperiorsCC($a_recipient);
 	}
 	
 	public function getMail($a_recipient) {
