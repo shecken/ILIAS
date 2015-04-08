8e7965f6d75d125d3df47584fb6a9aa29e833e79
Richard Klees <richard.klees@concepts-and-training.de>
2015-04-08 12:37:00 +0000

    spx: mails. finally.
    git-svn-id: http://svn.ilias.de/svn/ilias/branches/CaT/releases/4_4_seepex@58738 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/GEV/Mailing/classes/CrsMails/class.gevTrainerTrainingCancelled.php
+++ Services/GEV/Mailing/classes/CrsMails/class.gevTrainerTrainingCancelled.php
@@ -2,37 +2,30 @@
 
 require_once("Services/GEV/Mailing/classes/class.gevCrsAutoMail.php");
 
-class gevWaitingListCancelled extends gevCrsAutoMail {
+class gevParticipantTrainingCancelled extends gevCrsAutoMail {
 	public function getTitle() {
 		return "Info Participant";
 	}
 	
 	public function _getDescription() {
-		return "Waiting List is cancelled automatically.";
+		return "Training gets cancelled by Admin";
 	}
+	
 	
 	public function getScheduledFor() {
 		return null;
 	}
 	
 	public function getTemplateCategory() {
-		return "C07";
+		return "C09";
 	}
 	
 	public function getRecipientUserIDs() {
-		return $this->getCourseCancelledWithoutCostsMembers();
+		return $this->getCourseTrainers();
 	}
 	
 	public function getCC($a_recipient) {
-		return array();
-	}
-	
-	public function getMail($a_recipient) {
-		if ($this->getAdditionalMailSettings()->getSuppressMails()) {
-			return null;
-		}
-		
-		return parent::getMail($a_recipient);
+		return $this->maybeSuperiorsCC($a_recipient);
 	}
 }
 
