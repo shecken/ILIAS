8b8d2101cdf4d672a9823941e769faada7b70cdd
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2015-08-26 09:29:03 +0000

    spx:const added in settings
    git-svn-id: http://svn.ilias.de/svn/ilias/branches/CaT/releases/4_4_seepex@60518 21b2c9ec-7c21-0410-8b45-9bfb8ed2bfc5

--- Services/GEV/Utils/classes/class.gevSettings.php
+++ Services/GEV/Utils/classes/class.gevSettings.php
@@ -114,6 +114,14 @@ class gevSettings {
 	
 	// Organisationseinheit TEP
 	const CRS_AMD_TEP_ORGU			= "crs_amd_tep_orgu";
+
+	// Crs User PState
+	const CRS_URS_STATE_SUCCESS			= "erfolgreich";
+	const CRS_URS_STATE_SUCCESS_VAL		= "2";
+	const CRS_URS_STATE_EXCUSED			= "entschuldigt";
+	const CRS_URS_STATE_EXCUSED_VAL		= "3";
+	const CRS_URS_STATE_NOT_EXCUSED		= "unentschuldigt";
+	const CRS_URS_STATE_NOT_EXCUSED_VAL	= "4";
 	
 	
 	// Typen von Organisationseinheiten
