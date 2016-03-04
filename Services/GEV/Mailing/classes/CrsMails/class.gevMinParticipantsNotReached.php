355f2e40210ea64e226579638e3141f8dc372d88
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2016-03-04 09:58:02 +0100

    spx: #2195 change schedule of mail to 10 days

--- Services/GEV/Mailing/classes/CrsMails/class.gevMinParticipantsNotReached.php
+++ Services/GEV/Mailing/classes/CrsMails/class.gevMinParticipantsNotReached.php
@@ -3,7 +3,7 @@
 require_once("Services/GEV/Mailing/classes/class.gevCrsAutoMail.php");
 
 class gevMinParticipantsNotReached extends gevCrsAutoMail {
-	const DAYS_BEFORE_COURSE_START = 31;
+	const DAYS_BEFORE_COURSE_START = 11;
 	
 	public function getTitle() {
 		return "Info Admin";
