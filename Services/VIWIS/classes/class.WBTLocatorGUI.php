70c4c9f6d41f29dff8a6ecdfd096f9b0f1eb019a
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2016-09-30 13:37:00 +0200

    wbdlocatorgui operational

--- Services/VIWIS/classes/class.WBTLocatorGUI.php
+++ Services/VIWIS/classes/class.WBTLocatorGUI.php
@@ -1,5 +1,25 @@
 <?php
-
+require_once 'Services/VIWIS/exceptions/class.WBTLocatorException.php';
+require_once 'Services/VIWIS/classes/class.WBTLocator.php';
 class WBTLocatorGUI {
-	
+
+	public function __construct() {
+		global $ilDB, $ilCtrl;
+		$this->ctrl = $ilCtrl;
+		$this->locator = new WBTLocator($ilDB);
+	}
+
+	public function executeCommand() {
+		$cmd = $this->ctrl->getCmd();
+		switch($cmd) {
+			case 'redirect_viwis':
+				$redirect_params = $this->locator->getRedirectParameterForQuestionRef($_GET['q_ref']);
+				$redirect = "ilias.php?baseClass=ilSAHSPresentationGUI"
+					."&ref_id=".$redirect_params['ref_id']
+					.'&redirect_viwis='.$redirect_params['wbt_item'];
+				ilUtil::redirect($redirect);
+			default:
+				throw new WBTLocatorException('unknown command: '.$cmd);
+		}
+	}
 }
