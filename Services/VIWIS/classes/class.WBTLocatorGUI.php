8fbd344d862305940edbd7045d20b8eecfc7e8be
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2016-09-30 14:02:38 +0200

    send warning if no training found to qid

--- Services/VIWIS/classes/class.WBTLocatorGUI.php
+++ Services/VIWIS/classes/class.WBTLocatorGUI.php
@@ -1,12 +1,16 @@
 <?php
 require_once 'Services/VIWIS/exceptions/class.WBTLocatorException.php';
 require_once 'Services/VIWIS/classes/class.WBTLocator.php';
+/**
+ * Redirect to the appropriate WBT.
+ */
 class WBTLocatorGUI {
 
 	public function __construct() {
-		global $ilDB, $ilCtrl;
+		global $ilDB, $ilCtrl, $lng;
 		$this->ctrl = $ilCtrl;
 		$this->locator = new WBTLocator($ilDB);
+		$this->lng = $lng;
 	}
 
 	public function executeCommand() {
@@ -14,10 +18,15 @@ class WBTLocatorGUI {
 		switch($cmd) {
 			case 'redirect_viwis':
 				$redirect_params = $this->locator->getRedirectParameterForQuestionRef($_GET['q_ref']);
-				$redirect = "ilias.php?baseClass=ilSAHSPresentationGUI"
-					."&ref_id=".$redirect_params['ref_id']
-					.'&redirect_viwis='.$redirect_params['wbt_item'];
-				ilUtil::redirect($redirect);
+				if($redirect_params !== null) {
+					$redirect = "ilias.php?baseClass=ilSAHSPresentationGUI"
+						."&ref_id=".$redirect_params['ref_id']
+						.'&redirect_viwis='.$redirect_params['wbt_item'];
+					ilUtil::redirect($redirect);
+				} else {
+					ilUtil::sendInfo(sprintf($this->lng->txt('no_wbt_found_for_ref'),$_GET['q_ref']));
+				}
+				break;
 			default:
 				throw new WBTLocatorException('unknown command: '.$cmd);
 		}
