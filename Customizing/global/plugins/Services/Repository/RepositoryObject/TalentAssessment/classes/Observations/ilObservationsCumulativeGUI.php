487c79053c3507f7f3c46ba0a983970aabdc187c
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2016-10-10 13:34:37 +0200

    remove typo and move check to earlier position

--- Customizing/global/plugins/Services/Repository/RepositoryObject/TalentAssessment/classes/Observations/ilObservationsCumulativeGUI.php
+++ Customizing/global/plugins/Services/Repository/RepositoryObject/TalentAssessment/classes/Observations/ilObservationsCumulativeGUI.php
@@ -19,16 +19,17 @@ class ilObservationsCumulativeGUI {
 		$actions = $this->parent_obj->getActions();
 
 		$observator = $actions->getAssignedUser($obj_id);
+
+		if(empty($observator)) {
+			\ilUtil::sendInfo($this->parent_obj->txt("no_observator_no_cumulative", false));
+			return "";
+		}
+
 		$obs = $actions->getObservationsCumulative($obj_id);
 		$req_res = $actions->getRequestresultCumulative(array_keys($obs));
 		$col_span = count($observator);
 
 		$tpl = new \ilTemplate("tpl.talent_assessment_observations_cumulative.html", true, true, "Customizing/global/plugins/Services/Repository/RepositoryObject/TalentAssessment");
-
-		if(empty($observator)) {
-			\ilUtil::sendInfo($this->parent_obj->txt("no_observator_no_cumulative", flase));
-			return "";
-		}
 
 		foreach($obs as $key => $title) {
 			$tpl->setCurrentBlock("observations");
