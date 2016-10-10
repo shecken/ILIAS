23aea339e2598425840fad3aeced32e2ccde0c6d
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2016-10-10 11:04:03 +0200

    code refactoring and streamlining

--- Customizing/global/plugins/Services/EventHandling/EventHook/TestrunHistorizingAppEventListener/classes/class.TestrunHistorizingDataProvider.php
+++ Customizing/global/plugins/Services/EventHandling/EventHook/TestrunHistorizingAppEventListener/classes/class.TestrunHistorizingDataProvider.php
@@ -2,10 +2,17 @@
 require_once 'Services/HistorizingStorage/interfaces/interface.DataProvider.php';
 class TestrunHistorizingDataProvider implements DataProvider {
 
-	protected $data = null;
-	protected $case = null;
-	protected $plugin;
+	const LAST_PASS = 'last_pass';
+	const BEST_PASS = 'best_pass';
 
+	protected $plugin;
+	protected $component;
+	protected $event;
+
+	protected $case = null;#
+
+	private $test = null;
+	private $obj_id = null;
 
 	public function __construct($a_component, $a_event, array $a_parameter, ilEventHookPlugin $plugin) {
 		if(!$plugin->eventRelevant($a_component, $a_event, $a_parameter)) {
@@ -21,7 +28,7 @@ class TestrunHistorizingDataProvider implements DataProvider {
 		if($this->case === null) {
 			$this->case = $this->calculateCase($this->component, $this->event, $this->parameter);
 		}
-		return $this->case_id;
+		return $this->case;
 	}
 
 	public function data() {
@@ -43,48 +50,73 @@ class TestrunHistorizingDataProvider implements DataProvider {
 		return false;
 	}
 
-	private function calculateData($a_component, $a_event, array $a_parameter) {
+	private function calculateData() {
 		global $ilLog;
 		if($this->component === 'Services/Object' && $this->event === 'update') {
 			return array('test_title' => ilObject::_lookupTitle($a_parameter['obj_id']));
 		} elseif($this->component === 'Modules/Test' && $this->event === 'testPassIncreased') {
-			require_once 'Modules/Test/classes/class.ilObjTest.php';
-			$test_session = $a_parameter['test_session'];
-			$obj_id = ilObjTest::_getObjectIDFromTestID($test_session->getTestId());
-			$test = new ilObjTest($obj_id, false);
-			$test_results = $test->getTestResult($test_session->getActiveId())['pass'];
 
-			$ilLog->dump($test_results);
+			$test_session = $this->parameter['test_session'];
+			$test = $this->getTestInstanceByObjId($this->getObjIdFromTestSession($test_session));
+			$test_result = $test->getTestResult($test_session->getActiveId());
+			$ilLog->write(' ptp '.$this->getPercentToPass($test_session));
 			return array(
 				'test_title'			=> $test->getTitle()
-				,'max_points'			=> $test_results['total_max_points']
-				,'points_achived'		=> $test_results['total_reached_points']
-				,'points_to_pass'		=> $this->getPointsToPass()
+				,'max_points'			=> $test_result['pass']['total_max_points']
+				,'points_achieved'		=> $test_result['pass']['total_reached_points']
+				,'percent_to_pass'		=> $this->getPercentToPass($test_session)
 				,'testrun_finished_at'	=> $test_session->getSubmittedTimestamp()
-				,'testrun_passed'		=> $test_results['passed']
-				,'pass_schema'			=> 'test'
+				,'passed'				=> $test_result['test']['passed']
+				,'pass_scoring'			=> $this->getPassScoring($test)
 				);
 		}
 	}
 
-	private function getPointsToPass($test_session) {
+	private function getObjIdFromTestSession(ilTestSession $test_session) {
+		if($this->obj_id === null) {
+			require_once 'Modules/Test/classes/class.ilObjTest.php';
+			$this->obj_id = ilObjTest::_getObjectIDFromTestID($test_session->getTestId());
+		}
+		return $this->obj_id;
+	}
+
+	private function getTestInstanceByObjId($obj_id) {
+		if($this->test === null) {
+			require_once 'Modules/Test/classes/class.ilObjTest.php';
+			$this->test = new ilObjTest($obj_id, false);
+		}
+		return $this->test;
+	}
+
+	private function getPassScoring(ilObjTest $test) {
+		$t_pass_scoring = $test->getPassScoring();
+		if($t_pass_scoring === SCORE_LAST_PASS) {
+			return self::LAST_PASS;
+		} elseif ($t_pass_scoring === SCORE_BEST_PASS) {
+			return self::BEST_PASS;
+		}
+		throw new ilHistorizingException('unknown pass scoring');
+	}
+
+	private function getPercentToPass(ilTestSession $test_session) {
 		$db = $this->plugin->getDbInstance();
-		$sql = 'SELECT MIN(minimum_level)/100 as pass_threshold FROM tst_mark'
+		$sql = 'SELECT MIN(minimum_level)/100 as points_to_pass FROM tst_mark'
 				.' 	WHERE test_fi = '.$db->quote($test_session->getTestId(),'integer')
 				.'	AND passed = '.$db->quote(1,'integer')
 				.'	GROUP BY test_fi';
-		return $db->fetchAssoc($db->query($sql))['pass_threshold'];
+		return $db->fetchAssoc($db->query($sql))['points_to_pass'];
 	}
 
 	private function calculateCase($a_component, $a_event, array $a_parameter) {
 		if($this->component === 'Services/Object' && $this->event === 'update') {
 			return array('obj_id' => $a_parameter['obj_id']);
 		} elseif($this->component === 'Modules/Test' && $this->event === 'testPassIncreased') {
-			$test_session = $a_parameter['test_session'];
-			$obj_id = ilObjTest::_getObjectIDFromTestID($test_session->getTestId());
-			$usr_id = $test_session->getUserId();
-			$pass = $test_session->getPass();
-			return array('usr_id' => $usr_id, 'obj_id' => $obj_id, 'pass' => $pass);
+			$test_session = $this->parameter['test_session'];
+			return array(
+				'usr_id' => $test_session->getUserId()
+				,'obj_id' => $this->getObjIdFromTestSession($test_session)
+				,'pass' => $test_session->getPass()
+				);
 		}
 	}
 }
