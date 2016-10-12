a9b3f83a9805c6f6f688271f60c8e695fb719805
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2016-10-12 15:10:47 +0200

    space for exam bio created

--- Customizing/global/plugins/Services/Repository/RepositoryObject/ReportExamBio/classes/class.ilObjReportExamBio.php
+++ Customizing/global/plugins/Services/Repository/RepositoryObject/ReportExamBio/classes/class.ilObjReportExamBio.php
@@ -24,4 +24,72 @@ class ilObjReportExamBio extends ilObjReportBase {
 	public function initType() {
 		$this->setType("xexb");
 	}
+
+	public function prepareTable(catSelectableReportTableGUI $table) {
+		$table	->defineFieldColumn($this->plugin->txt("code"), 'code', array('code' => $this->fields['c']['coupon_code']))
+				->defineFieldColumn($this->plugin->txt("start"), 'start', array('start' => $this->fields['c2']['coupon_value']))
+				->defineFieldColumn($this->plugin->txt("diff"), 'diff', array('diff' => $this->tf->diffFieldsSql('diff',$this->fields['c2']['coupon_value'],$this->fields['c']['coupon_value'])))
+				->defineFieldColumn($this->plugin->txt("current"), 'current', array( 'current' => $this->fields['c']['coupon_value']))
+				->defineFieldColumn($this->plugin->txt("expires"),'expires',array('expires' => $this->fields['c']['coupon_expires']),true);
+		if($this->settings['admin_mode']) {
+			$table
+				->defineFieldColumn($this->plugin->txt("name"), 'name', array('lastname' => $this->fields['hu']['lastname']
+																			, 'firstname' => $this->fields['hu']['firstname']),true)
+				->defineFieldColumn($this->plugin->txt("odbd"), 'odbd', array('above1' => $this->tf->groupConcatFieldSql('above1', $this->fields['huo']['org_unit_above1'],';;')
+																			, 'above2' => $this->tf->groupConcatFieldSql('above2', $this->fields['huo']['org_unit_above2'],';;')),true,false)
+				->defineFieldColumn($this->plugin->txt("orgu"), 'orgu', array('orgu' => $this->tf->groupConcatFieldSql('orgu', $this->fields['huo']['orgu_title'])),true);
+		}
+		$this->space = $table->prepareTableAndSetRelevantFields($this->space);
+		return $table;
+	}
+
+	public function initSpace() {
+		$aux = $this->tf->histUsertestrun('recent_pass_aux');
+		$aux = $aux->addConstraint($aux->field('hist_historic')->EQ()->int(0));
+
+		$recent_pass_case = $this->tf->derivedTable('recent_pass_case'
+			,$this->tf->TableSpace()
+				->addTablePrimary($aux)
+				->request($aux->field('usr_id'))
+				->request($aux->field('obj_id'))
+				->request($this->tf->maxSql('recent_pass',$aux->field('pass')))
+				->groupBy($aux->field('usr_id'))
+				->groupBy($aux->field('obj_id')));
+
+		$all_pass = $this->tf->histUsertestrun('all_pass');
+		$all_pass	= $all_pass->addConstraint($all_pass->field('hist_historic')->EQ()->int(0));
+
+		$recent_pass_data = $this->tf->histUsertestrun('recent_pass_data');
+		$recent_pass_data = $recent_pass_data->addConstraint($recent_pass_data->field('hist_historic')->EQ()->int(0));
+
+		$usr = $this->tf->hist_user('usr');
+		$usr = $usr->addConstraint($usr->field('hist_historic')->EQ()->int(0));
+
+		$orgus = $this->tf->allOrgusOfUsers('orgu_all',$users);
+
+		$this->space = $this->tf->TableSpace()
+			->addTablePrimary($recent_pass_case)
+			->addTableSecondary($recent_pass_data)
+			->addTableSecondary($all_pass)
+			->addTableSecondary($orgus)
+			->addTableSecondary($usr)
+			->setRootTable($recent_pass_case)
+			->addDependency($this->tf->TableJoin($recent_pass_case,$all_passes,
+				$recent_pass_cases->field('usr_id')->EQ($all_passes->field('usr_id'))
+					->_AND($recent_passes->field('obj_id')->EQ($all_passes->field('obj_id'))))
+				)
+			->addDependency($this->tf->TableJoin($recent_pass_case,$usr,
+				$recent_pass_cases->field('usr_id')->EQ($usr->field('user_id')))
+				)
+			->addDependency($this->tf->TableJoin($recent_pass_case,$orgu,
+				$recent_pass_cases->field('usr_id')->EQ($orgu->field('usr_id')))
+				)
+			->addDependency($this->tf->TableJoin($recent_pass_case,$recent_pass_data,
+				$recent_pass_cases->field('usr_id')->EQ($all_passes->field('usr_id'))
+					->_AND($recent_pass_case->field('obj_id')->EQ($recent_pass_data->field('obj_id')))
+					->_AND($recent_pass_case->field('pass')->EQ($recent_pass_data->field('pass'))))
+				)
+			->groupBy($recent_pass_case->field('usr_id'))
+			->groupBy($recent_pass_case->field('obj_id'));
+	}
 }
