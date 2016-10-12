5d5bdb287b4ccc09d6df527e7378894e3aea93eb
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2016-10-12 13:05:26 +0200

    exam bio wip

--- Customizing/global/plugins/Services/Repository/RepositoryObject/ReportExamBio/classes/class.ilObjReportExamBio.php
+++ Customizing/global/plugins/Services/Repository/RepositoryObject/ReportExamBio/classes/class.ilObjReportExamBio.php
@@ -5,4 +5,23 @@ use CaT\TableRelations as TableRelations;
 use CaT\Filter as Filters;
 
 class ilObjReportExamBio extends ilObjReportBase {
+
+	public function __construct($a_ref_id = 0) {
+		global $ilUser;
+		$this->gUser = $ilUser;
+		parent::__construct($a_ref_id);
+		$this->gf = new TableRelations\GraphFactory();
+		$this->pf = new Filters\PredicateFactory();
+		$this->tf = new TableRelations\TableFactory($this->pf, $this->gf);
+
+	}
+
+	protected function createLocalReportSettings() {
+		$this->local_report_settings =
+			$this->s_f->reportSettings('rep_robj_rcpn');
+	}
+
+	public function initType() {
+		$this->setType("xexb");
+	}
 }
