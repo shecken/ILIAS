8633b7fd6fa68e5cfe094c94eb104bd6dc4d0146
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2016-10-21 10:06:42 +0200

    use regexp to test query elements

--- CaT/test/TableRelations/Tables/SqlQueryInterpreterTest.php
+++ CaT/test/TableRelations/Tables/SqlQueryInterpreterTest.php
@@ -24,20 +24,24 @@ class SqlQueryInterpreterTest extends PHPUnit_Framework_TestCase {
 
 
 	public function test_field() {
+
 		$i = $this->i;
 		$f1 = $this->field('f1');
 		$f2 = $this->field('f2');
 		$f3 = $this->field('f3');
+		$tf = $this->tf;
 		$table = $this->table('a')->addField($f1)->addField($f2)->addField($f3);
-		$this->assertEquals(' a.f1',$i->_interpretField($table->field('f1')));
-		$this->assertEquals(' a.f1 + a.f2',$i->_interpretField($this->tf->plus('plus',$table->field('f1'),$table->field('f2'))));
-		$this->assertEquals(' a.f1 / a.f2',$i->_interpretField($this->tf->quot('quot',$table->field('f1'),$table->field('f2'))));
-		$this->assertEquals(' SUM( a.f1)',$i->_interpretField($this->tf->sum('sum',$table->field('f1'))));
-		$this->assertEquals(' COUNT(*)',$i->_interpretField($this->tf->countAll('bal')));
-		$this->assertEquals(' MAX( a.f1)',$i->_interpretField($this->tf->max('max',$table->field('f1'))));
-		$this->assertEquals(' MIN( a.f1)',$i->_interpretField($this->tf->min('min',$table->field('f1'))));
-		$this->assertEquals(' MIN( a.f1)',$i->_interpretField($this->tf->min('foo',$table->field('f1'))));
-		$this->assertEquals(' SUM( MIN( a.f1) / MAX( a.f2))',
+		$this->assertRegExp('#\\s*a\\.f1\\s*#',$i->_interpretField($table->field('f1')));
+		$this->assertRegExp('#\\s*a\\.f1\\s*\\+\\s*a\\.f2\\s*#',$i->_interpretField($tf->plus('plus',$table->field('f1'),$table->field('f2'))));
+		$this->assertRegExp('#\\s*a\\.f1\\s*/\\s*a\\.f2\\s*#',$i->_interpretField($tf->quot('quot',$table->field('f1'),$table->field('f2'))));
+		$this->assertRegExp('#\\s*a\\.f1\\s*\\-\\s*a\\.f2\\s*#',$i->_interpretField($tf->minus('quot',$table->field('f1'),$table->field('f2'))));
+		$this->assertRegExp('#\\s*a\\.f1\\s*\\*\\s*a\\.f2\\s*#',$i->_interpretField($tf->times('quot',$table->field('f1'),$table->field('f2'))));
+		$this->assertRegExp('#\\s*SUM\\(\\s*a\\.f1\\s*\\)\\s*#',$i->_interpretField($tf->sum('sum',$table->field('f1'))));
+		$this->assertRegExp('#\\s*COUNT\\(\\s*\\*\\s*\\)#',$i->_interpretField($tf->countAll('bal')));
+		$this->assertRegExp('#\\s*MAX\\(\\s*a\\.f1\\s*\\)\\s*#',$i->_interpretField($tf->max('max',$table->field('f1'))));
+		$this->assertRegExp('#\\s*MIN\\(\\s*a\\.f1\\s*\\)\\s*#',$i->_interpretField($tf->min('min',$table->field('f1'))));
+		$this->assertRegExp('#\\s*MIN\\(\\s*a\\.f1\\s*\\)\\s*#',$i->_interpretField($tf->min('foo',$table->field('f1'))));
+		$this->assertRegExp('#\\s*SUM\\(\\s*MIN\\(\\s*a\\.f1\\s*\\)\\s*/\\s*MAX\\(\\s*a\\.f2\\s*\\)\\s*\\)\\s*#',
 			$i->_interpretField(
 				$this->tf->sum('sum',
 					$this->tf->quot('quot',$this->tf->min('min',$table->field('f1')),
@@ -45,12 +49,20 @@ class SqlQueryInterpreterTest extends PHPUnit_Framework_TestCase {
 	}
 
 	public function test_table() {
+		$v = '[\\n\\r\\s]';
 		$i = $this->i;
+		$tf = $this->tf;
 		$f1 = $this->field('f1');
 		$f2 = $this->field('f2');
 		$f3 = $this->field('f3');
 		$table = $this->table('a')->addField($f1)->addField($f2)->addField($f3);
-		$this->assertEquals($i->_interpretTable($table),'foo AS a');
+		$this->assertRegExp('#\\s*foo\\s+AS\\s+a\\s*#',$i->_interpretTable($table));
+		$space = $tf->TableSpace()->addTablePrimary($table)->setRootTable($table);
+		$space->request($tf->times('times',$f1,$f2));
+		$derived = $tf->DerivedTable($space,'derived');
+		$this->assertRegExp(
+			str_replace(PHP_EOL, ' ', '#\\s*\\(SELECT\\s+a\\.f1\\s*\\*\\s*a\\.f2\\s+AS\\s+times[\\s\\r\\n]+FROM\\s+foo\\s+AS\\s+a\\s*\\)\\s*AS\\s+derived\\s*#')
+			,$i->_interpretTable($derived));
 	}
 }
 
