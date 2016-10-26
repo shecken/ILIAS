f84e544f967e257179967207240d7b0c45419a44
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2016-10-26 10:31:52 +0200

    gev2: 2562 only if download is required

--- Services/PDFGeneration/classes/class.ilTCPDFGenerator.php
+++ Services/PDFGeneration/classes/class.ilTCPDFGenerator.php
@@ -49,9 +49,13 @@ class ilTCPDFGenerator
 			$pdf->AddPage();
 			$pdf->writeHTML($page, true, false, true, false, '');			
 		}
-		//gev-patch 2559 do not show pre loader on download
-		ilUtil::setCookie("download_started", "started", false, false, false);
-		//gev-patch end
+
+		if($job->getOutputMode() == ilTestPDFGenerator::PDF_OUTPUT_DOWNLOAD) {
+			//gev-patch 2559 do not show pre loader on download
+			ilUtil::setCookie("download_started", "started", false, false, false);
+			//gev-patch end
+		}
+
 		$result = $pdf->Output($job->getFilename(), $job->getOutputMode() ); // (I - Inline, D - Download, F - File)
 	}
 }
