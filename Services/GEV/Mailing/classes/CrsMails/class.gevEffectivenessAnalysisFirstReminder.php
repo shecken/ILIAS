b336d73e48041e0397f54056d6fbf5ac2f7a1477
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2016-11-10 16:02:56 +0100

    spx: 2603 add getMail

--- Services/GEV/Mailing/classes/CrsMails/class.gevEffectivenessAnalysisFirstReminder.php
+++ Services/GEV/Mailing/classes/CrsMails/class.gevEffectivenessAnalysisFirstReminder.php
@@ -24,15 +24,42 @@ class gevEffectivenessAnalysisFirstReminder extends gevCrsAutoMail {
 	}
 	
 	public function getCC($a_recipient) {
-		$training_officer = $this->getTrainingOfficerMail();
+		$training_officer_name = $this->getTrainingOfficerName();
+		$training_officer_mail = $this->getTrainingOfficerMail();
 
-		if($training_officer) {
-			return $training_officer;
+		if($training_officer_mail) {
+			if(!$training_officer_name) {
+				$training_officer_name = $training_officer_mail;
+			}
+
+			return array($training_officer_name." <".$training_officer_mail.">");
 		}
 		return array();
 	}
 
 	protected function log($a_mail, $a_occasion) {
+	}
+
+	protected function getMessage($a_template_id, $a_recipient) {
+		$message = $this->getMessageFromTemplate($a_template_id
+												, $a_recipient
+												, $this->getFullnameForTemplate($a_recipient)
+												, $this->getEmailForTemplate($a_recipient));
+
+		return array( "from" => $this->getFrom()
+					, "to" => $this->getTo($a_recipient)
+					, "cc" => $this->getCC($a_recipient)
+					, "bcc" => $this->getBCC($a_recipient)
+					, "subject" => $message["subject"]?$message["subject"]:""
+					, "message_plain" => str_replace("<br />", "\n", $message["plain"])
+					, "message_html" => $message["html"]
+					, "attachments" => $this->getAttachmentsForMail($a_recipient)
+					, "frame_plain" => $this->template_frame->getPlainTextFrame()
+					, "frame_html" => $this->template_frame->getHtmlFrame()
+					, "image_path" => $this->template_frame->getFileSystemBasePath()."/"
+									  .$this->template_frame->getImageName()
+					, "image_styles" => $this->template_frame->getImageStyles()
+					);
 	}
 }
 
