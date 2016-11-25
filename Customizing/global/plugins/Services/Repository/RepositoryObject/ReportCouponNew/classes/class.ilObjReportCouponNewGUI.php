c934ef023ecb5cbd64ccf8cb7df72c1815fc6e91
daniwe4 <daniel.weise@concepts-and-training.de>
2016-11-25 10:35:03 +0100

    add column -created- to ReportCouponNew

--- Customizing/global/plugins/Services/Repository/RepositoryObject/ReportCouponNew/classes/class.ilObjReportCouponNewGUI.php
+++ Customizing/global/plugins/Services/Repository/RepositoryObject/ReportCouponNew/classes/class.ilObjReportCouponNewGUI.php
@@ -328,6 +328,7 @@ class ilObjReportCouponNewGUI extends ilObjectPluginGUI {
 		$rec['start'] = number_format($rec['start'],2,',','');
 		$rec['current'] = number_format($rec['current'],2,',','');
 		$rec['diff'] = number_format($rec['diff'],2,',','');
+		$rec['created'] = date('d.m.Y',(int)$rec['created']);
 		$rec['expires'] = date('d.m.Y',(int)$rec['expires']);
 
 
