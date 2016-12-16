9e8b8582f2af01146a2a78bdb2009cceadbee2b7
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2016-12-16 14:16:33 +0100

    WIP

--- Customizing/global/plugins/Services/Repository/RepositoryObject/ReportOrguAtt/classes/class.ilObjReportOrguAtt.php
+++ Customizing/global/plugins/Services/Repository/RepositoryObject/ReportOrguAtt/classes/class.ilObjReportOrguAtt.php
@@ -7,60 +7,68 @@ require_once("Modules/OrgUnit/classes/class.ilObjOrgUnit.php");
 require_once("Services/GEV/Utils/classes/class.gevObjectUtils.php");
 require_once("Services/GEV/Utils/classes/class.gevOrgUnitUtils.php");
 
-ini_set("memory_limit","2048M"); 
+ini_set("memory_limit", "2048M");
 ini_set('max_execution_time', 0);
 set_time_limit(0);
 
-class ilObjReportOrguAtt extends ilObjReportBase {
+class ilObjReportOrguAtt extends ilObjReportBase
+{
 	protected $relevant_parameters = array();
 	protected $sum_parts = array();
 
-	public function __construct($ref_id = 0) {
+	public function __construct($ref_id = 0)
+	{
 		parent::__construct($ref_id);
 
 		require_once $this->plugin->getDirectory().'/config/cfg.att_org_units.php';
 	}
 
-	public function initType() {
+	public function initType()
+	{
 		 $this->setType("xroa");
 	}
 
 
-	protected function createLocalReportSettings() {
+	protected function createLocalReportSettings()
+	{
 		$this->local_report_settings =
 			$this->s_f->reportSettings('rep_robj_roa')
 				->addSetting($this->s_f
-							->settingBool('is_local',$this->plugin->txt('report_is_local')))
+							->settingBool('is_local', $this->plugin->txt('report_is_local')))
 				->addSetting($this->s_f
-							->settingBool('all_orgus_filter',$this->plugin->txt('report_all_orgus')));
+							->settingBool('all_orgus_filter', $this->plugin->txt('report_all_orgus')));
 	}
 
 
-	public function prepareReport() {
+	public function prepareReport()
+	{
 		$this->sum_table = $this->buildSumTable(catReportTable::create());
 		parent::prepareReport();
 	}
 
-	protected function buildSumTable(catReportTable $table) {
+	protected function buildSumTable(catReportTable $table)
+	{
 		foreach ($this->sum_parts as $title => $query) {
 			$table
-				->column($title,$this->plugin->txt($title),true);
+				->column($title, $this->plugin->txt($title), true);
 		}
 		$table	->template("tpl.gev_attendance_by_orgunit_sums_row.html", $this->plugin->getDirectory());
 		return $table;
 	}
 
-	public function deliverSumTable() {
-		if($this->sum_table !== null ) {
+	public function deliverSumTable()
+	{
+		if ($this->sum_table !== null) {
 			return $this->sum_table;
 		}
-		throw new Exception("ilObjReportBase::deliverSumTable: you need to define a sum table.");	
+		throw new Exception("ilObjReportBase::deliverSumTable: you need to define a sum table.");
 	}
 
 	/**
 	 * @inheritdoc
 	 */
-	protected function buildOrder($order) {
+	protected function buildOrder($order)
+	{
 		return $order
 			->defaultOrder("orgu_title", "ASC");
 	}
@@ -68,24 +76,27 @@ class ilObjReportOrguAtt extends ilObjReportBase {
 	/**
 	 * @inheritdoc
 	 */
-	protected function buildTable($table) {
-		$table	->column("orgu_title", $this->plugin->txt('orgu_title'),true)
-				->column("odbd", $this->plugin->txt('od_bd'),true, "",false,false);
+	protected function buildTable($table)
+	{
+		$table	->column("orgu_title", $this->plugin->txt('orgu_title'), true)
+				->column("odbd", $this->plugin->txt('od_bd'), true, "", false, false);
 		foreach ($this->sum_parts as $title => $query) {
 			$table
-				->column($title, $this->plugin->txt($title),true);
+				->column($title, $this->plugin->txt($title), true);
 		}
 		return parent::buildTable($table);
 	}
 
-	protected function getRowTemplateTitle() {
+	protected function getRowTemplateTitle()
+	{
 		return "tpl.gev_attendance_by_orgunit_row.html";
 	}
 
 	/**
 	 * @inheritdoc
 	 */
-	protected function buildQuery($query) {
+	protected function buildQuery($query)
+	{
 		$query	->select("orgu.orgu_title")
 				->select("orgu.org_unit_above1")
 				->select("orgu.org_unit_above2");
@@ -101,9 +112,9 @@ class ilObjReportOrguAtt extends ilObjReportBase {
 					->on('usr.user_id = orgu.usr_id')
 				->left_join("hist_usercoursestatus usrcrs")
 					->on("usrcrs.usr_id = orgu.usr_id AND usrcrs.hist_historic = 0 "
-						."	AND usrcrs.booking_status != ".$this->gIldb->quote('-empty-','text')
-						."	AND (usrcrs.begin_date <= ".$this->gIldb->quote($this->date_end,'date')
-						."		AND (usrcrs.end_date >= ".$this->gIldb->quote($this->date_start,'date')
+						."	AND usrcrs.booking_status != ".$this->gIldb->quote('-empty-', 'text')
+						."	AND (usrcrs.begin_date <= ".$this->gIldb->quote($this->date_end, 'date')
+						."		AND (usrcrs.end_date >= ".$this->gIldb->quote($this->date_start, 'date')
 						."			OR `usrcrs`.`end_date` = '0000-00-00' OR `usrcrs`.`end_date` = '-empty-'))"
 						."	".($no_wbd_imported ? ' AND usrcrs.crs_id > 0' : ''))
 				->left_join("hist_course crs")
@@ -114,15 +125,54 @@ class ilObjReportOrguAtt extends ilObjReportBase {
 		return $query;
 	}
 
-	protected function deliverSumQuery() {
+	protected function buildQueryStatement()
+	{
+		$query =	'SELECT orgu.orgu_title '.PHP_EOL
+					.'	,orgu.org_unit_above_1'.PHP_EOL
+					.'	,orgu.org_unit_above2'.PHP_EOL;
+		foreach ($this->sum_parts as $title => $query_term) {
+			$query .= '	,'.$query_term["regular"].PHP_EOL;
+		}
+		$query .= 	'	FROM hist_userorgu.orgu'.PHP_EOL
+					.'	JOIN hist_user usr'.PHP_EOL
+					.'		ON usr.user_id = orgu.usr_id'.PHP_EOL
+					.'	LEFT JOIN hist_usercoursestatus usrcrs'.PHP_EOL
+					.'		ON usrcrs.usr_id = orgu.usr_id AND usrcrs.hist_historic = 0'.PHP_EOL
+					.'			AND usrcrs.booking_status != '.$this->gIldb->quote('-empty-', 'text').PHP_EOL
+					.'			AND '.$this->datePeriodFilter()
+					.'			AND '.$this->noWBDImportedFilter()
+					.'	LEFT JOIN hist_course crs'.PHP_EOL
+					.'		ON crs.crs_id = usrcrs.crs_id AND crs.hist_historic = 0'.PHP_EOL
+					.'			AND '.$this->tpl_filter
+					.'	WHERE orgu.hist_historic = 0 AND orgu.action >= 0'.PHP_EOL
+					.'	GROUP BY orgu.orgu_id';
+	}
+
+	private function datePeriodFilter($query)
+	{
+		return $query;
+	}
+
+	private function newWBDImportedFilter($query)
+	{
+		return $query;
+	}
+
+	private function possiblyJoinCourseRelatedFilters($query)
+	{
+		return $query;
+	}
+
+	protected function deliverSumQuery()
+	{
 		$sum_terms = array();
 		foreach ($this->sum_parts as $title => $query_term) {
 			$sum_terms[] = $query_term["sum"];
 		}
 		$no_wbd_imported = $this->filter->get('no_wbd_imported');
-		$sum_sql = 
+		$sum_sql =
 		"SELECT  "
-		."	".implode(', ',$sum_terms)
+		."	".implode(', ', $sum_terms)
 		." 	FROM("
 		."		SELECT DISTINCT orgu.usr_id, crs.crs_id, usrcrs.booking_status, "
 		."			usrcrs.participation_status, crs.type "
@@ -131,153 +181,80 @@ class ilObjReportOrguAtt extends ilObjReportBase {
 		."				ON orgu.usr_id = usr.user_id"
 		."			LEFT JOIN `hist_usercoursestatus` usrcrs "
 		."				ON usrcrs.usr_id = orgu.usr_id AND usrcrs.hist_historic = 0 "
-		."					AND usrcrs.booking_status != ".$this->gIldb->quote('-empty-','text')
-		."					AND (usrcrs.begin_date <= ".$this->gIldb->quote($this->date_end,'date')
-		."						AND (usrcrs.end_date >= ".$this->gIldb->quote($this->date_start,'date')
+		."					AND usrcrs.booking_status != ".$this->gIldb->quote('-empty-', 'text')
+		."					AND (usrcrs.begin_date <= ".$this->gIldb->quote($this->date_end, 'date')
+		."						AND (usrcrs.end_date >= ".$this->gIldb->quote($this->date_start, 'date')
 		."							OR `usrcrs`.`end_date` = '0000-00-00' OR `usrcrs`.`end_date` = '-empty-'))"
 		."					".($no_wbd_imported ? ' AND usrcrs.crs_id > 0' : '')
 		."			LEFT JOIN `hist_course` crs "
 		."				ON usrcrs.crs_id = crs.crs_id AND crs.hist_historic = 0 "
 		."					AND ".$this->tpl_filter;
 		$topics = $this->filter->get('crs_topics');
-		if(count($topics) > 0) {
+		if (count($topics) > 0) {
 			$sum_sql .=
 			"			JOIN hist_topicset2topic ts2t ON crs.topic_set = ts2t.topic_set_id"
 			."			JOIN hist_topics ts ON ts2t.topic_id = ts.topic_id "
 			."				AND ".$this->gIldb->in("ts.topic_title", $topics, false, 'text');
-		}	
+		}
 		$sum_sql .=
 		"			".$this->queryWhere()
 		.") as temp";
 		return $sum_sql;
 	}
 
-	public function insertSumData($table, callable $callback) {
+	public function insertSumData($table, callable $callback)
+	{
 		$res = $this->gIldb->query($this->deliverSumQuery());
 		$summed_data = $this->gIldb->fetchAssoc($res);
 
-		if(count($summed_data) == 0) {
+		if (count($summed_data) == 0) {
 			$summed_data = array();
-			foreach($this->sum_parts as $name => $query) {
+			foreach ($this->sum_parts as $name => $query) {
 				$summed_data[$name] = 0;
 			}
 		}
-		$table->setData(array(call_user_func($callback,$summed_data)));
+		$table->setData(array(call_user_func($callback, $summed_data)));
 		return $table;
 	}
 
 	/**
 	 * @inheritdoc
 	 */
-	protected function buildFilter($filter) {
+	protected function buildFilter($filter)
+	{
 		$this->orgu_filter = new recursiveOrguFilter('org_unit', 'orgu.orgu_id', true, true);
-		if("1" === (string)$this->settings['all_orgus_filter']) {
+		if ("1" === (string)$this->settings['all_orgus_filter']) {
 			$this->orgu_filter->setFilterOptionsAll();
 		} else {
 			$this->orgu_filter->setFilterOptionsByArray(
-				array_unique(array_map(function($ref_id) {return ilObject::_lookupObjectId($ref_id);},
-										$this->user_utils->getOrgUnitsWhereUserCanViewEduBios())));
+				array_unique(array_map(
+					function ($ref_id) {
+						return ilObject::_lookupObjectId($ref_id);
+					},
+					$this->user_utils->getOrgUnitsWhereUserCanViewEduBios()
+				))
+			);
 		}
-		$this->crs_topics_filter = new courseTopicsFilter('crs_topics','crs.topic_set');
+		$this->crs_topics_filter = new courseTopicsFilter('crs_topics', 'crs.topic_set');
 		$this->orgu_filter->addToFilter($filter);
 		$this->crs_topics_filter->addToFilter($filter);
-		$filter	->dateperiod( "period"
-							, $this->plugin->txt("period")
-							, $this->plugin->txt("until")
-							, "usrcrs.begin_date"
-							, "usrcrs.end_date"
-							, date("Y")."-01-01"
-							, date("Y")."-12-31"
-							, false
-							," OR TRUE"
-							)
-				->multiselect("edu_program"
-							 , $this->plugin->txt("edu_program")
-							 , "edu_program"
-							 , gevCourseUtils::getEduProgramsFromHisto()
-							 , array()
-							 , ""
-							 , 200
-							 , 160	
-							 )
-				->multiselect("type"
-							 , $this->plugin->txt("course_type")
-							 , "type"
-							 , gevCourseUtils::getLearningTypesFromHisto()
-							 , array()
-							 , ""
-							 , 200
-							 , 160	
-							 )
-				->multiselect("template_title"
-							 , $this->plugin->txt("crs_title")
-							 , "template_title"
-							 , gevCourseUtils::getTemplateTitleFromHisto()
-							 , array()
-							 , ""
-							 , 300
-							 , 160	
-							 )
-				->multiselect("participation_status"
-							 , $this->plugin->txt("participation_status")
-							 , "participation_status"
-							 , array(	"teilgenommen"=>"teilgenommen"
+		$filter	->dateperiod("period", $this->plugin->txt("period"), $this->plugin->txt("until"), "usrcrs.begin_date", "usrcrs.end_date", date("Y")."-01-01", date("Y")."-12-31", false, " OR TRUE")
+				->multiselect("edu_program", $this->plugin->txt("edu_program"), "edu_program", gevCourseUtils::getEduProgramsFromHisto(), array(), "", 200, 160)
+				->multiselect("type", $this->plugin->txt("course_type"), "type", gevCourseUtils::getLearningTypesFromHisto(), array(), "", 200, 160)
+				->multiselect("template_title", $this->plugin->txt("crs_title"), "template_title", gevCourseUtils::getTemplateTitleFromHisto(), array(), "", 300, 160)
+				->multiselect("participation_status", $this->plugin->txt("participation_status"), "participation_status", array(	"teilgenommen"=>"teilgenommen"
 							 			,"fehlt ohne Absage"=>"fehlt ohne Absage"
 							 			,"fehlt entschuldigt"=>"fehlt entschuldigt"
-							 			,"nicht gesetzt"=>"gebucht, noch nicht abgeschlossen")
-							 , array()
-							 , ""
-							 , 200
-							 , 160
-							 , "text"
-							 , "asc"
-							 , true
-							 )
-				->multiselect("booking_status"
-							 , $this->plugin->txt("booking_status")
-							 , "booking_status"
-							 , catFilter::getDistinctValues('booking_status', 'hist_usercoursestatus')
-							 , array()
-							 , ""
-							 , 200
-							 , 160	
-							 )
-				->multiselect("gender"
-							 , $this->plugin->txt("gender")
-							 , "gender"
-							 , array('f', 'm')
-							 , array()
-							 , ""
-							 , 100
-							 , 160	
-							 )
-				->multiselect("venue"
-							 , $this->plugin->txt("venue")
-							 , "venue"
-							 , catFilter::getDistinctValues('venue', 'hist_course')
-							 , array()
-							 , ""
-							 , 300
-							 , 160	
-							 )
-				->multiselect("provider"
-							 , $this->plugin->txt("provider")
-							 , "provider"
-							 , catFilter::getDistinctValues('provider', 'hist_course')
-							 , array()
-							 , ""
-							 , 300
-							 , 160	
-							 )
-				->checkbox('no_wbd_imported'
-							, $this->plugin->txt("filter_no_wbd_imported")
-							," TRUE "
-							," TRUE "
-							);
-			if("1" !== (string)$this->options['all_orgus_filter']) {
-				$filter
-				->static_condition($this->gIldb->in("orgu.usr_id", $this->user_utils->getEmployeesWhereUserCanViewEduBios(), false, "integer"));
-			}
+							 			,"nicht gesetzt"=>"gebucht, noch nicht abgeschlossen"), array(), "", 200, 160, "text", "asc", true)
+				->multiselect("booking_status", $this->plugin->txt("booking_status"), "booking_status", catFilter::getDistinctValues('booking_status', 'hist_usercoursestatus'), array(), "", 200, 160)
+				->multiselect("gender", $this->plugin->txt("gender"), "gender", array('f', 'm'), array(), "", 100, 160)
+				->multiselect("venue", $this->plugin->txt("venue"), "venue", catFilter::getDistinctValues('venue', 'hist_course'), array(), "", 300, 160)
+				->multiselect("provider", $this->plugin->txt("provider"), "provider", catFilter::getDistinctValues('provider', 'hist_course'), array(), "", 300, 160)
+				->checkbox('no_wbd_imported', $this->plugin->txt("filter_no_wbd_imported"), " TRUE ", " TRUE ");
+		if ("1" !== (string)$this->options['all_orgus_filter']) {
+			$filter
+			->static_condition($this->gIldb->in("orgu.usr_id", $this->user_utils->getEmployeesWhereUserCanViewEduBios(), false, "integer"));
+		}
 			$filter
 				->static_condition('usr.hist_historic = 0')
 				->static_condition("orgu.hist_historic = 0")
@@ -288,32 +265,39 @@ class ilObjReportOrguAtt extends ilObjReportBase {
 		$date_filter = $filter->get("period");
 		$this->date_start = $date_filter["start"]->get(IL_CAL_DATE);
 		$this->date_end = $date_filter["end"]->get(IL_CAL_DATE);
-		$this->tpl_filter 
+		$this->tpl_filter
 			= (int)$this->settings['is_local'] === 1
-				? $this->gIldb->in('crs.template_obj_id',$this->getSubtreeCourseTemplates(),false,'integer')
+				? $this->gIldb->in('crs.template_obj_id', $this->getSubtreeCourseTemplates(), false, 'integer')
 				: "TRUE" ;
 		return $filter;
 	}
 
 
-	protected function getSubtreeCourseTemplates() {
+	protected function getSubtreeCourseTemplates()
+	{
 		$query = 	'SELECT obj_id FROM adv_md_values_text amd_val '
-					.'	WHERE '.$this->gIldb->in('obj_id',
-							$this->getSubtreeTypeIdsBelowParentType('crs','cat'),false,'integer')
+					.'	WHERE '.$this->gIldb->in(
+						'obj_id',
+						$this->getSubtreeTypeIdsBelowParentType('crs', 'cat'),
+						false,
+						'integer'
+					)
 					.'		AND field_id = '.$this->gIldb->quote(
-												gevSettings::getInstance()
-													->getAMDFieldId(gevSettings::CRS_AMD_IS_TEMPLATE)
-												,'integer')
-					.'		AND value = '.$this->gIldb->quote('Ja','text');
+						gevSettings::getInstance()
+													->getAMDFieldId(gevSettings::CRS_AMD_IS_TEMPLATE),
+						'integer'
+					)
+					.'		AND value = '.$this->gIldb->quote('Ja', 'text');
 		$return = array();
 		$res = $this->gIldb->query($query);
-		while($rec = $this->gIldb->fetchAssoc($res)) {
+		while ($rec = $this->gIldb->fetchAssoc($res)) {
 			$return[] = $rec['obj_id'];
 		}
 		return $return;
 	}
 
-	public function getRelevantParameters() {
+	public function getRelevantParameters()
+	{
 		return $this->relevant_parameters;
 	}
 }
--- Customizing/global/plugins/Services/Repository/RepositoryObject/ReportOrguAtt/classes/class.ilObjReportOrguAttGUI.php
+++ Customizing/global/plugins/Services/Repository/RepositoryObject/ReportOrguAtt/classes/class.ilObjReportOrguAttGUI.php
@@ -8,20 +8,43 @@ require_once 'Customizing/global/plugins/Services/Cron/CronHook/ReportMaster/cla
 * @ilCtrl_Calls ilObjReportOrguAttGUI: ilPermissionGUI, ilInfoScreenGUI, ilObjectCopyGUI, gevDBVReportGUI
 * @ilCtrl_Calls ilObjReportOrguAttGUI: ilCommonActionDispatcherGUI
 */
-class ilObjReportOrguAttGUI extends ilObjReportBaseGUI {
-	static $od_regexp = null;
-	static $bd_regexp = null;
-	public function getType() {
+class ilObjReportOrguAttGUI extends ilObjReportBaseGUI
+{
+
+	public static $od_regexp = null;
+	public static $bd_regexp = null;
+
+	public function getType()
+	{
 		return 'xroa';
 	}
 
-	protected function prepareTitle($a_title) {
+	protected function afterConstructor()
+	{
+		parent::afterConstructor();
+		if ($this->object->plugin) {
+			$this->tpl->addCSS($this->object->plugin->getStylesheetLocation('report.css'));
+		}
+
+		if ($this->object) {
+			$this->filter = $this->object->filter();
+			$this->display = new \CaT\Filter\DisplayFilter(
+				new \CaT\Filter\FilterGUIFactory,
+				new \CaT\Filter\TypeFactory
+			);
+		}
+		$this->loadFilterSettings();
+	}
+
+	protected function prepareTitle($a_title)
+	{
 		$a_title = parent::prepareTitle($a_title);
 		$a_title->image("GEV_img/ico-head-edubio.png");
 		return $a_title;
 	}
 
-	protected function render() {
+	protected function render()
+	{
 		$this->gTpl->setTitle(null);
 		return 	($this->title !== null ? $this->title->render() : "")
 				. ($this->object->deliverFilter() !== null ? $this->object->deliverFilter()->render() : "")
@@ -32,14 +55,15 @@ class ilObjReportOrguAttGUI extends ilObjReportBaseGUI {
 	}
 
 
-	private function renderSumTable(){
+	private function renderSumTable()
+	{
 		$table = new catTableGUI($this, "showContent");
 		$table->setEnableTitle(false);
 		$table->setTopCommands(false);
 		$table->setEnableHeader(true);
 		$sum_table = $this->object->deliverSumTable();
 		$table->setRowTemplate(
-			$sum_table->row_template_filename, 
+			$sum_table->row_template_filename,
 			$sum_table->row_template_module
 		);
 
@@ -48,13 +72,10 @@ class ilObjReportOrguAttGUI extends ilObjReportBaseGUI {
 		$table->setLimit($cnt);
 		$table->setMaxCount($cnt);
 		foreach ($sum_table->columns as $col) {
-			$table->addColumn( $col[2] ? $col[1] : $this->object->plugin->txt($col[1])
-							 , $col[0]
-							 , $col[3]
-							 );
+			$table->addColumn($col[2] ? $col[1] : $this->object->plugin->txt($col[1]), $col[0], $col[3]);
 		}
 		$callback = get_class($this).'::transformResultRow';
-		$table = $this->object->insertSumData($table,$callback);
+		$table = $this->object->insertSumData($table, $callback);
 
 		$this->enableRelevantParametersCtrl();
 		$return = $table->getHtml();
@@ -62,26 +83,28 @@ class ilObjReportOrguAttGUI extends ilObjReportBaseGUI {
 		return $return;
 	}
 
-	protected function getRowTemplateTitle() {
+	protected function getRowTemplateTitle()
+	{
 		return "tpl.gev_attendance_by_orgunit_row.html";
 	}
 
-	public static function transformResultRow($rec) {
+	public static function transformResultRow($rec)
+	{
 
-		foreach($rec as &$data) {
-			if((string)$data === "0") {
+		foreach ($rec as &$data) {
+			if ((string)$data === "0") {
 				$data = '-';
 			}
 		}
-		if(isset($rec['org_unit_above1'])) {
-			if(!self::$od_regexp || !self::$bd_regexp ) {
+		if (isset($rec['org_unit_above1'])) {
+			if (!self::$od_regexp || !self::$bd_regexp) {
 				require_once './Customizing/global/plugins/Services/Cron/CronHook/ReportMaster/config/od_bd_strings.php';
 			}
 			$orgu_above1 =  $rec['org_unit_above1'];
 			$orgu_above2 =  $rec['org_unit_above2'];
 			if (preg_match(self::$od_regexp, $orgu_above1)) {
 				$od = $orgu_above1;
-			} elseif(preg_match(self::$od_regexp, $orgu_above2)) {
+			} elseif (preg_match(self::$od_regexp, $orgu_above2)) {
 				$od = $orgu_above2;
 			} else {
 				$od = '-';
@@ -89,7 +112,7 @@ class ilObjReportOrguAttGUI extends ilObjReportBaseGUI {
 
 			if (preg_match(self::$bd_regexp, $orgu_above1)) {
 				$bd = $orgu_above1;
-			} elseif(preg_match(self::$bd_regexp, $orgu_above2)) {
+			} elseif (preg_match(self::$bd_regexp, $orgu_above2)) {
 				$bd = $orgu_above2;
 			} else {
 				$bd = '-';
@@ -99,21 +122,22 @@ class ilObjReportOrguAttGUI extends ilObjReportBaseGUI {
 		return parent::transformResultRow($rec);
 	}
 
-	public static function transformResultRowXLSX($rec) {
-		foreach($rec as &$data) {
-			if((string)$data === "0") {
+	public static function transformResultRowXLSX($rec)
+	{
+		foreach ($rec as &$data) {
+			if ((string)$data === "0") {
 				$data = '-';
 			}
 		}
-		if(isset($rec['org_unit_above1'])) {
-			if(!self::$od_regexp || !self::$bd_regexp ) {
+		if (isset($rec['org_unit_above1'])) {
+			if (!self::$od_regexp || !self::$bd_regexp) {
 				require_once './Customizing/global/plugins/Services/Cron/CronHook/ReportMaster/config/od_bd_strings.php';
 			}
 			$orgu_above1 =  $rec['org_unit_above1'];
 			$orgu_above2 =  $rec['org_unit_above2'];
 			if (preg_match(self::$od_regexp, $orgu_above1)) {
 				$od = $orgu_above1;
-			} elseif(preg_match(self::$od_regexp, $orgu_above2)) {
+			} elseif (preg_match(self::$od_regexp, $orgu_above2)) {
 				$od = $orgu_above2;
 			} else {
 				$od = '-';
@@ -121,7 +145,7 @@ class ilObjReportOrguAttGUI extends ilObjReportBaseGUI {
 
 			if (preg_match(self::$bd_regexp, $orgu_above1)) {
 				$bd = $orgu_above1;
-			} elseif(preg_match(self::$bd_regexp, $orgu_above2)) {
+			} elseif (preg_match(self::$bd_regexp, $orgu_above2)) {
 				$bd = $orgu_above2;
 			} else {
 				$bd = '-';
