4505d9fbcb010a92d32c04142dddc3a0e87eca40
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2016-12-21 15:12:16 +0100

    wip

--- Customizing/global/plugins/Services/Repository/RepositoryObject/ReportTrDemandRet/classes/class.ilObjReportTrDemandRet.php
+++ Customizing/global/plugins/Services/Repository/RepositoryObject/ReportTrDemandRet/classes/class.ilObjReportTrDemandRet.php
@@ -2,41 +2,47 @@
 
 require_once 'Customizing/global/plugins/Services/Cron/CronHook/ReportMaster/classes/ReportBase/class.ilObjReportBase.php';
 
-ini_set("memory_limit","2048M"); 
+ini_set("memory_limit", "2048M");
 ini_set('max_execution_time', 0);
 set_time_limit(0);
 
-class ilObjReportTrDemandRet extends ilObjReportBase {
+class ilObjReportTrDemandRet extends ilObjReportBase
+{
 	protected $relevant_parameters = array();
 	protected $is_local;
 
-	public function initType() {
+	public function initType()
+	{
 		$this->setType("xtdr");
 	}
 
-	protected function getRowTemplateTitle() {
+	protected function getRowTemplateTitle()
+	{
 		return "tpl.gev_tr_demand_ret_row.html";
 	}
 
-	public function getRelevantParameters() {
+	public function getRelevantParameters()
+	{
 		return $this->relevant_parameters;
 	}
 
-	protected function buildOrder($order) {
+	protected function buildOrder($order)
+	{
 		return $order
 					->defaultOrder("tpl_title", "ASC")
-					->mapping("tpl_title",array("tpl_title","title","begin_date"));
+					->mapping("tpl_title", array("tpl_title","title","begin_date"));
 	}
 
-	protected function createLocalReportSettings() {
+	protected function createLocalReportSettings()
+	{
 		$this->local_report_settings =
 			$this->s_f->reportSettings('rep_robj_rtdr')
 				->addSetting($this->s_f
-								->settingBool('is_local', $this->plugin->txt('report_is_local'))
-								);
+								->settingBool('is_local', $this->plugin->txt('report_is_local')));
 	}
 
-	protected function buildTable($table) {
+	protected function buildTable($table)
+	{
 		$table	->column('tpl_title', $this->plugin->txt('tpl_title'), true)
 				->column('title', $this->plugin->txt('crs_title'), true)
 				->column('type', $this->plugin->txt('crs_type'), true)
@@ -52,8 +58,11 @@ class ilObjReportTrDemandRet extends ilObjReportBase {
 		return parent::buildTable($table);
 	}
 
-	protected function buildQuery($query) {
-		$query
+	protected function buildQuery($query)
+	{
+		$this->filter_selections = $this->getFilterSettings();
+		return $query;
+		/*$query
 			->select('crs.crs_id')
 			->select('crs.title')
 			->select('crs.type')
@@ -88,12 +97,156 @@ class ilObjReportTrDemandRet extends ilObjReportBase {
 		$this->crs_topics_filter->addToQuery($query)
 			->group_by('crs.crs_id')
 			->compile();
+		return $query;*/
+	}
+
+	protected function getFilterSettings()
+	{
+		$filter = $this->filter();
+		if ($this->filter_settings) {
+			$settings = call_user_func_array(array($filter, "content"), $this->filter_settings);
+		}
+		return $settings;
+	}
+
+	public function buildQueryStatement()
+	{
+		$cancell_condition = $this->gIldb->in(
+			'usrcrs.booking_status',
+			array('kostenfrei storniert','kostenpflichtig storniert'),
+			false,
+			'text'
+		);
+
+		$successfull = 'usrcrs.participation_status = \'teilgenommen\''
+			.' AND usrcrs.booking_status = \'gebucht\'';
+
+		$crs_has_overnights = 'usrcrs.overnights IS NOT NULL AND usrcrs.overnights > 0';
+
+		$trainers = 'GROUP_CONCAT('
+					.'	IF(usrcrs.function = \'Trainer\',CONCAT(usr.firstname,\' \',usr.lastname) ,NULL)'
+					.'	SEPARATOR \', \')';
+		$query =
+			'SELECT'
+			.'	crs.crs_id'
+			.'	,crs.title'
+			.'	,crs.type'
+			.'	,crs.begin_date'
+			.'	,crs.end_date'
+			.'	,crs.venue'
+			.'	,crs.accomodation'
+			.'	,crs.is_cancelled'
+			.'	,tpl.title AS tpl_title'
+			.'	,'.$this->sumIf('usrcrs.booking_status = \'gebucht\'').' AS bookings'
+			.'	,'.$this->sumIf($cancell_condition).' AS cancellations'
+			.'	,'.$this->sumIf($successfull).'	AS succ_participations'
+			.'	,'.$this->sumIf($crs_has_overnights, 'usrcrs.overnights').' AS overnights'
+			.'	,'.$trainers.' AS trainers'
+			.'	FROM hist_course tpl'
+			.'	JOIN hist_course crs'
+			.'		ON crs.template_obj_id = tpl.crs_id'
+			.'	LEFT JOIN hist_usercoursestatus usrcrs'
+			.'		ON usrcrs.usr_id = crs.crs_id AND usrcrs.hist_historic = 0'
+			.'	LEFT JOIN hist_user usr'
+			.'		ON usr.user_id = usrcrs.usr_id AND usr.hist_historic = 0'
+			.$this->possiblyJoinCourseTopicsFilter()
+			.$this->conditionsWhere()
+			.'	GROUP BY tpl.crs_id';
 		return $query;
 	}
 
-	protected function buildFilter($filter) {
-		$local_condition = (string)$this->settings['is_local'] === "1"
-			? $this->gIldb->in('tpl.crs_id',array_unique($this->getSubtreeCourseTemplates()),false,'integer') 
+	private function conditionsWhere()
+	{
+		$where =
+			'	WHERE'
+			.'		(crs.end_date < '.$this->gIldb->quote(date('Y-m-d'), 'text')
+								.' OR crs.is_cancelled = \'Ja\' )'
+			.'		AND crs.hist_historic = 0'
+			.'		AND crs.is_template = \'Nein\''
+			.'		AND tpl.hist_historic = 0'
+			.'		AND tpl.is_template = \'Ja\''
+			.'		AND crs.begin_date != \'0000-00-00\''
+			.'		AND '.$this->gIldb->in('crs.type', array('Webinar','Präsenztraining','Virtuelles Training'), false, 'text')
+			.'		'.$this->localCondition()
+			.$this->addFilters();
+		return $where;
+	}
+
+	private function addFilters()
+	{
+		return $this->datePeriodFilter()
+				.$this->courseTypeFilter()
+				.$this->cancelledFilter();
+	}
+
+	private function courseTypeFilter()
+	{
+		$selection = $this->filter_selections['crs_type'];
+		if (count($selection) > 0) {
+			return '		AND '.$this->gIldb->in('crs.type', $selection, false, 'text');
+		}
+		return '';
+	}
+
+	private function cancelledFilter()
+	{
+		$selection = $this->filter_selections['cancelled'];
+		if (count($selection) > 0) {
+			return '('.implode(' OR ', $selection).')';
+		}
+		return '';
+	}
+
+	private function datePeriodFilter()
+	{
+		if ($this->filter_selections['start'] !== null) {
+			$start = $this->filter_selections['start'];
+		} else {
+			$start = date('Y').'-01-01';
+		}
+		if ($this->filter_selections['end'] !== null) {
+			$end = $this->filter_selections['end'];
+		} else {
+			$end = date('Y').'-12-31';
+		}
+		$start = $this->gIldb->quote($start, 'date');
+		$end = $this->gIldb->quote($end, 'date');
+		return '	AND ( ( (`crs`.`begin_date` >= '.$start
+				.' 			OR `crs`.`begin_date` = \'0000-00-00\' OR `crs`.`begin_date` = \'-empty-\' )'
+				.'		AND `crs`.`begin_date` <= '.$end.' ) )';
+	}
+
+	private function possiblyJoinCourseTopicsFilter()
+	{
+		$selection = $this->filter_selections['categories'];
+		if (count($selection) > 0) {
+			return
+				'	JOIN (SELECT topic_set_id FROM hist_topicset2topic JOIN hist_topics '
+				.'			USING (topic_id) '
+				.'			WHERE '.$this->gIldb->in('topic_title', $selection, false, 'text')
+				.'			GROUP BY topic_set_id) as topics'
+				.'		ON crs.topic_set = topics.topic_set_id';
+		}
+		return '';
+	}
+
+	private function localCondition()
+	{
+		if ((string)$this->settings['is_local'] === "1") {
+			return '	AND '.$this->gIldb->in('tpl.crs_id', array_unique($this->getSubtreeCourseTemplates()), false, 'integer');
+		}
+		return '';
+	}
+
+	private function sumIf($condition, $function = '1')
+	{
+		return 'SUM(IF('.$condition.','.$function.',0))';
+	}
+
+	protected function buildFilter($filter)
+	{
+		/*$local_condition = (string)$this->settings['is_local'] === "1"
+            ? $this->gIldb->in('tpl.crs_id',array_unique($this->getSubtreeCourseTemplates()),false,'integer')
 			: 'TRUE';
 		$this->crs_topics_filter = new courseTopicsFilter('crs_topics','crs.topic_set');
 		$this->crs_topics_filter->addToFilter($filter);
@@ -107,11 +260,11 @@ class ilObjReportTrDemandRet extends ilObjReportBase {
 							, date("Y")."-12-31"
 							, false
 							)
-			->multiselect_custom( 'cancelled' 
+            ->multiselect_custom( 'cancelled'
 								, $this->plugin->txt("filter_cancelled")
-								, array("crs.is_cancelled  = 'Ja'" 
+                                , array("crs.is_cancelled  = 'Ja'"
 										=> $this->plugin->txt('crs_is_cancelled'),
-										"crs.is_cancelled  != 'Ja' OR crs.is_cancelled IS NULL" 
+                                        "crs.is_cancelled  != 'Ja' OR crs.is_cancelled IS NULL"
 										=> $this->plugin->txt('crs_is_not_cancelled'))
 								, array()
 								, ''
@@ -125,7 +278,7 @@ class ilObjReportTrDemandRet extends ilObjReportBase {
 							 , array()
 							 , ""
 							 , 200
-							 , 160					
+                             , 160
 							)
 			->static_condition("(crs.end_date < ".$this->gIldb->quote(date('Y-m-d'),'text')
 								." OR crs.is_cancelled = 'Ja' )")
@@ -137,22 +290,85 @@ class ilObjReportTrDemandRet extends ilObjReportBase {
 			->static_condition('tpl.hist_historic = 0')
 			->static_condition('tpl.is_template = '.$this->gIldb->quote('Ja','text'))
 			->action($this->filter_action)
-			->compile();
+			->compile();*/
 		return $filter;
 	}
 
-	protected function getSubtreeCourseTemplates() {
+	public function filter()
+	{
+		$pf = new \CaT\Filter\PredicateFactory();
+		$tf = new \CaT\Filter\TypeFactory();
+		$f = new \CaT\Filter\FilterFactory($pf, $tf);
+
+		$txt = function ($id) {
+			return $this->plugin->txt($id);
+		};
+
+		global $lng;
+
+		return
+			$f->sequence(
+				$f->sequence(
+					$f->dateperiod(
+						$txt("period"),
+						''
+					),
+					$f->multiselectsearch(
+						$lng->txt('categories'),
+						'',
+						gevAMDUtils::getInstance()->getOptions(gevSettings::CRS_AMD_TOPIC)
+					),
+					$f->multiselectsearch(
+						$txt('training_type'),
+						'',
+						array('Webinar' => 'Webinar',
+							'Präsenztraining' => 'Präsenztraining',
+							'Virtuelles Training' => 'Virtuelles Training')
+					),
+					$f->multiselectsearch(
+						$txt('filter_cancelled'),
+						'',
+						array(	"crs.is_cancelled  = 'Ja'"
+									=> $this->plugin->txt('crs_is_cancelled'),
+								"crs.is_cancelled  != 'Ja' OR crs.is_cancelled IS NULL"
+									=> $this->plugin->txt('crs_is_not_cancelled'))
+					)
+				)
+			)->map(function ($start, $end, $topics, $training_type, $cancelled, $categories) {
+						return array('start' => $start->format('Y-m-d'),
+									'end' => $end->format('Y-m-d'),
+									'categories' => $categories,
+									'training_type' => $training_type,
+									'cancelled' => $cancelled
+									);
+			}, $tf->dict(array(
+									'start' => $tf->string(),
+									'end' => $tf->string(),
+									'categories' => $tf->lst($tf->string()),
+									'training_type' => $tf->lst($tf->string()),
+									'cancelled' => $tf->lst($tf->string())
+
+							)));
+	}
+
+	protected function getSubtreeCourseTemplates()
+	{
 		$query = 	'SELECT obj_id FROM adv_md_values_text amd_val '
-					.'	WHERE '.$this->gIldb->in('obj_id',
-							$this->getSubtreeTypeIdsBelowParentType('crs','cat'),false,'integer')
+					.'	WHERE '.$this->gIldb->in(
+						'obj_id',
+						$this->getSubtreeTypeIdsBelowParentType('crs', 'cat'),
+						false,
+						'integer'
+					)
 					.'		AND field_id = '.$this->gIldb->quote(
-												gevSettings::getInstance()
-													->getAMDFieldId(gevSettings::CRS_AMD_IS_TEMPLATE)
-												,'integer')
-					.'		AND value = '.$this->gIldb->quote('Ja','text');
+						gevSettings::getInstance()
+													->getAMDFieldId(gevSettings::CRS_AMD_IS_TEMPLATE),
+						'integer'
+					)
+					.'		AND value = '.$this->gIldb->quote('Ja', 'text');
 		$return = array();
 		$res = $this->gIldb->query($query);
-		while($rec = $this->gIldb->fetchAssoc($res)) {
+		while ($rec = $this->gIldb->fetchAssoc($res)) {
 			$return[] = $rec['obj_id'];
 		}
 		return $return;
--- Customizing/global/plugins/Services/Repository/RepositoryObject/ReportTrDemandRet/classes/class.ilObjReportTrDemandRetGUI.php
+++ Customizing/global/plugins/Services/Repository/RepositoryObject/ReportTrDemandRet/classes/class.ilObjReportTrDemandRetGUI.php
@@ -8,57 +8,122 @@ require_once 'Customizing/global/plugins/Services/Cron/CronHook/ReportMaster/cla
 * @ilCtrl_Calls ilObjReportTrDemandRetGUI: ilPermissionGUI, ilInfoScreenGUI, ilObjectCopyGUI
 * @ilCtrl_Calls ilObjReportTrDemandRetGUI: ilCommonActionDispatcherGUI
 */
-class ilObjReportTrDemandRetGUI extends ilObjReportBaseGUI {
-	public function getType() {
+class ilObjReportTrDemandRetGUI extends ilObjReportBaseGUI
+{
+	public function getType()
+	{
 		return 'xtdr';
 	}
 
-	protected function prepareTitle($a_title) {
+	protected function prepareTitle($a_title)
+	{
 		$a_title = parent::prepareTitle($a_title);
 		$a_title->image("GEV_img/ico-head-edubio.png");
 		return $a_title;
 	}
 
-	public static function transformResultRow($rec) {
-		if($rec['title'] !== null) {
-			if(ilObject::_exists($rec['crs_id']) &&'crs' === ilObject::_lookupType($rec['crs_id'])) {
+	protected function afterConstructor()
+	{
+		parent::afterConstructor();
+		if ($this->object->plugin) {
+			$this->tpl->addCSS($this->object->plugin->getStylesheetLocation('report.css'));
+		}
+		if ($this->object) {
+			$this->filter = $this->object->filter();
+			$this->display = new \CaT\Filter\DisplayFilter(
+				new \CaT\Filter\FilterGUIFactory,
+				new \CaT\Filter\TypeFactory
+			);
+		}
+		$this->loadFilterSettings();
+	}
+
+	protected function loadFilterSettings()
+	{
+		if (isset($_POST['filter'])) {
+			$this->filter_settings = $_POST['filter'];
+		}
+		if (isset($_GET['filter'])) {
+			$this->filter_settings = unserialize(base64_decode($_GET['filter']));
+		}
+		if ($this->filter_settings) {
+			$this->object->addRelevantParameter('filter', base64_encode(serialize($this->filter_settings)));
+			$this->object->filter_settings = $this->display->buildFilterValues($this->filter, $this->filter_settings);
+		}
+	}
+
+	public static function transformResultRow($rec)
+	{
+		if ($rec['title'] !== null) {
+			if (ilObject::_exists($rec['crs_id']) &&'crs' === ilObject::_lookupType($rec['crs_id'])) {
 				$ref_id = current(ilObject::_getAllReferences($rec['crs_id']));
 				global $ilAccess;
-				if($ilAccess->checkAccess("write", "editInfo", $ref_id, "crs", $rec['crs_id'])) {
+				if ($ilAccess->checkAccess("write", "editInfo", $ref_id, "crs", $rec['crs_id'])) {
 					global $ilCtrl;
-					$ilCtrl->setParameterByClass("ilObjCourseGUI","ref_id",$ref_id);
-					$link = $ilCtrl->getLinkTargetByClass(array("ilRepositoryGUI","ilObjCourseGUI"),"editInfo");
-					$ilCtrl->setParameterByClass("ilObjCourseGUI","ref_id",null);
+					$ilCtrl->setParameterByClass("ilObjCourseGUI", "ref_id", $ref_id);
+					$link = $ilCtrl->getLinkTargetByClass(array("ilRepositoryGUI","ilObjCourseGUI"), "editInfo");
+					$ilCtrl->setParameterByClass("ilObjCourseGUI", "ref_id", null);
 					$rec["title"] = '<a href = "'.$link.'">'.$rec['title'].'</a>';
 				} elseif ($ilAccess->checkAccess("write_reduced_settings", "showSettings", $ref_id, "crs", $rec['crs_id'])) {
 					global $ilCtrl;
-					$ilCtrl->setParameterByClass("gevDecentralTrainingGUI","ref_id",$ref_id);
-					$link = $ilCtrl->getLinkTargetByClass(array("gevDesktopGUI","gevDecentralTrainingGUI"),"showSettings");
-					$ilCtrl->setParameterByClass("gevDecentralTrainingGUI","ref_id",null);
+					$ilCtrl->setParameterByClass("gevDecentralTrainingGUI", "ref_id", $ref_id);
+					$link = $ilCtrl->getLinkTargetByClass(array("gevDesktopGUI","gevDecentralTrainingGUI"), "showSettings");
+					$ilCtrl->setParameterByClass("gevDecentralTrainingGUI", "ref_id", null);
 					$rec["title"] = '<a href = "'.$link.'">'.$rec['title'].'</a>';
 				}
 			}
 			$rec['cancellation'] =
 				$rec['cancellation'] === 'Ja'
 					? $rec['cancellation'] : 'Nein';
-			$rec['begin_date'] = date_format(date_create($rec['begin_date']),'d.m.Y')
-					.' - '.date_format(date_create($rec['end_date']),'d.m.Y');
+			$rec['begin_date'] = date_format(date_create($rec['begin_date']), 'd.m.Y')
+					.' - '.date_format(date_create($rec['end_date']), 'd.m.Y');
 		} else {
 			$rec = array(	'tpl_title' => $rec['tpl_title']);
 		}
 		return parent::transformResultRow($rec);
 	}
 
-	public static function transformResultRowXLSX($rec) {
-		if($rec['title'] !== null) {
+	public static function transformResultRowXLSX($rec)
+	{
+		if ($rec['title'] !== null) {
 			$rec['cancellation'] =
 				$rec['cancellation'] === 'Ja'
 					? $rec['cancellation'] : 'Nein';
-			$rec['begin_date'] = date_format(date_create($rec['begin_date']),'d.m.Y')
-					.' - '.date_format(date_create($rec['end_date']),'d.m.Y');
+			$rec['begin_date'] = date_format(date_create($rec['begin_date']), 'd.m.Y')
+					.' - '.date_format(date_create($rec['end_date']), 'd.m.Y');
 		} else {
 			$rec = array(	'tpl_title' => $rec['tpl_title']);
 		}
 		return parent::transformResultRow($rec);
+	}
+
+	protected function renderFilter()
+	{
+		global $ilCtrl;
+		require_once("Customizing/global/plugins/Services/Cron/CronHook/ReportMaster/classes/ReportBase/class.catFilterFlatViewGUI.php");
+		$filter_flat_view = new catFilterFlatViewGUI($this, $this->filter, $this->display, $ilCtrl->getCmd());
+		return $filter_flat_view->render($this->filter_settings);
+	}
+
+	public function renderQueryView()
+	{
+		include_once "Services/Form/classes/class.ilNonEditableValueGUI.php";
+		$this->object->prepareReport();
+		$content = $this->renderFilter('query_view');
+		$form = new ilNonEditableValueGUI($this->gLng->txt("report_query_text"));
+		$form->setValue($this->object->buildQueryStatement());
+		$settings_form = new ilPropertyFormGUI();
+		$settings_form->addItem($form);
+		$content .= $settings_form->getHTML();
+		$this->gTpl->setContent($content);
+	}
+
+	protected function render()
+	{
+		$this->gTpl->setTitle(null);
+		return
+			$this->title->render()
+			.$this->renderFilter().'<br/>'
+			.$this->renderTable();
 	}
 }
