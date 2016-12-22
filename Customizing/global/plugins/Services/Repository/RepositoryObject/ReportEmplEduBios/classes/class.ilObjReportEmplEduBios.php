692a0fb866cd8c1fd0d2daf6fab2f485b3a46eef
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2016-12-22 15:47:38 +0100

    fixed orgu preselect

--- Customizing/global/plugins/Services/Repository/RepositoryObject/ReportEmplEduBios/classes/class.ilObjReportEmplEduBios.php
+++ Customizing/global/plugins/Services/Repository/RepositoryObject/ReportEmplEduBios/classes/class.ilObjReportEmplEduBios.php
@@ -202,12 +202,17 @@ class ilObjReportEmplEduBios extends ilObjReportBase
 
 	private function allOrgusOfUser()
 	{
+		$selection = $this->filter_selections['orgu_selection'];
+		if ($selection === null) {
+			if ($this->get) {
+			}
+		}
 		return
 			'SELECT usr_id, GROUP_CONCAT(DISTINCT orgu_title SEPARATOR \', \') as org_unit'.PHP_EOL
 			.'		, GROUP_CONCAT(DISTINCT org_unit_above1 SEPARATOR \';;\') as org_unit_above1'.PHP_EOL
 			.'		, GROUP_CONCAT(DISTINCT org_unit_above1 SEPARATOR \';;\') as org_unit_above2'.PHP_EOL
 			.'	FROM hist_userorgu'.PHP_EOL
-			.'	WHERE '.$this->gIldb->in("usr_id", array_intersect($this->visibleUsers(), $this->getRelevantUsersByOrguSelection()), false, "integer").PHP_EOL
+			.'	WHERE '.$this->gIldb->in("usr_id", $this->relevant_users, false, "integer").PHP_EOL
 			.'		AND action >= 0 AND hist_historic = 0'.PHP_EOL
 			.'	GROUP BY usr_id'.PHP_EOL;
 	}
@@ -313,7 +318,9 @@ class ilObjReportEmplEduBios extends ilObjReportBase
 					$this->filter_selections['recursive']
 				);
 		} else {
-			$this->relevant_users = $this->visibleUsers();
+			$this->relevant_users = $this->getRelevantUsersByOrguSelection(
+				$this->defaultOrguChoice()
+			);
 		}
 		return $query;
 	}
@@ -417,6 +424,7 @@ class ilObjReportEmplEduBios extends ilObjReportBase
 
 	private function getRelevantUsersByOrguSelection(array $orgu_ids = array(), $recursive = false)
 	{
+
 		if (count($orgu_ids) > 0) {
 			if ($recursive) {
 				$orgu_ids = $this->addRecursiveOrgusToSelection($orgu_ids);
