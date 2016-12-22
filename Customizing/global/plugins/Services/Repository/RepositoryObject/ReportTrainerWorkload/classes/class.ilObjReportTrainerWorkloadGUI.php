d6952891d85f22ddd94c46b5020613484709392e
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2016-12-22 12:28:33 +0100

    gui corrected

--- Customizing/global/plugins/Services/Repository/RepositoryObject/ReportTrainerWorkload/classes/class.ilObjReportTrainerWorkloadGUI.php
+++ Customizing/global/plugins/Services/Repository/RepositoryObject/ReportTrainerWorkload/classes/class.ilObjReportTrainerWorkloadGUI.php
@@ -8,8 +8,10 @@ require_once 'Customizing/global/plugins/Services/Cron/CronHook/ReportMaster/cla
 * @ilCtrl_Calls ilObjReportTrainerWorkloadGUI: ilPermissionGUI, ilInfoScreenGUI, ilObjectCopyGUI, gevDBVReportGUI
 * @ilCtrl_Calls ilObjReportTrainerWorkloadGUI: ilCommonActionDispatcherGUI
 */
-class ilObjReportTrainerWorkloadGUI extends ilObjReportBaseGUI {
-	public function performCustomCommand($cmd) {
+class ilObjReportTrainerWorkloadGUI extends ilObjReportBaseGUI
+{
+	public function performCustomCommand($cmd)
+	{
 		switch ($cmd) {
 			case "filter":
 				$this->showContent();
@@ -19,61 +21,81 @@ class ilObjReportTrainerWorkloadGUI extends ilObjReportBaseGUI {
 		}
 	}
 
-	public function getType() {
+	public function getType()
+	{
 		return 'xrtw';
 	}
 
-	protected function afterConstructor() {
+	protected function afterConstructor()
+	{
 		parent::afterConstructor();
-		if($this->object->plugin) {
+		if ($this->object->plugin) {
 			$this->tpl->addCSS($this->object->plugin->getStylesheetLocation('report.css'));
 			$this->filter = $this->object->filter();
-			$this->display = new \CaT\Filter\DisplayFilter
-						( new \CaT\Filter\FilterGUIFactory
-						, new \CaT\Filter\TypeFactory
-						);
+			$this->display = new \CaT\Filter\DisplayFilter(
+				new \CaT\Filter\FilterGUIFactory,
+				new \CaT\Filter\TypeFactory
+			);
 		}
 
 		$this->loadFilterSettings();
 	}
 
-	protected function loadFilterSettings() {
-		if(isset($_POST['filter'])) {
+	protected function loadFilterSettings()
+	{
+		if (isset($_POST['filter'])) {
 			$this->filter_settings = $_POST['filter'];
 		}
 
-		if(isset($_GET['filter'])) {
+		if (isset($_GET['filter'])) {
 			$this->filter_settings = unserialize(base64_decode($_GET['filter']));
 		}
 
-		if($this->filter_settings) {
+		if ($this->filter_settings) {
 			$this->object->addRelevantParameter("filter", base64_encode(serialize($this->filter_settings)));
 			$this->object->filter_settings = $this->display->buildFilterValues($this->filter, $this->filter_settings);
 		}
 	}
 
-	protected function render() {
-		$res = $this->renderFilter()."<br />";
-		$res .= $this->renderTable();
-
+	protected function render()
+	{
+		$this->gTpl->setTitle(null);
+		$res = $this->title->render()
+				.$this->renderFilter()."<br />"
+				.$this->renderTable();
 		return $res;
 	}
 
-	protected function renderFilter() {
-		require_once("Customizing/global/plugins/Services/Cron/CronHook/ReportMaster/classes/ReportBase/class.catFilterFlatViewGUI.php");
-		$filter_flat_view = new catFilterFlatViewGUI($this, $this->filter, $this->display, "filter");
-
-		return $filter_flat_view->render($this->filter_settings);
+	public function renderQueryView()
+	{
+		include_once "Services/Form/classes/class.ilNonEditableValueGUI.php";
+		$this->object->prepareReport();
+		$content = $this->renderFilter();
+		$form = new ilNonEditableValueGUI($this->gLng->txt("report_query_text"));
+		$form->setValue($this->object->buildQueryStatement());
+		$settings_form = new ilPropertyFormGUI();
+		$settings_form->addItem($form);
+		$content .= $settings_form->getHTML();
+		$this->gTpl->setContent($content);
 	}
 
-	protected function prepareTitle($a_title) {
+	protected function renderFilter()
+	{
+		require_once("Customizing/global/plugins/Services/Cron/CronHook/ReportMaster/classes/ReportBase/class.catFilterFlatViewGUI.php");
+		$filter_flat_view = new catFilterFlatViewGUI($this, $this->filter, $this->display, $this->gCtrl->getCmd());
+		return $filter_flat_view->render($this->filter_settings, (string)$_POST['filtered'] === '1');
+	}
+
+	protected function prepareTitle($a_title)
+	{
 		$a_title = parent::prepareTitle($a_title);
 		$a_title->image("GEV_img/ico-head-edubio.png");
 		return $a_title;
 	}
 
 
-	protected function renderTable() {
+	protected function renderTable()
+	{
 		$this->gCtrl->setParameter($this, 'filter', base64_encode(serialize($this->filter_settings)));
 		$table = parent::renderTable();
 		$sum_table = $this->renderSumTable();
@@ -81,8 +103,9 @@ class ilObjReportTrainerWorkloadGUI extends ilObjReportBaseGUI {
 		return $sum_table.$table;
 	}
 
-	private function renderSumTable(){
-		
+	private function renderSumTable()
+	{
+
 		$table = new catTableGUI($this, "showContent");
 		$table->setEnableTitle(false);
 		$table->setTopCommands(false);
@@ -90,21 +113,18 @@ class ilObjReportTrainerWorkloadGUI extends ilObjReportBaseGUI {
 		$table_sums = $this->object->deliverSumtable();
 
 		$table->setRowTemplate(
-			$table_sums->row_template_filename, 
+			$table_sums->row_template_filename,
 			$table_sums->row_template_module
 		);
 
 		$table->addColumn("", "blank", "0px", false);
 		foreach ($table_sums->columns as $col) {
-			$table->addColumn( $col[2] ? $col[1] : $this->lng->txt($col[1])
-							 , $col[0]
-							 , $col[3]
-							 );
+			$table->addColumn($col[2] ? $col[1] : $this->lng->txt($col[1]), $col[0], $col[3]);
 		}
 
-		$sum_row = $this->object->fetchSumData();	
-		if(count($sum_row) == 0) {
-			foreach(array_keys($table_sums->columns) as $field) {
+		$sum_row = $this->object->fetchSumData();
+		if (count($sum_row) == 0) {
+			foreach (array_keys($table_sums->columns) as $field) {
 				$sum_row[$field] = 0;
 			}
 		}
@@ -116,21 +136,23 @@ class ilObjReportTrainerWorkloadGUI extends ilObjReportBaseGUI {
 		return $return;
 	}
 
-	public static function transformResultRow($rec) {
+	public static function transformResultRow($rec)
+	{
 		global $ilCtrl;
 		foreach ($rec as $key => &$value) {
-			if($key != 'fullname') {
-				if(strpos($key,'_workload') === false) {
-					$value = number_format($value,2,',','.');
+			if ($key != 'fullname' && $key != 'user_id') {
+				if (strpos($key, '_workload') === false) {
+					$value = number_format($value, 2, ',', '.');
 				} else {
-					$value = number_format($value,0,',','.');
+					$value = number_format($value, 0, ',', '.');
 				}
 			}
 		}
 		return parent::transformResultRow($rec);
 	}
 
-	public static function transformResultRowXLSX($rec) {
+	public static function transformResultRowXLSX($rec)
+	{
 		return self::transformResultRow($rec);
 	}
 }
