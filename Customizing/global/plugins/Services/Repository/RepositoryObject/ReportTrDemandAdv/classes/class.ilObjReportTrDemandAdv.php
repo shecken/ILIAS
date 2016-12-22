ec1da4607ba8679d5979f74b7b899526ffb49056
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2016-12-22 15:20:53 +0100

    corrected group by

--- Customizing/global/plugins/Services/Repository/RepositoryObject/ReportTrDemandAdv/classes/class.ilObjReportTrDemandAdv.php
+++ Customizing/global/plugins/Services/Repository/RepositoryObject/ReportTrDemandAdv/classes/class.ilObjReportTrDemandAdv.php
@@ -2,44 +2,50 @@
 
 require_once 'Customizing/global/plugins/Services/Cron/CronHook/ReportMaster/classes/ReportBase/class.ilObjReportBase.php';
 
-ini_set("memory_limit","2048M"); 
+ini_set("memory_limit", "2048M");
 ini_set('max_execution_time', 0);
 set_time_limit(0);
 
-class ilObjReportTrDemandAdv extends ilObjReportBase {
+class ilObjReportTrDemandAdv extends ilObjReportBase
+{
 	protected $is_local;
 	protected $relevant_parameters = array();
 
-	public function initType() {
+	public function initType()
+	{
 		 $this->setType("xtda");
 	}
-	
-	protected function getRowTemplateTitle() {
+
+	protected function getRowTemplateTitle()
+	{
 		return "tpl.gev_training_utilisation_advanced_row.html";
 	}
 
-	protected function buildOrder($order) {
+	protected function buildOrder($order)
+	{
 		return $order
 					->defaultOrder("tpl_title", "ASC")
-					->mapping("tpl_title",array("tpl_title","title","begin_date"))
-					->mapping("booked_wl",array("waitinglist_active","booked_wl"));
+					->mapping("tpl_title", array("tpl_title","title","begin_date"))
+					->mapping("booked_wl", array("waitinglist_active","booked_wl"));
 	}
 
-	protected function createLocalReportSettings() {
+	protected function createLocalReportSettings()
+	{
 		$this->local_report_settings =
 			$this->s_f->reportSettings('rep_robj_rtda')
 				->addSetting($this->s_f
-								->settingBool('is_local', $this->plugin->txt('report_is_local'))
-								);
+								->settingBool('is_local', $this->plugin->txt('report_is_local')));
 	}
 
-	protected function getTopics() {
+	protected function getTopics()
+	{
 		require_once 'Services/GEV/Utils/classes/class.gevAMDUtils.php';
 		require_once 'Services/GEV/Utils/classes/class.gevSettings.php';
 		return	gevAMDUtils::getInstance()->getOptions(gevSettings::CRS_AMD_TOPIC);
 	}
 
-	protected function buildTable($table) {
+	protected function buildTable($table)
+	{
 		$table	->column('tpl_title', $this->plugin->txt('tpl_title'), true)
 				->column('title', $this->plugin->txt('crs_title'), true)
 				->column('type', $this->plugin->txt('crs_type'), true)
@@ -54,7 +60,8 @@ class ilObjReportTrDemandAdv extends ilObjReportBase {
 		return parent::buildTable($table);
 	}
 
-	protected function buildQuery($query) {
+	protected function buildQuery($query)
+	{
 		$query
 			->select('crs.crs_id')
 			->select('crs.title')
@@ -86,107 +93,96 @@ class ilObjReportTrDemandAdv extends ilObjReportBase {
 		return $query;
 	}
 
-	protected function buildFilter($filter) {
+	protected function buildFilter($filter)
+	{
 		return null;
 	}
 
-	public function filter() {
+	public function filter()
+	{
 		$db = $this->gIldb;
 		$pf = new \CaT\Filter\PredicateFactory();
 		$tf = new \CaT\Filter\TypeFactory();
 		$f = new \CaT\Filter\FilterFactory($pf, $tf);
-		$txt = function($id) { return $this->plugin->txt($id); };
+		$txt = function ($id) {
+			return $this->plugin->txt($id);
+		};
 
 		return
-		$f->sequence
-		(
-			$f->sequence
-			(
+		$f->sequence(
+			$f->sequence(
 
 				/* BEGIN BLOCK - PERIOD */
-				$f->dateperiod
-				(
-					$txt("period")
-					, ""
-				)->map
-					(
-						function($start,$end) use ($f)
-						{
-							$pc = $f->dateperiod_overlaps_predicate
-							(
+				$f->dateperiod(
+					$txt("period"),
+					""
+				)->map(
+					function ($start, $end) use ($f) {
+							$pc = $f->dateperiod_overlaps_predicate(
+								"crs.begin_date",
 								"crs.begin_date"
-								,"crs.begin_date"
 							);
 							return array ("date_period_predicate" => $pc($start,$end)
 										 ,"start" => $start
 										 ,"end" => $end);
-						},
-						$tf->dict
-						(
-							array
+					},
+					$tf->dict(
+						array
 							(
 								"date_period_predicate" => $tf->cls("CaT\\Filter\\Predicates\\Predicate")
 								,"start" => $tf->cls("DateTime")
 								,"end" => $tf->cls("DateTime")
 							)
-						)
-					),
-					/* END BLOCK - PERIOD */
+					)
+				),
+				/* END BLOCK - PERIOD */
 
 
 				/* BEGIN BLOCK - FILTER TOPICS */
-				$f->multiselectsearch
-				(
-					$txt("filter_topics")
-					, ""
-					, $this->getTopics()
+				$f->multiselectsearch(
+					$txt("filter_topics"),
+					"",
+					$this->getTopics()
 				),
 				/* END BLOCK - FILTER TOPICS */
 
 
 				/* BEGIN BLOCK - TRAINING TYPE */
-				$f->multiselectsearch
-				(
-					$txt("training_type")
-					, ""
-					, array('Webinar' => 'Webinar','Präsenztraining' => 'Präsenztraining','Virtuelles Training' => 'Virtuelles Training')
+				$f->multiselectsearch(
+					$txt("training_type"),
+					"",
+					array('Webinar' => 'Webinar','Präsenztraining' => 'Präsenztraining','Virtuelles Training' => 'Virtuelles Training')
 				),
 				/* END BLOCK - TRAINING TYPE */
 
 
 				/* BEGIN BLOCK - STATUS */
-				$f->multiselectsearch
-				(
-					$txt("status")
-					, ""
-					, array('min_participants > bookings' => $txt('cancel_danger'), 'min_participants <= bookings' => $txt('no_cancel_danger'))
+				$f->multiselectsearch(
+					$txt("status"),
+					"",
+					array('min_participants > bookings' => $txt('cancel_danger'), 'min_participants <= bookings' => $txt('no_cancel_danger'))
 				),
 				/* END BLOCK - STATUS */
 
 				/* BEGIN BLOCK - WAITING LIST */
-				$f->multiselectsearch
-				(
-					$txt("waiting_list")
-					, ""
-					, array("crs.waitinglist_active = 'Ja'" => $txt('waiting_list'), "crs.waitinglist_active = 'Nein'" => $txt('no_waiting_list'))
+				$f->multiselectsearch(
+					$txt("waiting_list"),
+					"",
+					array("crs.waitinglist_active = 'Ja'" => $txt('waiting_list'), "crs.waitinglist_active = 'Nein'" => $txt('no_waiting_list'))
 				),
 				/* END BLOCK - WAITING LIST */
 
 				/* BEGIN BLOCK - BOOKING OVER */
-				$f->multiselectsearch
-				(
-					$txt("booking_over")
-					, ""
-					, array($db->quote(date('Y-m-d'),'text')." > booking_dl " => $txt('book_dl_over'),
-							$db->quote(date('Y-m-d'),'text')." <= booking_dl " => $txt('book_dl_not_over'))
+				$f->multiselectsearch(
+					$txt("booking_over"),
+					"",
+					array($db->quote(date('Y-m-d'), 'text')." > booking_dl " => $txt('book_dl_over'),
+							$db->quote(date('Y-m-d'), 'text')." <= booking_dl " => $txt('book_dl_not_over'))
 				)
 				/* END BLOCK - BOOKING OVER */
-
 			)
-		)->map
-				(
-					function($date_period_predicate, $start, $end, $filter_topics, $training_type, $status, $waiting_list, $booking_over)
-					{
+		)->map(
+			function ($date_period_predicate, $start, $end, $filter_topics, $training_type, $status, $waiting_list, $booking_over) {
 						return array("period_pred" => $date_period_predicate
 									,"start" => $start
 									,"end" => $end
@@ -195,10 +191,9 @@ class ilObjReportTrDemandAdv extends ilObjReportBase {
 									,"status" => $status
 									,"waiting_list" => $waiting_list
 									,"booking_over" => $booking_over);
-					},
-					$tf->dict
-					(
-						array("period_pred" => $tf->cls("CaT\\Filter\\Predicates\\Predicate")
+			},
+			$tf->dict(
+				array("period_pred" => $tf->cls("CaT\\Filter\\Predicates\\Predicate")
 							 ,"start" => $tf->cls("DateTime")
 							 ,"end" => $tf->cls("DateTime")
 							 ,"filter_topics" => $tf->lst($tf->string())
@@ -207,16 +202,17 @@ class ilObjReportTrDemandAdv extends ilObjReportBase {
 							 ,"waiting_list" => $tf->lst($tf->string())
 							 ,"booking_over" => $tf->lst($tf->string())
 						)
-					)
-				);
+			)
+		);
 	}
 
-	public function buildQueryStatement() {
+	public function buildQueryStatement()
+	{
 		$db = $this->gIldb;
 		$query_object = $this->buildQuery(catReportQuery::create());
 		$select = $query_object->sql();
 
-		$where = " WHERE  (crs.begin_date >= " .$db->quote(date('Y-m-d'),"text") .")\n"
+		$where = " WHERE  (crs.begin_date >= " .$db->quote(date('Y-m-d'), "text") .")\n"
 				."     AND (crs.is_cancelled != 'Ja' OR crs.is_cancelled IS NULL)\n"
 				."     AND crs.hist_historic = 0\n"
 				."     AND crs.is_template = 'Nein'\n"
@@ -226,13 +222,13 @@ class ilObjReportTrDemandAdv extends ilObjReportBase {
 				."     AND tpl.is_template = 'Ja'\n";
 
 		$filter = $this->filter();
-		if($this->filter_settings) {
+		if ($this->filter_settings) {
 			$settings = call_user_func_array(array($filter, "content"), $this->filter_settings);
 			$to_sql = new \CaT\Filter\SqlPredicateInterpreter($db);
 			$dt_query = $to_sql->interpret($settings['period_pred']);
 			$where .= "    AND " .$dt_query;
 			$having = "";
-			if(!empty($settings['filter_topics'])) {
+			if (!empty($settings['filter_topics'])) {
 				$select .= " JOIN (SELECT topic_set_id FROM hist_topicset2topic JOIN hist_topics\n"
 						  ."         USING (topic_id)\n"
 						  ."         WHERE ".$db->in('topic_title', $settings['filter_topics'], false, 'text') ."\n"
@@ -242,36 +238,36 @@ class ilObjReportTrDemandAdv extends ilObjReportBase {
 				$where .= "     AND crs.topic_set != -1";
 			}
 
-			if(!empty($settings['training_type'])) {
+			if (!empty($settings['training_type'])) {
 				$where .= "    AND " .$db->in("crs.type", $settings['training_type'], false, "text");
 			}
 
-			if(!empty($settings['status'][0]) && !empty($settings['status'][1])) {
+			if (!empty($settings['status'][0]) && !empty($settings['status'][1])) {
 				$having .= "    AND (" .$settings['status'][0]
 						.  "    OR " .$settings['status'][1] .")";
-			} else if (!empty($settings['status'][0])) {
+			} elseif (!empty($settings['status'][0])) {
 				$having .= "    AND (" .$settings['status'][0] .")";
-			} else if (!empty($settings['status'][1])) {
+			} elseif (!empty($settings['status'][1])) {
 				$having .= "    AND (" .$settings['status'][1] .")";
 			}
 
-			if(!empty($settings['waiting_list'])) {
-				$where .= "    AND (" .implode(' OR ',$settings['waiting_list']).")";
+			if (!empty($settings['waiting_list'])) {
+				$where .= "    AND (" .implode(' OR ', $settings['waiting_list']).")";
 			}
 
-			if(!empty($settings['booking_over'])) {
+			if (!empty($settings['booking_over'])) {
 				$having .= "    AND (" .$settings['booking_over'][0] .")";
 			}
 		} else {
 			$where .= "     AND ((`crs`.`begin_date` < '" .date("Y") ."-12-31' ) OR (`crs`.`begin_date` = '".date("Y") ."-12-31' ) ) AND (('".date("Y") ."-01-01' < `crs`.`begin_date` ) OR ('" .date("Y") ."-01-01' = `crs`.`begin_date` ) )\n";
 		}
 
-		if((string)$this->settings['is_local'] === "1") {
+		if ((string)$this->settings['is_local'] === "1") {
 			$where .= "     AND " .$db->in("crs.template_obj_id", array_unique($this->getSubtreeCourseTemplates()), false, "integer");
 		}
-		$group = " GROUP BY ('crs.crs_id') ";
+		$group = " GROUP BY crs.crs_id ";
 		$order = $this->queryOrder();
-		if($having !== "") {
+		if ($having !== "") {
 			$having = " HAVING TRUE\n"
 					 .$having;
 		}
@@ -279,35 +275,43 @@ class ilObjReportTrDemandAdv extends ilObjReportBase {
 		return $query;
 	}
 
-	protected function fetchData(callable $callback) {
+	protected function fetchData(callable $callback)
+	{
 		$db = $this->gIldb;
 
 		$query = $this->buildQueryStatement();
 		$res = $db->query($query);
 		$data = [];
 
-		while($rec = $db->fetchAssoc($res)) {
+		while ($rec = $db->fetchAssoc($res)) {
 			$data[] = call_user_func($callback, $rec);
 		}
 		return $data;
 	}
 
-	public function getRelevantParameters() {
+	public function getRelevantParameters()
+	{
 		return $this->relevant_parameters;
 	}
 
-	protected function getSubtreeCourseTemplates() {
+	protected function getSubtreeCourseTemplates()
+	{
 		$query = 	'SELECT obj_id FROM adv_md_values_text amd_val '
-					.'	WHERE '.$this->gIldb->in('obj_id',
-							$this->getSubtreeTypeIdsBelowParentType('crs','cat'),false,'integer')
+					.'	WHERE '.$this->gIldb->in(
+						'obj_id',
+						$this->getSubtreeTypeIdsBelowParentType('crs', 'cat'),
+						false,
+						'integer'
+					)
 					.'		AND field_id = '.$this->gIldb->quote(
-												gevSettings::getInstance()
-													->getAMDFieldId(gevSettings::CRS_AMD_IS_TEMPLATE)
-												,'integer')
-					.'		AND value = '.$this->gIldb->quote('Ja','text');
+						gevSettings::getInstance()
+													->getAMDFieldId(gevSettings::CRS_AMD_IS_TEMPLATE),
+						'integer'
+					)
+					.'		AND value = '.$this->gIldb->quote('Ja', 'text');
 		$return = array();
 		$res = $this->gIldb->query($query);
-		while($rec = $this->gIldb->fetchAssoc($res)) {
+		while ($rec = $this->gIldb->fetchAssoc($res)) {
 			$return[] = $rec['obj_id'];
 		}
 		return $return;
