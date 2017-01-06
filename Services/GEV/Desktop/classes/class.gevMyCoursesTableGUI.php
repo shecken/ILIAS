ac55c1bd9a5e6395a4fdd7d9fa8def2977f9a988
Richard Klees <richard.klees@concepts-and-training.de>
2017-01-06 15:06:44 +0100

    Jill: refererrer?

--- Services/GEV/Desktop/classes/class.gevMyCoursesTableGUI.php
+++ Services/GEV/Desktop/classes/class.gevMyCoursesTableGUI.php
@@ -210,7 +210,7 @@ class gevCoursesTableGUI extends catAccordionTableGUI
 	protected function buildNAQualiLink($ref_id) {
 		$backlink = $this->gCtrl->getLinkTarget($this->parent_obj, $this->parent_cmd);
 		$this->gCtrl->setParameterByClass("ilObjJillGUI", "ref_id", $ref_id);
-		$this->gCtrl->setParameterByClass("ilObjJillGUI", "referrer", urlencode($backlink));
+		$this->gCtrl->setParameterByClass("ilObjJillGUI", "referrer", base64_encode($backlink));
 		$link = $this->gCtrl->getLinkTargetByClass(
 						array("ilObjPluginDispatchGUI", "ilObjJillGUI"),
 						"xView"
