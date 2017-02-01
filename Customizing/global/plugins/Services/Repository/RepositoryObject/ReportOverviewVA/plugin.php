94640dadcb8b763326962bbc44ba6baca116e463
Daniel Weise <daniel.weise@concepts-and-training.de>
2017-02-01 10:36:35 +0100

    added structure for new plugin: ReportOverviewVA

---
+++ Customizing/global/plugins/Services/Repository/RepositoryObject/ReportOverviewVA/plugin.php
@@ -0,0 +1,12 @@
+<?php
+
+$id = "xova";
+
+$version = "0.0.1";
+
+
+$ilias_min_version = "4.2.0";
+$ilias_max_version = "5.0.0";
+
+$responsible = "Daniel Weise";
+$responsible_mail = "daniel.weise@concepts-and-training.de";
