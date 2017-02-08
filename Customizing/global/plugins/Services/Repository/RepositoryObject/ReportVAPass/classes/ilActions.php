04d5f1dc90707f8cc58b27191770cd0fd302c6f2
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2017-02-08 16:09:38 +0100

    gev2: 2793 update settings

--- Customizing/global/plugins/Services/Repository/RepositoryObject/ReportVAPass/classes/ilActions.php
+++ Customizing/global/plugins/Services/Repository/RepositoryObject/ReportVAPass/classes/ilActions.php
@@ -4,13 +4,15 @@ namespace CaT\Plugins\ReportVAPass;
 
 class ilActions
 {
+	const F_TITLE = "title";
+	const F_DESCRIPTION = "description";
 	const F_SP_NODE_REF_ID = "sp_node_ref_id";
 	const F_ONLINE = "is_online";
 
-	public function __construct($obj_id, $db)
+	public function __construct(\ilObjReportVAPass $obj, $db)
 	{
-		assert('is_int($obj_id)');
-		$this->obj_id = $obj_id;
+		$this->obj = $obj;
+		$this->obj_id = $this->obj->getId();
 		$this->db = $db;
 	}
 
@@ -29,17 +31,40 @@ class ilActions
 		return $this->db->read($this->obj_id);
 	}
 
-	public function update(array $post)
+	public function update(Settings\VAPass $va_pass)
 	{
-		$sp_node_ref_id = $post[self::F_SP_NODE_REF_ID];
-		$is_online = (bool)$post[self::F_ONLINE];
-
-		$update_va_pass = new Settings\VAPass($this->obj_id, $sp_node_ref_id, $is_online);
-		$this->db->update($update_va_pass);
+		$this->db->update($va_pass);
 	}
 
 	public function delete()
 	{
 		$this->db->delete($this->obj_id);
+	}
+
+	public function getObject()
+	{
+		return $this->obj;
+	}
+
+	public function getSettingsFromArray($post)
+	{
+		$sp_node_ref_id = (int)$post[self::F_SP_NODE_REF_ID];
+		$online = (bool)$post[self::F_ONLINE];
+
+		return new Settings\VAPass($this->obj_id, $sp_node_ref_id, $online);
+	}
+
+	public function updateObjectFromArray(array $post)
+	{
+		$title = $post[self::F_TITLE];
+		$description = $post[self::F_DESCRIPTION];
+
+		$this->obj->setTitle($title);
+		$this->obj->setDescription($description);
+
+		$new_settings = $this->getSettingsFromArray($post);
+		$this->obj->setSettings($new_settings);
+
+		$this->obj->update();
 	}
 }
