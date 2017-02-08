f7677090cba8e2fd31a56525fe6a242f52cccd8c
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2017-02-08 16:16:36 +0100

    gev2: 2793 add settings gui

---
+++ Customizing/global/plugins/Services/Repository/RepositoryObject/ReportVAPass/classes/Settings/class.ilVAPassSettingsGUI.php
@@ -0,0 +1,107 @@
+<?php
+
+use \CaT\Plugins\ReportVAPass;
+
+class ilVAPassSettingsGUI
+{
+	use ReportVAPass\Settings\ilFormHelper;
+
+	const EDIT_SETTINGS = "editProperties";
+	const SAVE_SETTINGS = "saveSettings";
+	public function __construct(ReportVAPass\ilActions $actions, Closure $txt)
+	{
+		global $ilCtrl, $tpl;
+
+		$this->g_ctrl = $ilCtrl;
+		$this->g_tpl = $tpl;
+		$this->actions = $actions;
+		$this->txt = $txt;
+	}
+
+	public function executeCommand()
+	{
+		$cmd = $this->g_ctrl->getCmd(self::EDIT_SETTINGS);
+
+		switch ($cmd) {
+			case self::EDIT_SETTINGS:
+			case self::SAVE_SETTINGS:
+				$this->$cmd();
+				break;
+			default:
+				throw new Exception("ilVAPassSettingsGUI::executeCommand: cmd not found: $cmd");
+		}
+	}
+
+	protected function editProperties($form = null)
+	{
+		if ($form === null) {
+			$form = $this->initForm();
+			$form = $this->fillForm($form);
+		}
+
+		$form->addCommandButton(self::SAVE_SETTINGS, $this->txt("save"));
+		$form->addCommandButton(self::EDIT_SETTINGS, $this->txt("cancel"));
+
+		$this->g_tpl->setContent($form->getHtml());
+	}
+
+	protected function saveSettings()
+	{
+		$form = $this->initForm();
+
+		if (!$form->checkInput()) {
+			$form->setValuesByPost();
+			$this->editProperties($form);
+			return;
+		}
+
+		$post = $_POST;
+		$this->actions->updateObjectFromArray($post);
+
+		$this->g_ctrl->redirect($this);
+	}
+
+	protected function initForm()
+	{
+		$form = new ilPropertyFormGUI();
+		$form->setFormAction($this->g_ctrl->getFormAction($this));
+
+		$ti = new ilTextInputGUI($this->txt("settings_title"), ReportVAPass\ilActions::F_TITLE);
+		$ti->setRequired(true);
+		$form->addItem($ti);
+
+		$ta = new ilTextareaInputGUI($this->txt("setting_description"), ReportVAPass\ilActions::F_DESCRIPTION);
+		$form->addItem($ta);
+
+		$this->addSettingsEditFormItems($form);
+
+		return $form;
+	}
+
+	protected function fillForm($form)
+	{
+		$current = $this->actions->getObject();
+		$settings = $current->getSettings();
+
+		$values = array(ReportVAPass\ilActions::F_TITLE => $current->getTitle()
+			, ReportVAPass\ilActions::F_DESCRIPTION => $current->getDescription()
+			);
+
+		$this->getSettingValues($settings, $values);
+		$form->setValuesByArray($values);
+
+		return $form;
+	}
+
+	/**
+	 * @param 	string	$code
+	 * @return	string
+	 */
+	public function txt($code)
+	{
+		assert('is_string($code)');
+		$txt = $this->txt;
+
+		return $txt($code);
+	}
+}
