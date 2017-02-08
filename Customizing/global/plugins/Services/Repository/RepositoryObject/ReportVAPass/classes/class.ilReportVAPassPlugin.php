e66b0c85215aee690874821c4e576c435ded6b71
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2017-02-08 15:07:10 +0100

    gev2: 2793 creation of settings db added

--- Customizing/global/plugins/Services/Repository/RepositoryObject/ReportVAPass/classes/class.ilReportVAPassPlugin.php
+++ Customizing/global/plugins/Services/Repository/RepositoryObject/ReportVAPass/classes/class.ilReportVAPassPlugin.php
@@ -1,9 +1,33 @@
 <?php
-class ilReportVAPassPlugin
+include_once("./Services/Repository/classes/class.ilRepositoryObjectPlugin.php");
+require_once(__DIR__."/../vendor/autoload.php");
+
+use \CaT\Plugins\ReportVAPass\Settings;
+
+class ilReportVAPassPlugin extends ilRepositoryObjectPlugin
 {
+	/**
+	 * @var CaT\Plugins\ReportVAPass\Settings\DB
+	 */
+	protected $settings_db;
+
 	// must correspond to the plugin subdirectory
-	protected function getReportName()
+	public function getPluginName()
 	{
 		return "ReportVAPass";
+	}
+
+	/**
+	 * create (if not available) and returns SettingsDB
+	 *
+	 * @return \CaT\Plugins\ReportVAPass\Settings\DB
+	 */
+	public function getVAPassDB()
+	{
+		global $ilDB;
+		if ($this->settings_db === null) {
+			$this->settings_db = new Settings\ilDB($ilDB);
+		}
+		return $this->settings_db;
 	}
 }
