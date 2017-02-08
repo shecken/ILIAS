7c6f2b0be9ae2cffe34eb060518f3e3fa7fbf5c9
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2017-02-08 16:06:13 +0100

    gev2: 2793 add function to get settings values in array

--- Customizing/global/plugins/Services/Repository/RepositoryObject/ReportVAPass/classes/Settings/ilFormHelper.php
+++ Customizing/global/plugins/Services/Repository/RepositoryObject/ReportVAPass/classes/Settings/ilFormHelper.php
@@ -7,7 +7,7 @@ trait ilFormHelper
 {
 	public function addSettingsFormItems($form)
 	{
-		$ti = new \ilTextInputGUI($this->txt("setting_sp_node_ref_id"), ilActions::F_SP_NODE_REF_ID);
+		$ti = new \ilNumberInputGUI($this->txt("setting_sp_node_ref_id"), ilActions::F_SP_NODE_REF_ID);
 		$ti->setRequired(true);
 		$form->addItem($ti);
 
@@ -22,5 +22,11 @@ trait ilFormHelper
 		$form->addItem($cb);
 
 		return $form;
+	}
+
+	public function getSettingValues(VAPass $va_pass, array &$values)
+	{
+		$values[ilActions::F_SP_NODE_REF_ID] = $va_pass->getSPNodeRefId();
+		$values[ilActions::F_ONLINE] = $va_pass->getOnline();
 	}
 }
