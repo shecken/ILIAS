52df832fa25aa39b99721513b26d86c7d886a53e
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2017-02-24 16:56:26 +0100

    user to which report refers to instead of global user

--- Modules/StudyProgramme/classes/tables/class.ilIndividualPlanDetailTableGUI.php
+++ Modules/StudyProgramme/classes/tables/class.ilIndividualPlanDetailTableGUI.php
@@ -190,7 +190,7 @@ class ilIndividualPlanDetailTableGUI extends catTableGUI
 
 	protected function maybeAddViewRecordTo(array $items, ilObjManualAssessment $mass)
 	{
-		$member = $mass->membersStorage()->loadMember($mass, $this->g_user);
+		$member = $mass->membersStorage()->loadMember($mass, $this->user);
 		$ex_id = $member->examinerId();
 		$access = $mass->accessHandler();
 
@@ -209,7 +209,7 @@ class ilIndividualPlanDetailTableGUI extends catTableGUI
 
 	protected function maybeAddEditRecordTo(array $items, ilObjManualAssessment $mass)
 	{
-		$member = $mass->membersStorage()->loadMember($mass, $this->g_user);
+		$member = $mass->membersStorage()->loadMember($mass, $this->user);
 		$ex_id = $member->examinerId();
 
 		$finalized = $member->finalized();
