70ebad1057c04b6317d6d6a86526d7ca02c48d82
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2017-02-27 12:24:31 +0100

    adjusted lp-progress determination

--- Services/Tracking/classes/status/class.ilLPStatusManualAssessment.php
+++ Services/Tracking/classes/status/class.ilLPStatusManualAssessment.php
@@ -5,56 +5,63 @@ require_once 'Services/Tracking/classes/class.ilLPStatus.php';
 require_once 'Modules/ManualAssessment/classes/LearningProgress/class.ilManualAssessmentLPInterface.php';
 require_once 'Modules/ManualAssessment/classes/Members/class.ilManualAssessmentMembers.php';
 
-class ilLPStatusManualAssessment extends ilLPStatus {
+class ilLPStatusManualAssessment extends ilLPStatus
+{
 
-	public function _getNotAttempted($a_obj_id) {
-		return ilManualAssessmentLPInterface::getMembersHavingStatusIn($a_obj_id, 
-			ilManualAssessmentMembers::LP_NOT_ATTEMPTED);
+	public function _getNotAttempted($a_obj_id)
+	{
+		return ilManualAssessmentLPInterface::getMembersHavingStatusIn(
+			$a_obj_id,
+			ilManualAssessmentMembers::LP_NOT_ATTEMPTED
+		);
 	}
 
-	public function _getCountNotAttempted($a_obj_id) {
+	public function _getCountNotAttempted($a_obj_id)
+	{
 		return count(self::_getNotAttempted($a_obj_id));
 	}
 
-	public function _getCountInProgress($a_obj_id) {
+	public function _getCountInProgress($a_obj_id)
+	{
 		return count(self::_getInProgress($a_obj_id));
 	}
 
-	public function _getInProgress($a_obj_id) {
-		return ilManualAssessmentLPInterface::getMembersHavingStatusIn($a_obj_id, 
-			ilManualAssessmentMembers::LP_IN_PROGRESS);
+	public function _getInProgress($a_obj_id)
+	{
+		return ilManualAssessmentLPInterface::getMembersHavingStatusIn(
+			$a_obj_id,
+			ilManualAssessmentMembers::LP_IN_PROGRESS
+		);
 	}
 
-	public function _getCountCompleted($a_obj_id) {
+	public function _getCountCompleted($a_obj_id)
+	{
 		return count(self::_getCompleted($a_obj_id));
 	}
 
-	public function _getCompleted($a_obj_id) {
-		return ilManualAssessmentLPInterface::getMembersHavingStatusIn($a_obj_id, 
-			ilManualAssessmentMembers::LP_COMPLETED);
+	public function _getCompleted($a_obj_id)
+	{
+		return ilManualAssessmentLPInterface::getMembersHavingStatusIn(
+			$a_obj_id,
+			ilManualAssessmentMembers::LP_COMPLETED
+		);
 	}
 
-	public function _getCountFailed() {
+	public function _getCountFailed()
+	{
 		return count(self::_getFailed($a_obj_id));
 	}
 
-	public function _getFailed($a_obj_id) {
-		return ilManualAssessmentLPInterface::getMembersHavingStatusIn($a_obj_id, 
-			ilManualAssessmentMembers::LP_FAILED);
+	public function _getFailed($a_obj_id)
+	{
+		return ilManualAssessmentLPInterface::getMembersHavingStatusIn(
+			$a_obj_id,
+			ilManualAssessmentMembers::LP_FAILED
+		);
 	}
 
-	function determineStatus($a_obj_id, $a_user_id, $a_obj = null) {
-		switch ((string)ilManualAssessmentLPInterface::determineStatusOfMember($a_obj_id,$a_user_id)) {
-			case (string)ilManualAssessmentMembers::LP_NOT_ATTEMPTED:
-				return self::LP_STATUS_NOT_ATTEMPTED_NUM;
-			case (string)ilManualAssessmentMembers::LP_IN_PROGRESS:
-				return self::LP_STATUS_IN_PROGRESS_NUM;
-			case (string)ilManualAssessmentMembers::LP_FAILED:
-				return self::LP_STATUS_FAILED_NUM;
-			case (string)ilManualAssessmentMembers::LP_COMPLETED:
-				return self::LP_STATUS_COMPLETED_NUM;
-			default:
-				return self::LP_STATUS_NOT_ATTEMPTED_NUM;
-		}
+	function determineStatus($a_obj_id, $a_user_id, $a_obj = null)
+	{
+		return ilManualAssessmentLPInterface::determineStatusOfMember($a_obj_id, $a_user_id);
 	}
 }
