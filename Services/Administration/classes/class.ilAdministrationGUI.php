5d30694ba0a08c0c6fdc657a213b1922a8f61672
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2017-04-21 10:47:22 +0200

    in passing: it was still missing in ctrl-info-header, which would cause crashes at visible prg-objs

--- Services/Administration/classes/class.ilAdministrationGUI.php
+++ Services/Administration/classes/class.ilAdministrationGUI.php
@@ -11,6 +11,7 @@ include_once("./Services/Table/classes/class.ilTableGUI.php");
 * @version $Id$
 *
 * @ilCtrl_Calls ilAdministrationGUI: ilObjGroupGUI, ilObjFolderGUI, ilObjFileGUI, ilObjCourseGUI, ilCourseObjectivesGUI
+* @ilCtrl_Calls ilAdministrationGUI: ilObjStudyProgrammeGUI
 * @ilCtrl_Calls ilAdministrationGUI: ilObjSAHSLearningModuleGUI, ilObjChatroomGUI, ilObjForumGUI
 * @ilCtrl_Calls ilAdministrationGUI: ilObjLearningModuleGUI, ilObjDlBookGUI, ilObjGlossaryGUI
 * @ilCtrl_Calls ilAdministrationGUI: ilObjQuestionPoolGUI, ilObjSurveyQuestionPoolGUI, ilObjTestGUI
@@ -70,87 +71,74 @@ class ilAdministrationGUI
 
 		$this->ctrl = $ilCtrl;
 		$ilMainMenu->setActive("administration");
-		
+
 		$this->creation_mode = false;
 
 		$this->ctrl->saveParameter($this, array("ref_id", "admin_mode"));
-		
-		if ($_GET["admin_mode"] != "repository")
-		{
+
+		if ($_GET["admin_mode"] != "repository") {
 			$_GET["admin_mode"] = "settings";
 		}
-		
-		if (!ilUtil::isAPICall())
-			$this->ctrl->setReturn($this,"");
+
+		if (!ilUtil::isAPICall()) {
+			$this->ctrl->setReturn($this, "");
+		}
 
 		// determine current ref id and mode
-		if (!empty($_GET["ref_id"]) && $tree->isInTree($_GET["ref_id"]))
-		{
+		if (!empty($_GET["ref_id"]) && $tree->isInTree($_GET["ref_id"])) {
 			$this->cur_ref_id = $_GET["ref_id"];
-		}
-		else
-		{
+		} else {
 			//$this->cur_ref_id = $this->tree->getRootId();
 			$_POST = array();
-			if ($_GET["cmd"] != "getDropDown")
-			{
+			if ($_GET["cmd"] != "getDropDown") {
 				$_GET["cmd"] = "";
 			}
 		}
 	}
 
-	
+
 	/**
 	* execute command
 	*/
 	function &executeCommand()
 	{
 		global $tree, $rbacsystem, $ilias, $lng, $objDefinition, $ilHelp, $ilMainMenu;
-		
+
 		$ilMainMenu->setActive("gev_admin_menu");
-		
+
 		// permission checks
 		include_once './Services/MainMenu/classes/class.ilMainMenuGUI.php';
-		if(!ilMainMenuGUI::_checkAdministrationPermission())
-		{
-			$ilias->raiseError($this->lng->txt('permission_denied'),$ilias->error_obj->WARNING);
+		if (!ilMainMenuGUI::_checkAdministrationPermission()) {
+			$ilias->raiseError($this->lng->txt('permission_denied'), $ilias->error_obj->WARNING);
 		}
-		
+
 		// check creation mode
 		// determined by "new_type" parameter
 		$new_type = $_POST["new_type"]
 			? $_POST["new_type"]
 			: $_GET["new_type"];
-		if ($new_type != "")
-		{
+		if ($new_type != "") {
 			$this->creation_mode = true;
 		}
-	
+
 		// determine next class
-		if ($this->creation_mode)
-		{
+		if ($this->creation_mode) {
 			$obj_type = $new_type;
 			$class_name = $this->objDefinition->getClassName($obj_type);
 			$next_class = strtolower("ilObj".$class_name."GUI");
 			$this->ctrl->setCmdClass($next_class);
-		}
-		// set next_class directly for page translations
+		} // set next_class directly for page translations
 		// (no cmdNode is given in translation link)
-		elseif ($this->ctrl->getCmdClass() == "ilobjlanguageextgui")
-		{
+		elseif ($this->ctrl->getCmdClass() == "ilobjlanguageextgui") {
 			$next_class = "ilobjlanguageextgui";
-		}
-		else
-		{
+		} else {
 			$next_class = $this->ctrl->getNextClass($this);
-			
 		}
 
 		if (($next_class == "iladministrationgui" || $next_class == ""
-			) && ($this->ctrl->getCmd() == "return"))
-		{
+			) && ($this->ctrl->getCmd() == "return")) {
 			// get GUI of current object
-			$obj_type = ilObject::_lookupType($this->cur_ref_id,true);
+			$obj_type = ilObject::_lookupType($this->cur_ref_id, true);
 			$class_name = $this->objDefinition->getClassName($obj_type);
 			$next_class = strtolower("ilObj".$class_name."GUI");
 			$this->ctrl->setCmdClass($next_class);
@@ -160,25 +148,19 @@ class ilAdministrationGUI
 		$cmd = $this->ctrl->getCmd("forward");
 
 //echo "<br>cmd:$cmd:nextclass:$next_class:-".$_GET["cmdClass"]."-".$_GET["cmd"]."-";
-		switch ($next_class)
-		{
+		switch ($next_class) {
 			default:
-			
 				// forward all other classes to gui commands
-				if ($next_class != "" && $next_class != "iladministrationgui")
-				{
+				if ($next_class != "" && $next_class != "iladministrationgui") {
 					// check db update
-					include_once ("./Services/Database/classes/class.ilDBUpdate.php");
-					$dbupdate = new ilDBUpdate($this->ilias->db,true);
-					if (!$dbupdate->getDBVersionStatus())
-					{
+					include_once("./Services/Database/classes/class.ilDBUpdate.php");
+					$dbupdate = new ilDBUpdate($this->ilias->db, true);
+					if (!$dbupdate->getDBVersionStatus()) {
 						ilUtil::sendFailure($this->lng->txt("db_need_update"));
-					}
-					else if ($dbupdate->hotfixAvailable())
-					{
+					} elseif ($dbupdate->hotfixAvailable()) {
 						ilUtil::sendFailure($this->lng->txt("db_need_hotfix"));
 					}
-					
+
 					$class_path = $this->ctrl->lookupClassPath($next_class);
 					// get gui class instance
 					include_once($class_path);
@@ -186,46 +168,28 @@ class ilAdministrationGUI
 					if (($next_class == "ilobjrolegui" || $next_class == "ilobjusergui"
 						|| $next_class == "ilobjroletemplategui"
 						|| $next_class == "ilobjstylesheetgui"
-						|| $next_class == "gevlocalusergui"))
-					{
-						if ($_GET["obj_id"] != "")
-						{
+						|| $next_class == "gevlocalusergui")) {
+						if ($_GET["obj_id"] != "") {
 							$this->gui_obj = new $class_name("", $_GET["obj_id"], false, false);
 							$this->gui_obj->setCreationMode(false);
-						}
-						else
-						{
+						} else {
 							$this->gui_obj = new $class_name("", $this->cur_ref_id, true, false);
 							$this->gui_obj->setCreationMode(true);
 						}
-					}
-					else
-					{
-						if ($objDefinition->isPlugin(ilObject::_lookupType($this->cur_ref_id,true)))
-						{
+					} else {
+						if ($objDefinition->isPlugin(ilObject::_lookupType($this->cur_ref_id, true))) {
 							$this->gui_obj = new $class_name($this->cur_ref_id);
-						}
-						else
-						{
-							if (!$this->creation_mode)
-							{
-								if(is_subclass_of($class_name, "ilObject2GUI"))
-								{
+						} else {
+							if (!$this->creation_mode) {
+								if (is_subclass_of($class_name, "ilObject2GUI")) {
 									$this->gui_obj = new $class_name($this->cur_ref_id, ilObject2GUI::REPOSITORY_NODE_ID);
-								}
-								else
-								{
+								} else {
 									$this->gui_obj = new $class_name("", $this->cur_ref_id, true, false);
 								}
-							}
-							else
-							{
-								if(is_subclass_of($class_name, "ilObject2GUI"))
-								{
+							} else {
+								if (is_subclass_of($class_name, "ilObject2GUI")) {
 									$this->gui_obj = new $class_name(null, ilObject2GUI::REPOSITORY_NODE_ID, $this->cur_ref_id);
-								}
-								else
-								{
+								} else {
 									$this->gui_obj = new $class_name("", 0, true, false);
 								}
 							}
@@ -240,21 +204,19 @@ class ilAdministrationGUI
 //					if (strtolower($next_class) == strtolower($this->ctrl->getCmdClass()) ||
 //						"ilpermissiongui" == strtolower($this->ctrl->getCmdClass()))
 //					{
-						$ilHelp->setScreenIdComponent(ilObject::_lookupType($this->cur_ref_id,true));
+						$ilHelp->setScreenIdComponent(ilObject::_lookupType($this->cur_ref_id, true));
 //					}
 					$this->showTree();
-						
-					$this->ctrl->setReturn($this, "return");					
+
+					$this->ctrl->setReturn($this, "return");
 					$ret = $this->ctrl->forwardCommand($this->gui_obj);
 					$html = $this->gui_obj->getHTML();
 
-					if ($html != "")
-					{
+					if ($html != "") {
 						$this->tpl->setVariable("OBJECTS", $html);
 					}
 					$this->tpl->show();
-				}
-				else	// 
+				} else //
 				{
 					$cmd = $this->ctrl->getCmd("forward");
 					$this->$cmd();
@@ -269,54 +231,46 @@ class ilAdministrationGUI
 	function forward()
 	{
 		global $tree;
-		
-		if ($_GET["admin_mode"] != "repository")	// settings
-		{
-			if ($_GET["ref_id"] == USER_FOLDER_ID)
-			{
+
+		if ($_GET["admin_mode"] != "repository") {	// settings
+			if ($_GET["ref_id"] == USER_FOLDER_ID) {
 				$this->ctrl->setParameter($this, "ref_id", USER_FOLDER_ID);
 				$this->ctrl->setParameterByClass("iladministrationgui", "admin_mode", "settings");
-				if (((int) $_GET["jmpToUser"]) > 0 && ilObject::_lookupType((int)$_GET["jmpToUser"]) == "usr")
-				{
-					$this->ctrl->setParameterByClass("ilobjuserfoldergui", "jmpToUser",
-						(int)$_GET["jmpToUser"]);
+				if (((int) $_GET["jmpToUser"]) > 0 && ilObject::_lookupType((int)$_GET["jmpToUser"]) == "usr") {
+					$this->ctrl->setParameterByClass(
+						"ilobjuserfoldergui",
+						"jmpToUser",
+						(int)$_GET["jmpToUser"]
+					);
 					$this->ctrl->redirectByClass("ilobjuserfoldergui", "jumpToUser");
-				}
-				else
-				{
+				} else {
 					$this->ctrl->redirectByClass("ilobjuserfoldergui", "view");
 				}
-			}
-			else
-			{
+			} else {
 				$this->ctrl->setParameter($this, "ref_id", SYSTEM_FOLDER_ID);
 				$this->ctrl->setParameterByClass("iladministrationgui", "admin_mode", "settings");
 
-                if($_GET['fr'])
-                {
-                	// Security check: We do only allow relative urls
-                	$url_parts = parse_url(base64_decode(rawurldecode($_GET['fr'])));
-                	if($url_parts['http'] || $url_parts['host'])
-                	{
-                		global $ilias;
-                		
-                		$ilias->raiseError($this->lng->txt('permission_denied'), $ilias->error_obj->MESSAGE);
-                	}
-                	
-                    $fs_gui->setMainFrameSource(
-                        base64_decode(rawurldecode($_GET['fr'])));
-                    ilUtil::redirect(ILIAS_HTTP_PATH.'/'.base64_decode(rawurldecode($_GET['fr'])));
-                }
-                else
-                {
-                    $fs_gui->setMainFrameSource(
-                        $this->ctrl->getLinkTargetByClass("ilobjsystemfoldergui", "view"));
-                    $this->ctrl->redirectByClass("ilobjsystemfoldergui", "view");
-                }
+				if ($_GET['fr']) {
+					// Security check: We do only allow relative urls
+					$url_parts = parse_url(base64_decode(rawurldecode($_GET['fr'])));
+					if ($url_parts['http'] || $url_parts['host']) {
+						global $ilias;
+
+						$ilias->raiseError($this->lng->txt('permission_denied'), $ilias->error_obj->MESSAGE);
+					}
+
+					$fs_gui->setMainFrameSource(
+						base64_decode(rawurldecode($_GET['fr']))
+					);
+					ilUtil::redirect(ILIAS_HTTP_PATH.'/'.base64_decode(rawurldecode($_GET['fr'])));
+				} else {
+					$fs_gui->setMainFrameSource(
+						$this->ctrl->getLinkTargetByClass("ilobjsystemfoldergui", "view")
+					);
+					$this->ctrl->redirectByClass("ilobjsystemfoldergui", "view");
+				}
 			}
-		}
-		else
-		{
+		} else {
 			$this->ctrl->setParameter($this, "ref_id", ROOT_FOLDER_ID);
 			$this->ctrl->setParameterByClass("iladministrationgui", "admin_mode", "repository");
 			$this->ctrl->redirectByClass("ilobjrootfoldergui", "view");
@@ -329,38 +283,33 @@ class ilAdministrationGUI
 	function showTree()
 	{
 		global $tpl, $tree, $lng;
-		
-		if ($_GET["admin_mode"] != "repository")
-		{
+
+		if ($_GET["admin_mode"] != "repository") {
 			return;
 		}
-		
+
 		include_once("./Services/Administration/classes/class.ilAdministrationExplorerGUI.php");
 		$exp = new ilAdministrationExplorerGUI($this, "showTree");
-		if (!$exp->handleCommand())
-		{
+		if (!$exp->handleCommand()) {
 			$tpl->setLeftNavContent($exp->getHTML());
 		}
 	}
-	
+
 	/**
 	 * Special jump to plugin slot after ilCtrl has been reloaded
 	 */
 	function jumpToPluginSlot()
 	{
 		global $ilCtrl;
-		
+
 		$ilCtrl->setParameterByClass("ilobjcomponentsettingsgui", "ctype", $_GET["ctype"]);
 		$ilCtrl->setParameterByClass("ilobjcomponentsettingsgui", "cname", $_GET["cname"]);
 		$ilCtrl->setParameterByClass("ilobjcomponentsettingsgui", "slot_id", $_GET["slot_id"]);
-		
-		if($_GET["plugin_id"])
-		{
+
+		if ($_GET["plugin_id"]) {
 			$ilCtrl->setParameter($this, "plugin_id", $_GET["plugin_id"]);
 			$ilCtrl->redirectByClass("ilobjcomponentsettingsgui", "showPlugin");
-		}
-		else
-		{
+		} else {
 			$ilCtrl->redirectByClass("ilobjcomponentsettingsgui", "listPlugins");
 		}
 	}
@@ -376,13 +325,13 @@ class ilAdministrationGUI
 
 		$objects = $tree->getChilds(SYSTEM_FOLDER_ID);
 
-		foreach($objects as $object)
-		{
+		foreach ($objects as $object) {
 			$new_objects[$object["title"].":".$object["child"]]
 				= $object;
 			//have to set it manually as translation type of main node cannot be "sys" as this type is a orgu itself.
-			if($object["type"] == "orgu")
+			if ($object["type"] == "orgu") {
 				$new_objects[$object["title"].":".$object["child"]]["title"] = $lng->txt("obj_orgu");
+			}
 		}
 
 		// add entry for switching to repository admin
@@ -414,33 +363,27 @@ class ilAdministrationGUI
 
 		// determine items to show
 		$items = array();
-		foreach ($new_objects as $c)
-		{
+		foreach ($new_objects as $c) {
 			// check visibility
 			if ($tree->getParentId($c["ref_id"]) == ROOT_FOLDER_ID && $c["type"] != "adm" &&
-				$_GET["admin_mode"] != "repository")
-			{
+				$_GET["admin_mode"] != "repository") {
 				continue;
 			}
 			// these objects may exist due to test cases that didnt clear
 			// data properly
 			if ($c["type"] == "" || $c["type"] == "objf" ||
-				$c["type"] == "xxx")
-			{
+				$c["type"] == "xxx") {
 				continue;
 			}
 			$accessible = $rbacsystem->checkAccess('visible,read', $c["ref_id"]);
-			if (!$accessible)
-			{
+			if (!$accessible) {
 				continue;
 			}
 			if ($c["ref_id"] == ROOT_FOLDER_ID &&
-				!$rbacsystem->checkAccess('write', $c["ref_id"]))
-			{
+				!$rbacsystem->checkAccess('write', $c["ref_id"])) {
 				continue;
 			}
-			if ($c["type"] == "rolf" && $c["ref_id"] != ROLE_FOLDER_ID)
-			{
+			if ($c["type"] == "rolf" && $c["ref_id"] != ROLE_FOLDER_ID) {
 				continue;
 			}
 			$items[] = $c;
@@ -448,11 +391,10 @@ class ilAdministrationGUI
 
 		$cnt = 0;
 		$titems = array();
-		foreach ($items as $i)
-		{
+		foreach ($items as $i) {
 			$titems[$i["type"]] = $i;
 		}
-		
+
 		// admin menu layout
 		$layout = array(
 			1 => array(
@@ -463,37 +405,30 @@ class ilAdministrationGUI
 				),
 			2 => array(
 				"services" =>
-					array("pdts", "nwss", "tags", "prfa", "skmg", "cals", "mail", 
+					array("pdts", "nwss", "tags", "prfa", "skmg", "cals", "mail",
 						"---", "seas", "mds","cert", 'ecss', "pays", "extt")
 				),
 			3 => array(
 				"objects" =>
-					array("reps", "---", "blga", "chta", "crss", "facs", "frma", 
+					array("reps", "---", "blga", "chta", "crss", "facs", "frma",
 						"grps", "lrss", "mcts", "mobs", "svyf", "assf", "wbrs", "wiks",
 						"---", 'otpl',"root", "recf", "prgs")
 				)
 			);
-		
+
 		// now get all items and groups that are accessible
 		$groups = array();
-		for ($i = 1; $i <= 3; $i++)
-		{
+		for ($i = 1; $i <= 3; $i++) {
 			$groups[$i] = array();
-			foreach ($layout[$i] as $group => $entries)
-			{
+			foreach ($layout[$i] as $group => $entries) {
 				$groups[$i][$group] = array();
 				$entries_since_last_sep = false;
-				foreach ($entries as $e)
-				{
-					if ($e == "---" || $titems[$e]["type"] != "")
-					{
-						if ($e == "---" && $entries_since_last_sep)
-						{
+				foreach ($entries as $e) {
+					if ($e == "---" || $titems[$e]["type"] != "") {
+						if ($e == "---" && $entries_since_last_sep) {
 							$groups[$i][$group][] = $e;
 							$entries_since_last_sep = false;
-						}
-						else if ($e != "---")
-						{
+						} elseif ($e != "---") {
 							$groups[$i][$group][] = $e;
 							$entries_since_last_sep = true;
 						}
@@ -501,59 +436,62 @@ class ilAdministrationGUI
 				}
 			}
 		}
-		
+
 		include_once("./Services/UIComponent/GroupedList/classes/class.ilGroupedListGUI.php");
 		$gl = new ilGroupedListGUI();
-		
-		for ($i = 1; $i <= 3; $i++)
-		{
-			if ($i > 1)
-			{
+
+		for ($i = 1; $i <= 3; $i++) {
+			if ($i > 1) {
 				$gl->nextColumn();
 			}
-			foreach ($groups[$i] as $group => $entries)
-			{
-				if (count($entries) > 0)
-				{
+			foreach ($groups[$i] as $group => $entries) {
+				if (count($entries) > 0) {
 					$gl->addGroupHeader($lng->txt("adm_".$group));
-						
-					foreach ($entries as $e)
-					{
-						if ($e == "---")
-						{
+
+					foreach ($entries as $e) {
+						if ($e == "---") {
 							$gl->addSeparator();
-						}
-						else
-						{
+						} else {
 							$path = ilUtil::getImagePath("icon_".$titems[$e]["type"]."_s.png");
 							$icon = ($path != "")
 								? ilUtil::img($path)." "
 								: "";
-								
-							if ($_GET["admin_mode"] == "settings" && $titems[$e]["ref_id"] == ROOT_FOLDER_ID)
-							{
-								$gl->addEntry($icon.$titems[$e]["title"],
+
+							if ($_GET["admin_mode"] == "settings" && $titems[$e]["ref_id"] == ROOT_FOLDER_ID) {
+								$gl->addEntry(
+									$icon.$titems[$e]["title"],
 									"ilias.php?baseClass=ilAdministrationGUI&amp;ref_id=".
 									$titems[$e]["ref_id"]."&amp;admin_mode=repository",
-									"_top", "", "", "mm_adm_rep",
+									"_top",
+									"",
+									"",
+									"mm_adm_rep",
 									ilHelp::getMainMenuTooltip("mm_adm_rep"),
-									"bottom center", "top center", false);
-							}
-							else
-							{
-								$gl->addEntry($icon.$titems[$e]["title"],
+									"bottom center",
+									"top center",
+									false
+								);
+							} else {
+								$gl->addEntry(
+									$icon.$titems[$e]["title"],
 									"ilias.php?baseClass=ilAdministrationGUI&amp;ref_id=".
 										$titems[$e]["ref_id"]."&amp;cmd=jump",
-									"_top", "", "", "mm_adm_".$titems[$e]["type"],
+									"_top",
+									"",
+									"",
+									"mm_adm_".$titems[$e]["type"],
 									ilHelp::getMainMenuTooltip("mm_adm_".$titems[$e]["type"]),
-									"bottom center", "top center", false);
+									"bottom center",
+									"top center",
+									false
+								);
 							}
 						}
 					}
 				}
 			}
 		}
-		
+
 		//$gl->addSeparator();
 
 		echo $gl->getHTML();
@@ -576,5 +514,3 @@ class ilAdministrationGUI
 		$ilCtrl->redirectByClass($class, "view");
 	}
 }
-
-?>
