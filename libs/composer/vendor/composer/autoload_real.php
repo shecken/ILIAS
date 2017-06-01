f0f341c603a71cf512e5f56082626a5a38a111e1
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2017-06-01 14:56:54 +0200

    gev2: 3005 reinstall geshi from composer

--- libs/composer/vendor/composer/autoload_real.php
+++ libs/composer/vendor/composer/autoload_real.php
@@ -23,7 +23,7 @@ class ComposerAutoloaderInitdf10d65404ae6ccf02b102f486e8e6ea
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInitdf10d65404ae6ccf02b102f486e8e6ea', 'loadClassLoader'));
 
-        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
+        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION');
         if ($useStaticLoader) {
             require_once __DIR__ . '/autoload_static.php';
 
