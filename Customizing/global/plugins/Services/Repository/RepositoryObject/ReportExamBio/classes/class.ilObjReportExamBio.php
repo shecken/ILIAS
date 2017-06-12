ff609199f93f93425fe114e0f745414bee20b885
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2017-06-12 08:32:48 +0200

    gev2: 3154 Get title of parent course

--- Customizing/global/plugins/Services/Repository/RepositoryObject/ReportExamBio/classes/class.ilObjReportExamBio.php
+++ Customizing/global/plugins/Services/Repository/RepositoryObject/ReportExamBio/classes/class.ilObjReportExamBio.php
@@ -369,4 +369,26 @@ class ilObjReportExamBio extends ilObjReportBase2
 		}
 		return $return;
 	}
+
+	protected function getParentCourseId()
+	{
+		$data = $this->gTree->getParentNodeData($this->getRefId());
+		while ("crs" !== $data['type'] && (string)ROOT_FOLDER_ID !== (string)$data['ref_id']) {
+			$data = $this->gTree->getParentNodeData($data['ref_id']);
+		}
+		return ( "crs" === $data['type'] )
+			? $data['obj_id'] : null;
+	}
+
+	public function getParentCourseTitle()
+	{
+		$crs_id = $this->getParentCourseId();
+
+		if (!$crs_id) {
+			return null;
+		}
+
+		$crs = ilObjectFactory::getInstanceByObjId($crs_id);
+		return $crs->getTitle();
+	}
 }
