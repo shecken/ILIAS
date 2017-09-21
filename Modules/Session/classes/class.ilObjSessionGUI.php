cf7d4504e2bda8ec9bf89a6ec0ec0bd9bda0f428
Daniel Weise <daniel.weise@concepts-and-training.de>
2017-09-21 09:38:47 +0200

    show an error message if no tutor is selected

--- Modules/Session/classes/class.ilObjSessionGUI.php
+++ Modules/Session/classes/class.ilObjSessionGUI.php
@@ -1797,6 +1797,11 @@ class ilObjSessionGUI extends ilObjectGUI implements ilDesktopItemHandling
 		$this->object->setTutorSource((int)ilUtil::stripSlashes($_POST[self::INPUT_TUTOR_SOURCE]));
 		if($this->object->getTutorSource() === \ilObjSession::TUTOR_CFG_FROMCOURSE) {
 			$tids = $_POST[self::INPUT_TUTOR_SELECTION];
+			if(!is_array($tids))
+			{
+				ilUtil::sendFailure($this->lng->txt('sess_no_tutor_select'),TRUE);
+				$this->ctrl->redirect($this,'edit');
+			}
 			$this->object->setAssignedTutors($tids);
 		}
 		// cat-tms-patch end
