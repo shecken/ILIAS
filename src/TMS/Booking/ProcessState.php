0e2583e67d8c2eda9c7b00744e853bd7e740ed11
Richard Klees <richard.klees@concepts-and-training.de>
2017-10-11 15:15:38 +0200

    Booking: ProcessState stores data about steps.

--- src/TMS/Booking/ProcessState.php
+++ src/TMS/Booking/ProcessState.php
@@ -25,13 +25,19 @@ class ProcessState {
 	 */
 	protected $step_number;
 
-	public function __construct($crs_id, $usr_id, $step_number) {
+	/**
+	 * @var array<int,string>
+	 */
+	protected $step_data;
+
+	public function __construct($crs_id, $usr_id, $step_number, array $step_data = []) {
 		assert('is_int($crs_id)');
 		assert('is_int($usr_id)');
 		assert('is_int($step_number)');
 		$this->crs_id = $crs_id;
 		$this->usr_id = $usr_id;
 		$this->step_number = $step_number;
+		$this->step_data = $step_data;
 	}
 
 	/**
@@ -59,5 +65,43 @@ class ProcessState {
 	 */
 	public function getStepNumber() {
 		return $this->step_number;
+	}
+
+	/**
+	 * Set data for a certain step.
+	 *
+	 * @param	int		$step_number
+ 	 * @param	mixed	$data	needs to be json-serializable
+	 * @return	ProcessState
+	 */
+	public function withStepData($step_number, $data) {
+		$clone = clone $this;
+		$encoded = json_encode($data);
+		assert('is_string($encoded)');
+		$clone->step_data[$step_number] = $encoded;
+		return $clone;
+	}
+
+	/**
+	 * Get the data for a certain step.
+	 *
+	 * @param	int		$step_number
+	 * @throws	\OutOfBoundsException	if there is no data for the given key.
+	 * @return	mixed
+	 */
+	public function getStepData($step_number) {
+		if (!array_key_exists($step_number, $this->step_data)) {
+			throw new \OutOfBoundsException("No data for $step_number.");
+		}
+		return json_decode($this->step_data[$step_number]);
+	}
+
+	/**
+	 * Get all step data in an array.
+	 *
+	 * @return	array<int,string>
+	 */
+	public function getAllStepData() {
+		return $this->step_data;
 	}
 }
