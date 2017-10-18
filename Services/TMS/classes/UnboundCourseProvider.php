ce95b3221aa04ed0922c744fa1fe6cd95450f403
Richard Klees <richard.klees@concepts-and-training.de>
2017-10-18 08:32:08 +0200

    TMS: provide some info for course

--- Services/TMS/classes/UnboundCourseProvider.php
+++ Services/TMS/classes/UnboundCourseProvider.php
@@ -21,6 +21,21 @@ class UnboundCourseProvider extends Base {
 	 * @return  Component[]
 	 */
 	public function buildComponentsOf($component_type, Entity $entity) {
+		global $DIC;
+		$lng = $DIC["lng"];
+		$object = $entity->object();
+		if ($component_type === CourseInfo::class) {
+			return
+				[ new CourseInfoImpl
+					( $entity
+					, $lng->txt("title")
+					, $object->getTitle()
+					, ""
+					, 100
+					, [CourseInfo::CONTEXT_SEARCH_SHORT_INFO]
+					)
+				];
+		}
 		throw new \InvalidArgumentException("Unexpected component type '$component_type'");
 	}
 }
