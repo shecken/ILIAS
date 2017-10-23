288941f31bf5bbf6a504c20f3e0db8e2db84b173
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2017-10-23 10:51:28 +0200

    Add cancelUser to interface and update docstrings with exceptions

--- src/TMS/Booking/Actions.php
+++ src/TMS/Booking/Actions.php
@@ -19,8 +19,23 @@ interface Actions {
 	 *
 	 * @param	int		$crs_ref_id
 	 * @param	int		$user_id
+	 *
+	 * @throws \LogicException 	if user can't be booked to course or waitinglist
+	 *
 	 * @return	mixed	one of the STATEs
 	 */
 	public function bookUser($crs_ref_id, $user_id);
+
+	/**
+	 * Removes a user from course or waiting list
+	 *
+	 * @param	int		$crs_ref_id
+	 * @param	int		$user_id
+	 *
+	 * @throws \LogicException 	if user can't be canceld from course or waitinglist
+	 *
+	 * @return mixed	one of the STATEs
+	 */
+	public function cancelUser($crs_ref_id, $user_id) {
 }
 
