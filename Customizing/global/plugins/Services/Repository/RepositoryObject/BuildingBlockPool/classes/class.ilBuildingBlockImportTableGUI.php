aa0f9976c85d07f7676f4b485ff1c114f5c30153
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2017-11-20 10:08:37 +0100

    removing dbv hot topic fomt import table gui

--- Customizing/global/plugins/Services/Repository/RepositoryObject/BuildingBlockPool/classes/class.ilBuildingBlockImportTableGUI.php
+++ Customizing/global/plugins/Services/Repository/RepositoryObject/BuildingBlockPool/classes/class.ilBuildingBlockImportTableGUI.php
@@ -36,7 +36,6 @@ class ilBuildingBlockImportTableGUI extends ilTable2GUI {
 		
 		$this->addColumn($this->lng->txt("gev_dec_training_training_category"), 'training_categories');
 		$this->addColumn($this->lng->txt("gev_dec_training_gdv_topic"), 'gdv_topic');
-		$this->addColumn($this->lng->txt("gev_dec_training_dbv_topic"), 'dbv_topic');
 		$this->addColumn($this->lng->txt("gev_dec_training_topic"), 'topic');
 
 		$this->addColumn($this->lng->txt("gev_dec_building_block_is_wp_relevant"), "is_wp_relevant");
@@ -61,7 +60,6 @@ class ilBuildingBlockImportTableGUI extends ilTable2GUI {
 
 		$this->tpl->setVariable("TRAINING_CAT", implode("<br />",$a_set["training_categories"]));
 		$this->tpl->setVariable("GDV_TOPIC", $a_set["gdv_topic"]);
-		$this->tpl->setVariable("DBV_TOPIC", $a_set["dbv_topic"]);
 		$this->tpl->setVariable("TOPIC", $a_set["topic"]);
 
 		$this->tpl->setVariable("IS_WP_RELEVANT", ($a_set["is_wp_relevant"]) ? "Ja" : "Nein");
