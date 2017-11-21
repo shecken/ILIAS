28a76332f9c1a1d3cddc4de5a3a192392ee67c6d
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2017-11-21 14:06:11 +0100

    352 new helper trait for my users

---
+++ src/TMS/MyUsersHelper.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace ILIAS\TMS;
+
+trait MyUsersHelper {
+	/**
+	 * Get user ids and fullname of user, where current ilUser is allowed to book for
+	 *
+	 * @param int 	$superior_user_id
+	 *
+	 * @return string[]
+	 */
+	public function getUserWhereCurrentCanBookFor($superior_user_id) {
+		require_once("Services/User/classes/class.ilObjUser.php");
+		$ret = array();
+		$employees = $this->getEmployesUnderUser($superior_user_id);
+		$superiors = $this->getSuperiorsUnderUser($superior_user_id);
+
+		$members = array_unique(array_merge($employees, $superiors));
+		foreach($members as $user_id) {
+			$ret[$user_id] = \ilObjUser::_lookupFullname($user_id);
+		}
+
+		uasort($ret, function($a, $b) {
+			return strcmp($a, $b);
+		});
+
+		return $ret;
+	}
+
+	/**
+	 * Returns employees under current user
+	 *
+	 * @param int 	$superior_user_id
+	 *
+	 * @return int[]
+	 */
+	protected function getEmployesUnderUser($superior_user_id) {
+		require_once("Modules/OrgUnit/classes/class.ilObjOrgUnitTree.php");
+		return \ilObjOrgUnitTree::_getInstance()->getEmployeesUnderUser($superior_user_id, true);
+	}
+
+	/**
+	 * Returns superiors under current user
+	 *
+	 * @param int 	$superior_user_id
+	 *
+	 * @return int[]
+	 */
+	protected function getSuperiorsUnderUser($superior_user_id) {
+		require_once("Modules/OrgUnit/classes/class.ilObjOrgUnitTree.php");
+		return \ilObjOrgUnitTree::_getInstance()->getSuperiorsUnderUser($superior_user_id, true);
+	}
+}
