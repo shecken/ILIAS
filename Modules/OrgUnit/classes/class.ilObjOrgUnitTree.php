5de605c2881f632e0cad7b4fe80734402bd98ca2
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2017-11-21 13:36:04 +0100

    352 patch to get superiors of orgunits where user is superior

--- Modules/OrgUnit/classes/class.ilObjOrgUnitTree.php
+++ Modules/OrgUnit/classes/class.ilObjOrgUnitTree.php
@@ -311,6 +311,33 @@ class ilObjOrgUnitTree {
 		return $employees;
 	}
 
+	// cat-tms-patch start (352)
+	/**
+	 * @param $user_id   int
+	 * @param $recursive bool if this is true subsequent orgunits of this users superior role get searched as well.
+	 * @return int[] returns an array of user_ids of the users which have an superior role in an orgunit of which this user's id has a superior role.
+	 */
+	public function getSuperiorsUnderUser($user_id, $recursive = true) {
+		//querry for all orgu where user_id is superior.
+		$q = "SELECT orgu.obj_id, refr.ref_id FROM object_data orgu
+                INNER JOIN object_reference refr ON refr.obj_id = orgu.obj_id
+				INNER JOIN object_data roles ON roles.title LIKE CONCAT('il_orgu_superior_',refr.ref_id)
+				INNER JOIN rbac_ua rbac ON rbac.usr_id = " . $this->db->quote($user_id, "integer") . " AND roles.obj_id = rbac.rol_id
+				WHERE orgu.type = 'orgu'";
+		$set = $this->db->query($q);
+		$orgu_ref_ids = array();
+		while ($res = $this->db->fetchAssoc($set)) {
+			$orgu_ref_ids[] = $res['ref_id'];
+		}
+		$employees = array();
+		foreach ($orgu_ref_ids as $orgu_ref_id) {
+			$employees = array_merge($employees, $this->getSuperiors($orgu_ref_id, $recursive));
+		}
+
+		return $employees;
+	}
+	// cat-tms-patch end
+
 
 	/**
 	 * @param $user_id   int
