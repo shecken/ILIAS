e2ceb8c8b7fb7aa90305575f84cf8d29dd8e71a1
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2017-11-27 10:18:20 +0100

    reverted remove wbd-adata in edubio

--- Customizing/global/plugins/Services/Repository/RepositoryObject/ReportEduBio/classes/class.ilObjReportEduBio.php
+++ Customizing/global/plugins/Services/Repository/RepositoryObject/ReportEduBio/classes/class.ilObjReportEduBio.php
@@ -61,7 +61,8 @@ class ilObjReportEduBio extends ilObjReportBase
 				->column("tutor", $this->plugin->txt("crs_tutor"), true)
 				->column("fee", $this->plugin->txt("fee"), true)
 				->column("status", $this->plugin->txt("status"), true)
-				->column("credit_points", $this->plugin->txt("wb_time"), true, "40px")
+				->column("credit_points", $this->plugin->txt("points"), true, "40px")
+				->column("wbd_reported", $this->plugin->txt("wbd_reported"), true)
 				->column("action", '<img src="'.ilUtil::getImagePath("gev_action.png").'" />', true, "", true, false);
 		return parent::buildTable($table);
 	}
@@ -123,6 +124,13 @@ class ilObjReportEduBio extends ilObjReportBase
 				->select("usrcrs.booking_status")
 				->select("usrcrs.certificate_filename")
 				->select("oref.ref_id")
+				->select_raw('IF('.$this->gIldb->in('usrcrs.okz', array('OKZ1','OKZ2','OZ3'), false, 'text').' AND usrcrs.credit_points > 0 AND usrcrs.credit_points IS NOT NULL'
+								.'	,IF(usrcrs.wbd_booking_id != '.$this->gIldb->quote('-empty-', 'text').' AND usrcrs.wbd_booking_id IS NOT NULL AND usrcrs.wbd_cancelled != 1'
+								.'		,'.$this->gIldb->quote('Ja', 'text')
+								.'		,IF((usrcrs.end_date > '.$this->gIldb->quote($one_year_befone_now, 'date')
+								.'				OR (usrcrs.end_date = '.$this->gIldb->quote($one_year_befone_now, 'date').' AND usrcrs.begin_date >= '.$this->gIldb->quote($one_year_befone_now, 'date').' ) )'
+								.'			,'.$this->gIldb->quote('Nein', 'text').',\'-\')'
+								.'),\'-\') as wbd_reported')
 				->from("hist_usercoursestatus usrcrs")
 				->join("hist_user usr")
 					->on("usr.user_id = usrcrs.usr_id AND usr.hist_historic = 0")
@@ -138,6 +146,99 @@ class ilObjReportEduBio extends ilObjReportBase
 	{
 		parent::prepareReport();
 		$this->target_user_utils = gevUserUtils::getInstance($this->target_user_id);
+		$this->wbd_data = $this->getWBDData();
+		$this->academy_points = $this->getAcademyData();
+	}
+
+	protected function getWBDData()
+	{
+		require_once("Services/Calendar/classes/class.ilDatePresentation.php");
+		$wbd_data = array();
+		$wbd = $this->getWBD();
+
+		$cy_start = $wbd->getStartOfCurrentCertificationYear();
+		$cy_end = $wbd->getStartOfCurrentCertificationYear();
+		$cy_end->increment(ilDateTime::YEAR, 1);
+
+		$cp_start = $wbd->getStartOfCurrentCertificationPeriod();
+		$cp_end = $wbd->getStartOfCurrentCertificationPeriod();
+		$cp_end->increment(ilDateTime::YEAR, 5);
+
+		$wbd_data["cert_period"] = ilDatePresentation::formatPeriod($cp_start, $cp_end);
+		$wbd_data["cert_year"] = ilDatePresentation::formatPeriod($cy_start, $cy_end);
+
+		$period = $this->filter->get("period");
+
+		$query = $this->wbdQuery($period["start"], $period["end"]);
+		$wbd_data["sum"] =  $this->gIldb->fetchAssoc($this->gIldb->query($query))["sum"];
+
+		$query = $this->wbdQuery($cp_start, $cp_end);
+		$wbd_data["sum_cert_period"] = $this->gIldb->fetchAssoc($this->gIldb->query($query))["sum"];
+		return $wbd_data;
+	}
+
+	protected function getAcademyData()
+	{
+		$period = $this->filter->get("period");
+		$academy_points = array();
+
+		$query = $this->academyQuery($period["start"], $period["end"], false);
+		$academy_points["to_transfer_sum"] = $this->gIldb->fetchAssoc($this->gIldb->query($query))["sum"];
+
+		$query = $this->academyQuery($period["start"], $period["end"], true);
+		$academy_points["transfered_sum"] = $this->gIldb->fetchAssoc($this->gIldb->query($query))["sum"];
+		return $academy_points;
+	}
+
+	protected function academyQuery(ilDate $start = null, ilDate $end = null, $transferred)
+	{
+		$one_year_befone_now  = (new DateTime())->sub(new DateInterval('P1Y'))->format('Y-m-d');
+		return 	"SELECT SUM(usrcrs.credit_points) sum "
+				."	FROM hist_usercoursestatus usrcrs "
+				."	JOIN hist_course crs"
+				."		ON crs.crs_id = usrcrs.crs_id "
+				."	WHERE usrcrs.hist_historic = 0 "
+				."		AND usrcrs.usr_id = ".$this->gIldb->quote($this->target_user_id, 'integer')
+				."		AND crs.hist_historic = 0 "
+				."		AND ".$this->gIldb->in('usrcrs.okz', array('OKZ1','OKZ2','OZ3'), false, 'text')
+				."		AND usrcrs.participation_status = ".$this->gIldb->quote('teilgenommen', 'text')
+				."		AND ".$this->gIldb->in('usrcrs.function', array('Mitglied', 'Teilnehmer'), false, 'text')
+				."		AND usrcrs.booking_status = ".$this->gIldb->quote('gebucht', 'text')
+				."		AND usrcrs.credit_points > 0 "
+				."		AND ( usrcrs.end_date >= ".$this->gIldb->quote($start->get(IL_CAL_DATE), "date")
+				."			OR usrcrs.end_date = '0000-00-00')"
+				."		AND usrcrs.begin_date <= ".$this->gIldb->quote($end->get(IL_CAL_DATE), "date")
+				."		AND ((".$this->gIldb->in("crs.type", array('Selbstlernkurs'), false, 'text')
+				."				AND usrcrs.begin_date > ".$this->gIldb->quote('2013-01-01', 'date').")"
+				."			OR (".$this->gIldb->in("crs.type", array('Selbstlernkurs'), true, 'text')
+				."				AND usrcrs.end_date > ".$this->gIldb->quote('2013-01-01', 'date')."))"
+				."		AND (usrcrs.wbd_booking_id IS ".($transferred ? "NOT" : "")." NULL "
+				." 			".($transferred ? "AND" : "OR")." usrcrs.wbd_booking_id ".($transferred ? "!=" : "=")." '-empty-')"
+				."		AND usrcrs.wbd_cancelled != 1 "
+				."		AND (usrcrs.last_wbd_report IS ".($transferred ? "NOT" : "")." NULL "
+				." 			".($transferred ? "AND" : "OR")." usrcrs.last_wbd_report ".($transferred ? "!=" : "=")." '0000-00-00')"
+				."		".(!$transferred ? " AND usrcrs.end_date > ".$this->gIldb->quote($one_year_befone_now, 'date') : "");
+	}
+
+	protected function wbdQueryWhere(ilDate $start = null, ilDate $end = null)
+	{
+		return $start === null ?
+				$this->queryWhere() :
+				" WHERE usrcrs.usr_id = ".$this->gIldb->quote($this->target_user_id, "integer")
+				."   AND usrcrs.hist_historic = 0 "
+				."   AND ( usrcrs.end_date >= ".$this->gIldb->quote($start->get(IL_CAL_DATE), "date")
+				."        OR usrcrs.end_date = '0000-00-00')"
+				."   AND usrcrs.begin_date <= ".$this->gIldb->quote($end->get(IL_CAL_DATE), "date")
+				."   AND ".$this->gIldb->in("usrcrs.booking_status", array("gebucht", "kostenpflichtig storniert", "kostenfrei storniert"), false, "text");
+	}
+
+	protected function wbdQuery(ilDate $start, ilDate $end)
+	{
+		return   "SELECT SUM(usrcrs.credit_points) sum "
+				." FROM hist_usercoursestatus usrcrs"
+				.$this->wbdQueryWhere($start, $end)
+				." AND usrcrs.wbd_booking_id IS NOT NULL AND usrcrs.wbd_booking_id != '-empty-'"
+				." AND usrcrs.wbd_cancelled != 1";
 	}
 
 	public function validateBill($bill_id)
--- Customizing/global/plugins/Services/Repository/RepositoryObject/ReportEduBio/classes/class.ilObjReportEduBioGUI.php
+++ Customizing/global/plugins/Services/Repository/RepositoryObject/ReportEduBio/classes/class.ilObjReportEduBioGUI.php
@@ -102,7 +102,49 @@ class ilObjReportEduBioGUI extends ilObjReportBaseGUI {
 		return 	$this->title->render()
 				.($this->object->deliverFilter() !== null ? $this->object->deliverFilter()->render() : "")
 				.($this->spacer !== null ? $this->spacer->render() : "")
+				.($this->object->getWBD()->getWBDTPType() !== gevWBD::WBD_NO_SERVICE ?
+					$this->renderOverview().($this->spacer !== null ? $this->spacer->render() : "") : "")
 				.$this->renderTable();
+	}
+
+	protected function renderOverview() {
+		$tpl = new ilTemplate("tpl.gev_edu_bio_overview.html", true, true, $this->object->plugin->getDirectory());
+		$this->insertAcademyPoints($tpl);
+		if($this->object->getWBD()->getWBDTPType() === gevWBD::WBD_TP_SERVICE ) {
+			$this->insertWBDPoints($tpl);
+			if ($this->object->getWBD()->transferPointsFromWBD()) {
+				$tpl->setVariable("WBDPOINTSVISIBIBLE", "visible");
+			} else {
+				$tpl->setVariable("WBDPOINTSVISIBIBLE", "invisible");
+				if($this->object->getWBD()->transferPointsToWBD()) {
+					ilUtil::sendInfo($this->plugin->txt("wbd_transfer_on"));
+				} elseif($this->object->getWBD()->wbdRegistrationIsPending()) {
+					ilUtil::sendInfo($this->plugin->txt("wbd_reg_pending"));
+				}
+			}
+		} else {
+			$tpl->setVariable("WBDPOINTSVISIBIBLE", "invisible");
+		}
+		return $tpl->get();
+	}
+
+	protected function insertAcademyPoints($tpl) {
+		$tpl->setVariable("ACA_TRANSFERED_SUM_TITLE", $this->object->plugin->txt("aca_transferred_points_filter"));
+		$tpl->setVariable("ACA_TO_TRANSFER_SUM_TITLE", $this->object->plugin->txt("aca_to_transdfer_points_filter"));
+		$aux = $this->object->academy_points["transfered_sum"];
+		$tpl->setVariable("ACA_TRANSFERED_SUM", $aux ? $aux : 0);
+		$aux = $this->object->academy_points["to_transfer_sum"];
+		$tpl->setVariable("ACA_TO_TRANSFER_SUM", $aux ? $aux : 0);
+	}
+
+	protected function insertWBDPoints($tpl) {
+		$tpl->setVariable("WBD_SUM_TITLE", $this->object->plugin->txt("points_in_wbd"));
+		$tpl->setVariable("WBD_SUM_CERT_PERIOD_TITLE", $this->object->plugin->txt("points_in_wbd_cert_period"));
+		$tpl->setVariable("WBD_CERT_PERIOD", $this->object->wbd_data["cert_period"]);
+		$aux = $this->object->wbd_data["sum"];
+		$tpl->setVariable("WBD_SUM", $aux ? $aux : 0);
+		$aux = $this->object->wbd_data["sum_cert_period"];
+		$tpl->setVariable("WBD_SUM_CERT_PERIOD", $aux ? $aux : 0);
 	}
 
 	protected function getBill() {
@@ -215,6 +257,9 @@ class ilObjReportEduBioGUI extends ilObjReportBaseGUI {
 
 		$rec['credit_points'] = gevCourseUtils::convertCreditpointsToFormattedDuration((int)$rec['credit_points']);
 
+		$rec["wbd"] = in_array($rec["okz"], array("OKZ1", "OKZ2", "OKZ3"))
+					? $lng->txt("yes")
+					: $lng->txt("no");
 		return parent::transformResultRowXLSX($rec);
 	}
 
