3b67cf40b562c254fce41bb46663700a337a6dc3
Nils Haagen <nhaagen@cat06.de>
2017-11-29 16:31:08 +0100

    dunno if good, but I think this is a service; at least, the dependency-construction overhead is way smaller this way.

--- src/TMS/Mailing/Actions.php
+++ src/TMS/Mailing/Actions.php
@@ -8,13 +8,43 @@ namespace ILIAS\TMS\Mailing;
  * This encapsulates basic functionality for mailing.
  */
 interface Actions {
+
 	const BOOKED_ON_COURSE = "B01";
 	const BOOKED_ON_WAITINGLIST = "B02";
 	const CANCELED_FROM_COURSE = "C01";
 	const CANCELED_FROM_WAITINGLIST = "C02";
 
 	/**
-	 * Sends a mail for a course to the user.
+	 * Get a template's data by its ident, e.g. "B01".
+	 *
+	 * @param string 	$ident
+	 * @return array<string, string> | null
 	 */
-	public function sendCourseMail($mail_id, $crs_ref_id, $user_id);
+	public function getTemplateDataByIdent($ident);
+
+	/**
+	 * Get an instance of content-builder.
+	 * @return ContentBuilder
+	 */
+	public function getContentBuilder();
+
+	/**
+	 * Get an instance of the logging db.
+	 * @return LoggingDB
+	 */
+	public function getMailLogDB();
+
+	/**
+	 * Get the sender of ilias-mails
+	 * @return Recipient
+	 */
+	public function getStandardSender();
+
+	/**
+	 * Get the TMS Clerk
+	 *
+	 * @return TMSMailClerk
+	 */
+	public function getClerk();
+
 }
