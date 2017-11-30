b548237260c32f9b63d6dad0695b2031853e1746
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2017-11-30 16:42:22 +0100

    381 provide link to course member view

--- src/TMS/CourseInfo.php
+++ src/TMS/CourseInfo.php
@@ -20,6 +20,7 @@ interface CourseInfo extends Component {
 	const CONTEXT_USER_CAN_CANCEL = 8;
 	const CONTEXT_USER_CAN_BOOK = 9;
 	const CONTEXT_TO_COURSE_BUTTON = 10;
+	const CONTEXT_COURSE_MEMBER_BUTTON = 11;
 
 	/**
 	 * Get a label for this step in the process.
