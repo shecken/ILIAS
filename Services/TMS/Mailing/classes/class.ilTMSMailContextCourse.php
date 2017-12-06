25383dec6bf282e8155275f3d2a945d5792490df
Nils Haagen <nhaagen@cat06.de>
2017-12-06 10:35:31 +0100

    SCHEDULE-placeholder in course-context

--- Services/TMS/Mailing/classes/class.ilTMSMailContextCourse.php
+++ Services/TMS/Mailing/classes/class.ilTMSMailContextCourse.php
@@ -10,17 +10,13 @@ class ilTMSMailContextCourse implements Mailing\MailContext {
 	private static $PLACEHOLDER = array(
 		'COURSE_TITLE' => 'crsTitle',
 		'COURSE_LINK' => 'crsLink',
+		'SCHEDULE' => 'crsSchedule'
 	);
 
 	/**
 	 * @var int
 	 */
 	protected $crs_ref_id;
-
-	/**
-	 * @var ilObjCourse
-	 */
-	protected $crs;
 
 	public function __construct($crs_ref_id) {
 		assert('is_int($crs_ref_id)');
@@ -68,5 +64,84 @@ class ilTMSMailContextCourse implements Mailing\MailContext {
 		require_once './Services/Link/classes/class.ilLink.php';
 		return ilLink::_getLink($this->getCourseRefId(), 'crs');
 	}
+
+
+	/**
+	 * @return string
+	 */
+	public function crsSchedule() {
+		$schedule = array();
+		$sessions = $this->getSessionAppointments();
+		foreach ($sessions as $sortdat => $times) {
+			list($date, $start, $end) = $times;
+			$schedule[] = sprintf("%s, %s - %s", $date, $start, $end);
+		}
+		return implode('<br>', $schedule);
+	}
+
+
+
+	/**
+	 * Get session appointments from within the course
+	 *
+	 * @param Entity $entity
+	 * @param Object 	$object
+	 *
+	 * @return string
+	 */
+	protected function getSessionAppointments() {
+		$vals = array();
+		$sessions = $this->getAllChildrenOfByType($this->getCourseRefId(), "sess");
+
+		if(count($sessions) > 0) {
+			foreach ($sessions as $session) {
+				$appointment = $session->getFirstAppointment();
+				$sort_date = $appointment->getStart()->get(IL_CAL_FKT_DATE, "Ymd");
+				$start_date = $appointment->getStart()->get(IL_CAL_FKT_DATE, "d.m.Y");
+				$start_time = $appointment->getStart()->get(IL_CAL_FKT_DATE, "H:i");
+				$end_time = $appointment->getEnd()->get(IL_CAL_FKT_DATE, "H:i");
+				$vals[$sort_date] = array($start_date, $start_time, $end_time);
+			}
+		}
+
+		ksort($vals, SORT_NUMERIC);
+		return $vals;
+	}
+
+
+
+	/**
+	 * Get all children by type recursive
+	 *
+	 * @param int 	$ref_id
+	 * @param string 	$search_type
+	 *
+	 * @return Object 	of search type
+	 */
+	protected function getAllChildrenOfByType($ref_id, $search_type) {
+		global $DIC;
+		$g_tree = $DIC->repositoryTree();
+		$g_objDefinition = $DIC["objDefinition"];
+
+		$childs = $g_tree->getChilds($ref_id);
+		$ret = array();
+
+		foreach ($childs as $child) {
+			$type = $child["type"];
+			if($type == $search_type) {
+				$ret[] = \ilObjectFactory::getInstanceByRefId($child["child"]);
+			}
+
+			if($g_objDefinition->isContainer($type)) {
+				$rec_ret = $this->getAllChildrenOfByType($child["child"], $search_type);
+				if(! is_null($rec_ret)) {
+					$ret = array_merge($ret, $rec_ret);
+				}
+			}
+		}
+
+		return $ret;
+	}
+
 
 }
