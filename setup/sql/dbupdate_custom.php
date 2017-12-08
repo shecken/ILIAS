5106a8cf24a76b3e0a9fbb4cb1d1d5e3662f2455
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2017-12-08 09:01:49 +0100

    Switch column names

--- setup/sql/dbupdate_custom.php
+++ setup/sql/dbupdate_custom.php
@@ -21,3 +21,10 @@ if( !$ilDB->tableExists('crs_copy_mappings') )
 	$ilDB->addPrimaryKey('crs_copy_mappings', array('obj_id', 'source_id'));
 }
 ?>
+
+<#2>
+<?php
+$ilDB->renameTableColumn('crs_copy_mappings', "obj_id", "obj_id2");
+$ilDB->renameTableColumn('crs_copy_mappings', "source_id", "obj_id");
+$ilDB->renameTableColumn('crs_copy_mappings', "obj_id2", "source_id");
+?>
