cee40e3bfda77a762a1ca042235899e60c11144b
daniwe4 <daniel.weise@concepts-and-training.de>
2017-12-19 15:20:42 +0000

    remove method getAttachments

--- src/TMS/Mailing/MailingOccasion.php
+++ src/TMS/Mailing/MailingOccasion.php
@@ -43,11 +43,4 @@ interface MailingOccasion extends Component {
 	 */
 	public function getMails($event, $parameter);
 
-	/**
-	 * Get attachments for mail.
-	 *
-	 * @return 	Attachments
-	 */
-	public function getAttachments();
-
 }
