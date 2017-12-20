0a64180459b43f67edf46b7a070a93ee0ba5760a
daniwe4 <daniel.weise@concepts-and-training.de>
2017-12-20 11:47:31 +0000

    implement interface ICalBuilder

---
+++ src/TMS/Mailing/ICalBuilder.php
@@ -0,0 +1,27 @@
+<?php
+namespace ILIAS\TMS\Mailing;
+
+/**
+ * Interface ICalBuilder
+ *
+ * @author Daniel Weise <daniel.weise@concepts-and-training.de>
+ * @copyright Extended GPL, see LICENSE
+ */
+interface ICalBuilder
+{
+	/**
+	 * Get an iCal file path for crs_id.
+	 *
+	 * @param 	array 	$info
+	 * @return 	string 	path to iCal file
+	 */
+	public function getICalFilePath(array $info);
+
+	/**
+	 * Get an iCal string for crs_id.
+	 *
+	 * @param 	array 	$info
+	 * @return 	string
+	 */
+	public function getIcalString(array $info);
+}
