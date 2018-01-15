a9283585a73d24e8c9004760e7f400edc2a232b9
Richard Klees <richard.klees@concepts-and-training.de>
2018-01-15 16:13:01 +0100

    added update step for lib-ente

--- setup/sql/dbupdate_custom.php
+++ setup/sql/dbupdate_custom.php
@@ -109,3 +109,14 @@ while($row = $ilDB->fetchAssoc($res)) {
 	$ilCtrlStructureReader->getStructure();
 	// cat-tms-patch end
 ?>
+
+<#11>
+<?php
+	global $DIC;
+	require_once("Services/Tree/classes/class.ilTree.php");
+	$tree = new ilTree(0);
+	require_once("Services/Object/classes/class.ilObjectDataCache.php");
+	$cache = new ilObjectDataCache();
+	$provider_db = new CaT\Ente\ILIAS\ilProviderDB($DIC->database(), $tree, $cache);
+	$provider_db->createTables();
+?>
