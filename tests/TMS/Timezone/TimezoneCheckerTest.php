d3beec4c635664ee7b34fb0811d6e48c333eb110
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2018-01-31 09:37:39 +0100

    489 use DateTime objects and extended with more cases

--- tests/TMS/Timezone/TimezoneCheckerTest.php
+++ tests/TMS/Timezone/TimezoneCheckerTest.php
@@ -3,35 +3,59 @@
 use ILIAS\TMS\Timezone;
 
 class TimezoneCheckerTest {
-	public function test_SummerTimezone() {
+	public function test_SummerTime() {
 		$db = $this->getMockBuilder(Timezone\TimezoneDB::class)
 			->setMethods(["readFor"])
 			->disableOriginalConstructor()
 			->getMock();
 
+		$start_summer = new DateTime(strtotime("25.03.2018"));
+		$start_winter = new DateTime(strtotime("28.10.2018"));
+
 		$db
-			->expects($this->once())
+			->expects($this->exactly(6))
 			->method("readFor")
-			->will(array("start_summer" => "25.03.2018", "start_winter" => "28.10.2018"));
+			->will(array("start_summer" => $start_summer, "start_winter" => $start_winter));
 
 		$checker = new Timezone\TimezoneCheckerImpl($db);
-		$this->assertTrue($checker->isSummerTimeZone("25.03.2018"));
-		$this->assertFalse($checker->isWinterTimeZone("25.03.2018"));
+		$checktime = new DateTime(strtotime("25.03.2018"));
+		$this->assertTrue($checker->isSummerTime($checktime));
+		$this->assertFalse(!$checker->isSummerTime($checktime));
+
+		$checktime = new DateTime(strtotime("27.10.2018"));
+		$this->assertTrue($checker->isSummerTime($checktime));
+		$this->assertFalse(!$checker->isSummerTime($checktime));
+
+		$checktime = new DateTime(strtotime("19.06.2018"));
+		$this->assertTrue($checker->isSummerTime($checktime));
+		$this->assertFalse(!$checker->isSummerTime($checktime));
 	}
 
-	public function test_WinterTimezone() {
+	public function test_WinterTime() {
 		$db = $this->getMockBuilder(Timezone\TimezoneDB::class)
 			->setMethods(["readFor"])
 			->disableOriginalConstructor()
 			->getMock();
 
+		$start_summer = new DateTime(strtotime("25.03.2018"));
+		$start_winter = new DateTime(strtotime("28.10.2018"));
+
 		$db
-			->expects($this->once())
+			->expects($this->exactly(6))
 			->method("readFor")
-			->will(array("start_summer" => "25.03.2018", "start_winter" => "28.10.2018"));
+			->will(array("start_summer" => $start_summer, "start_winter" => $start_winter));
 
 		$checker = new Timezone\TimezoneCheckerImpl($db);
-		$this->assertTrue($checker->isWinterTimeZone("28.10.2018"));
-		$this->assertFalse($checker->isSummerTimeZone("28.10.2018"));
+		$checktime = new DateTime(strtotime("28.10.2018"));
+		$this->assertTrue(!$checker->isSummerTime($checktime));
+		$this->assertFalse($checker->isSummerTime($checktime));
+
+		$checktime = new DateTime(strtotime("31.12.2018"));
+		$this->assertTrue(!$checker->isSummerTime($checktime));
+		$this->assertFalse($checker->isSummerTime($checktime));
+
+		$checktime = new DateTime(strtotime("03.10.2018"));
+		$this->assertTrue(!$checker->isSummerTime($checktime));
+		$this->assertFalse($checker->isSummerTime($checktime));
 	}
 }
