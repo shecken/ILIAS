6fac13989cf2b97cf4cf44496402d94e9d36dbc7
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2018-02-05 17:11:48 +0100

    187 new action to get to the members tab of course

---
+++ Services/TMS/CourseActions/ToCourseMemberTab.php
@@ -0,0 +1,61 @@
+<?php
+
+/* Copyright (c) 2018 Stefan Hecken <stefan.hecken@concepts-and-training.de> */
+
+use ILIAS\TMS;
+
+/**
+ * This is an information about a course action, noteworthy for a user in some context.
+ */
+class ToCourseMemberTab extends TMS\CourseActionImpl
+{
+	/**
+	 * @inheritdoc
+	 */
+	public function isAllowedFor($usr_id)
+	{
+		$course = $this->entity->object();
+		var_dump($this->hasAccess($course->getRefId()));
+		return $this->hasAccess($course->getRefId());
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function getLink(\ilCtrl $ctrl, $usr_id)
+	{
+		$course = $this->entity->object();
+
+		require_once("Services/Link/classes/class.ilLink.php");
+		return ilLink::_getStaticLink($course->getRefId(), "crs", true, "_mem");
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function getLabel()
+	{
+		global $DIC;
+		$lng = $DIC->language();
+		return $lng->txt("to_course_member_tab");
+	}
+
+	/**
+	 * Has user read access to the course
+	 *
+	 * @param int 	$crs_ref_id
+	 *
+	 * @return bool
+	 */
+	protected function hasAccess($crs_ref_id)
+	{
+		global $DIC;
+		$access = $DIC->access();
+		$course = $this->entity->object();
+		if($access->checkAccess("manage_members", "", $crs_ref_id)) {
+			return true;
+		}
+
+		return false;
+	}
+}
