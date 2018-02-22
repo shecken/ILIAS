24113316a9e61587c1aeb71a25f89289818b0127
Richard Klees <richard.klees@concepts-and-training.de>
2018-02-22 15:38:53 +0100

    TMS\CourseCreation: turned RequestBuilder into a richer interface.
    In this way it can be implemented in some database class. This seems
    to be a good idea, since the builder actually needs to create ids, which
    is what the database does in our standard implementations.

--- src/TMS/CourseCreation/RequestBuilder.php
+++ src/TMS/CourseCreation/RequestBuilder.php
@@ -7,5 +7,29 @@ namespace ILIAS\TMS\CourseCreation;
 /**
  * Build requests from data given to it.
  */
-class RequestBuilder {
+interface RequestBuilder {
+	/**
+	 * Set the user and session id for the requests.
+	 *
+	 * @var	int		$user_id
+	 * @var string	$session_id
+	 * @return	self
+	 */
+	public function setUserIdAndSessionId($user_id, $session_id);
+
+	/**
+	 * Set the course id for the request.
+	 *
+	 * @var	int		$crs_ref_id
+	 * @return	self
+	 */
+	public function setCourseRefId($crs_ref_id);
+
+	/**
+	 * Get the request object requested as of given timestamp.
+	 *
+	 * @var	\DateTime	$requested_ts
+	 * @return	Request
+	 */
+	public function getRequest();
 }
