e228b8189eea3495f206f8b262442e95cec946d9
daniwe4 <daniel.weise@concepts-and-training.de>
2018-02-28 11:14:23 +0000

    ajust db query for correct credit_points handling

--- Customizing/global/plugins/Services/Repository/RepositoryObject/ReportEduBio/classes/class.ilObjReportEduBio.php
+++ Customizing/global/plugins/Services/Repository/RepositoryObject/ReportEduBio/classes/class.ilObjReportEduBio.php
@@ -108,11 +108,11 @@ class ilObjReportEduBio extends ilObjReportBase
 				->select("crs.tutor")
 				->select_raw('IF('.$this->gIldb->in('usrcrs.okz', array('OKZ1','OKZ2','OZ3'), false, 'text')
 								.'	,IF(usrcrs.wbd_booking_id != '.$this->gIldb->quote('-empty-', 'text').' AND usrcrs.wbd_booking_id IS NOT NULL AND usrcrs.wbd_cancelled != 1'
-								.'		,usrcrs.credit_points/3'
+								.'		,usrcrs.credit_points'
 								.'		,IF((usrcrs.end_date > '.$this->gIldb->quote($one_year_befone_now, 'date')
 												.'OR (usrcrs.end_date = '.$this->gIldb->quote($one_year_befone_now, 'date').' AND usrcrs.begin_date >= '.$this->gIldb->quote($one_year_befone_now, 'date').' ) )'
 												.' AND usrcrs.credit_points > 0 AND usrcrs.credit_points IS NOT NULL AND usrcrs.wbd_cancelled != 1'
-								.'			,usrcrs.credit_points/3,\'-\')'
+								.'			,usrcrs.credit_points,\'-\')'
 								.'	)'
 								.'	,\'-\''
 								.') as credit_points')
