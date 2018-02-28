e01b59c24b3abd5db5b5dce36776ac69b309e9c9
Richard Klees <richard.klees@concepts-and-training.de>
2018-02-28 16:42:34 +0100

    TMS/CourseCreation: slight refactoring of access extension.

--- src/TMS/CourseCreation/CourseAccessExtension.php
+++ src/TMS/CourseCreation/CourseAccessExtension.php
@@ -12,23 +12,26 @@ require_once("Services/Component/classes/class.ilPluginAdmin.php");
 trait CourseAccessExtension {
 	static function _checkAccessExtension($a_cmd, $a_permission, $a_ref_id, $a_obj_id, $a_user_id) {
 		if ($a_cmd === "create_course_from_template") {
-			// TODO: implement me properly
-			if (!\ilPluginAdmin::isPluginActive("xccr") || !\ilPluginAdmin::isPluginActive("xcps")) {
-				return false;
-			}
-			if (!self::_isTemplateCourse($a_ref_id)) {
-				return false;
-			}
-			global $DIC;
-			$access = $DIC->access();
-			if (!$access->checkAccessOfUser($a_user_id, "copy", "initTargetSelection", $a_ref_id, "crs")) {
-				return false;
-			}
-			if (!self::_userCanSeeCopySettingsObject($a_user_id, $a_ref_id)) {
-				return false;
-			}
-			return true;
+			return self::_mayUserCreateCourseFromTemplate($a_ref_id, $a_user_id);
 		}
+	}
+
+	static function _mayUserCreateCourseFromTemplate($a_ref_id, $a_user_id) {
+		if (!\ilPluginAdmin::isPluginActive("xccr") || !\ilPluginAdmin::isPluginActive("xcps")) {
+			return false;
+		}
+		if (!self::_isTemplateCourse($a_ref_id)) {
+			return false;
+		}
+		global $DIC;
+		$access = $DIC->access();
+		if (!$access->checkAccessOfUser($a_user_id, "copy", "initTargetSelection", $a_ref_id, "crs")) {
+			return false;
+		}
+		if (!self::_userCanSeeCopySettingsObject($a_user_id, $a_ref_id)) {
+			return false;
+		}
+		return true;
 	}
 
 	static function _isTemplateCourse($ref_id) {
