5972f3583df235ce78563f134194f0fb9733474b
Richard Klees <richard.klees@concepts-and-training.de>
2018-03-01 15:19:24 +0100

    TMS/CourseCreation: more helpers for creation of links to course creation gui

--- src/TMS/CourseCreation/LinkHelper.php
+++ src/TMS/CourseCreation/LinkHelper.php
@@ -26,13 +26,17 @@ trait LinkHelper {
 	}
 
 	/**
+	 * @param	int	$parent_ref_id
+	 * @param	int|string $template_ref_id
 	 * @return	string
 	 */
-	protected function getCreateCourseCommandLink() {
+	protected function getCreateCourseCommandLink($parent_ref_id, $template_ref_id, $async = false) {
+		assert('is_int($parent_ref_id)');
+		assert('is_int($template_ref_id) || is_string($template_ref_id)');
 		$ctrl = $this->getCtrl();
-		$ctrl->setParameterByClass("ilCourseCreationGUI", "parent_ref_id", $this->parent_ref_id);
-		$ctrl->setParameterByClass("ilCourseCreationGUI", "ref_id", $this->ref_id);
-		return $ctrl->getLinkTargetByClass(["ilRepositoryGUI", "ilCourseCreationGUI"], $this->getCreateCourseCommand());
+		$ctrl->setParameterByClass("ilCourseCreationGUI", "parent_ref_id", $parent_ref_id);
+		$ctrl->setParameterByClass("ilCourseCreationGUI", "ref_id", $template_ref_id);
+		return $ctrl->getLinkTargetByClass(["ilRepositoryGUI", "ilCourseCreationGUI"], $this->getCreateCourseCommand(), "", $async);
 	}
 
 	/**
@@ -40,7 +44,7 @@ trait LinkHelper {
 	 */
 	protected function getCreateCourseCommandLngVar() {
 		$lng = $this->getLng();
-		$this->lng->loadLanguageModule("tms");
+		$lng->loadLanguageModule("tms");
 		return "create_course_from_template";
 	}
 
@@ -49,5 +53,78 @@ trait LinkHelper {
 	 */
 	protected function getCreateCourseCommandLabel() {
 		return $this->getLng()->txt($this->getCreateCourseCommandLngVar());
+	}
+
+	/**
+	 * @param	array<string,CourseTemplateInfo[]>	$info
+	 * @param	string	$select_name
+	 * @return	\ilGroupableSelectInputGUI
+	 */
+	protected function getGroupableSelectInputGUIForCourseTemplates(array $info, $select_name) {
+		assert('is_string($select_name)');
+
+		ksort($info, SORT_NATURAL);
+		foreach ($info as $k => $is) {
+			$group = [];
+			foreach($is as $i) {
+				$group[$i->getRefId()] = $i->getTitle();
+			}
+			asort($group, SORT_NATURAL);
+			$info[$k] = $group;
+		}
+
+		require_once("Services/Form/classes/class.ilGroupableSelectInputGUI.php");
+		$select = new \ilGroupableSelectInputGUI("", $select_name);
+		$select->setGroups($info);
+		return $select;
+	}
+
+	/**
+	 * @return	ILIAS\UI\Component\Modal\Modal
+	 */
+	protected function getCourseTemplateSelectionModal(\ILIAS\UI\Factory $ui_factory, array $info, $parent_ref_id) {
+		assert('is_int($parent_ref_id)');
+		$placeholder = "_REF_ID_";
+		$link = $this->getCreateCourseCommandLink($parent_ref_id, $placeholder, true);
+		$select_name = "course_template_select";
+
+		$next_button = $ui_factory->button()
+			->standard(
+				ucfirst($this->g_lng->txt("next")),
+				""
+			)
+			->withAdditionalOnLoadCode(function($id) use ($link, $select_name, $placeholder) {
+				return "$('#$id').on('click', function(ev) {
+					var link = '$link';
+					var ref_id = $('select[name^=$select_name]').val();
+					link = link.replace('$placeholder', ref_id);
+					window.location.href = link;
+					ev.preventDefault();
+				});";
+			});
+
+		$select = $this->getGroupableSelectInputGUIForCourseTemplates($info, $select_name);
+
+		return $ui_factory->modal()
+			->roundtrip(
+				$this->getLng()->txt("choose_course_template"),
+				$ui_factory->legacy($select->render())
+			)
+			->withActionButtons([$next_button]);
+	}
+
+	/**
+	 * @return void
+	 */
+	protected function addCourseTemplateSelectionModalToToolbar(\ILIAS\UI\Factory $ui_factory, \ILIAS\UI\Renderer $ui_renderer, \ilToolbarGUI $toolbar, array $info, $parent_ref_id) {
+		assert('is_int($parent_ref_id)');
+		$modal = $this->getCourseTemplateSelectionModal($ui_factory, $info, $parent_ref_id);
+		$button = $ui_factory->button()
+			->primary(
+				$this->getCreateCourseCommandLabel(),
+				""
+			)->withOnClick($modal->getShowSignal());
+
+		$toolbar->addText($ui_renderer->render([$button, $modal]));
 	}
 }
