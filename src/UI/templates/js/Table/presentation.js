37fee28c8e2a13b7752613b56bf9772b880aa37f
Nils Haagen <nhaagen@cat06.de>
2018-03-07 11:52:06 +0100

    separate PRs

--- src/UI/templates/js/Table/presentation.js
+++ src/UI/templates/js/Table/presentation.js
@@ -4,44 +4,38 @@ il.UI.table = il.UI.table || {};
 
 (function($, UI) {
 
-	UI.table.presentation = (function ($) {
-		_cls_collapsed = 'collapsed';
-		_cls_expanded = 'expanded';
-		var expandRow = function (id) {
-			var row = $('#' + id);
-			row.find('.il-table-presentation-row-controls-expander').hide();
-			row.find('.il-table-presentation-row-controls-collapser').show();
-			row.find('.il-table-presentation-row-expanded').show();
-			row.find('.il-table-presentation-row-header-fields').hide();
-			row.removeClass(_cls_collapsed);
-			row.addClass(_cls_expanded);
-		};
+    UI.table.presentation = (function ($) {
 
-		var collapseRow = function (id) {
-			var row = $('#' + id);
-			row.find('.il-table-presentation-row-controls-expander').show();
-			row.find('.il-table-presentation-row-controls-collapser').hide();
-			row.find('.il-table-presentation-row-expanded').hide();
-			row.find('.il-table-presentation-row-header-fields').show();
-			row.removeClass(_cls_expanded);
-			row.addClass(_cls_collapsed);
-		};
+        var expandRow = function (id) {
+            var row = $('#' + id);
+            row.find('.il-table-presentation-row-controls-expander').hide();
+            row.find('.il-table-presentation-row-controls-collapser').show();
+            row.find('.il-table-presentation-row-expanded').show();
+            row.find('.il-table-presentation-row-header-fields').hide();
+        };
 
-		var toggleRow = function (id) {
-			var row = $('#' + id);
-			row.find('.il-table-presentation-row-controls-expander').toggle();
-			row.find('.il-table-presentation-row-controls-collapser').toggle();
-			row.find('.il-table-presentation-row-expanded').toggle();
-			row.find('.il-table-presentation-row-header-fields').toggle();
+        var collapseRow = function (id) {
+            var row = $('#' + id);
+            row.find('.il-table-presentation-row-controls-expander').show();
+            row.find('.il-table-presentation-row-controls-collapser').hide();
+            row.find('.il-table-presentation-row-expanded').hide();
+            row.find('.il-table-presentation-row-header-fields').show();
+        };
 
-		};
+        var toggleRow = function (id) {
+            var row = $('#' + id);
+            row.find('.il-table-presentation-row-controls-expander').toggle();
+            row.find('.il-table-presentation-row-controls-collapser').toggle();
+            row.find('.il-table-presentation-row-expanded').toggle();
+            row.find('.il-table-presentation-row-header-fields').toggle();
+        };
 
-		return {
-			expandRow: expandRow,
-			collapseRow: collapseRow,
-			toggleRow: toggleRow
-		};
+        return {
+            expandRow: expandRow,
+            collapseRow: collapseRow,
+            toggleRow: toggleRow
+        };
 
-	})($);
+    })($);
 
 })($, il.UI);
