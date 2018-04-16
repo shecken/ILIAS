7536a540af40d837ea9ee223814a20452f3a990f
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2018-04-16 15:21:16 +0200

    fixing dateperiod

--- Services/TMS/Filter/classes/class.catFilterDatePeriodGUI.php
+++ Services/TMS/Filter/classes/class.catFilterDatePeriodGUI.php
@@ -21,19 +21,15 @@ class catFilterDatePeriodGUI extends catFilterGUI {
 	public function formElement() {
 		$duration = new ilDateDurationInputGUI($this->filter->label(), "filter[$this->path]");
 		$duration->setInfo($this->filter->description());
-		$duration->setShowDate(true);
 		$duration->setShowTime(false);
 		$duration->setStartText($this->gLng->txt("gev_filter_period_from"));
 		$duration->setEndText($this->gLng->txt("gev_filter_period_to"));
 		
 		if($this->val) {
-			$start_date = $this->val["start"]["date"]["y"]."-".str_pad($this->val["start"]["date"]["m"], 2, "0", STR_PAD_LEFT)
-							."-".str_pad($this->val["start"]["date"]["d"], 2, "0", STR_PAD_LEFT)." 00:00:00";
-			$end_date = $this->val["end"]["date"]["y"]."-".str_pad($this->val["end"]["date"]["m"], 2, "0", STR_PAD_LEFT)
-							."-".str_pad($this->val["end"]["date"]["d"], 2, "0", STR_PAD_LEFT)." 00:00:00";
-
-			$duration->setStart(new ilDateTime($start_date, IL_CAL_DATETIME));
-			$duration->setEnd(new ilDateTime($end_date, IL_CAL_DATETIME));
+			$start_date = \DateTime::createFromFormat('d.m.Y',$this->val["start"])->format('Y-m-d');
+			$end_date = \DateTime::createFromFormat('d.m.Y',$this->val["end"])->format('Y-m-d');
+			$duration->setStart(new ilDateTime($start_date, IL_CAL_DATE));
+			$duration->setEnd(new ilDateTime($end_date, IL_CAL_DATE));
 		} else {
 			$duration->setStart(new ilDateTime($this->filter->default_begin()->format("Y-m-d 00:00:00"),IL_CAL_DATETIME));
 			$duration->setEnd(new ilDateTime($this->filter->default_end()->format("Y-m-d 00:00:00"),IL_CAL_DATETIME));
