44913717b52b1bfe04ea3ca164b4b1fc1c98d55e
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2018-10-05 16:15:22 +0200

    Redirect to dialog login if http signs to dialog

--- Services/Init/classes/class.ilStartUpGUI.php
+++ Services/Init/classes/class.ilStartUpGUI.php
@@ -89,7 +89,6 @@ class ilStartUpGUI
 		{
 			$status = $_GET["auth_stat"];
 		}
-		
 		if($ilAuth->getAuth() && !$status)
 		{			
 			// deprecated?
@@ -99,7 +98,7 @@ class ilStartUpGUI
 			}			
 			$this->processStartingPage();
 		}
-		
+
 		// if authentication of soap user failed, but email address is
 		// known, show users and ask for password
 		if ($status == AUTH_SOAP_NO_ILIAS_USER_BUT_EMAIL)
@@ -1159,7 +1158,13 @@ class ilStartUpGUI
 		$client_id = $_COOKIE["ilClientId"];
 		ilUtil::setCookie("ilClientId","");
 
+		// cat-patch start
+		$url = $_SERVER["HTTP_REFERER"];
+		if (strpos($url, ILIAS_HTTP_PATH) !== false) {
+			ilUtil::redirect("dialog_login.php");
+		}
 		ilUtil::redirect("ilias.php");
+		// cat-patch end
 
 		//$this->showLogin();
 
