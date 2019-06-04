8dd6133dcc2a48f1ffb4f74c5e2adfd4d12a5a49
Nils Haagen <nhaagen@cat06.de>
2019-06-04 17:44:00 +0200

    add and apply filters
    count with applied filter
    
    auto-content GUI
    
    monitored category data-type
    
    gui for listing and editing
    
    update db
    store/delete/edit auto-category

---
+++ Modules/StudyProgramme/classes/class.ilObjStudyProgrammeAutoCategoriesGUI.php
@@ -0,0 +1,281 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Class ilObjStudyProgrammeAutoCategoriesGUI
+ *
+ * @author: Nils Haagen <nils.haagen@concepts-and-training.de>
+ */
+class ilObjStudyProgrammeAutoCategoriesGUI
+{
+	const F_TITLE = 'f_t';
+	const F_CATEGORY_REF = 'f_cr';
+	const CHECKBOX_CATEGORY_REF_IDS = 'c_catids';
+
+	/**
+	 * @var ilTemplate
+	 */
+	public $tpl;
+
+	/**
+	 * @var ilCtrl
+	 */
+	public $ctrl;
+
+	/**
+	 * @var ilToolbarGUI
+	 */
+	public $toolbar;
+
+	/**
+	 * @var ilLng
+	 */
+	public $lng;
+
+	/**
+	 * @var int | null
+	 */
+	public $prg_ref_id;
+
+	/**
+	 * @var ilObjStudyProgramme | null
+	 */
+	public $object;
+
+	/**
+	 * @var ILIAS\UI\Factory
+	 */
+	public $ui_factory;
+
+	/**
+	 * @var ILIAS\UI\Renderer
+	 */
+	public $ui_renderer;
+
+	/**
+	 * @var Psr\Http\Message\ServerRequestInterface
+	 */
+	protected $request;
+
+
+	public function __construct(
+		ilTemplate $tpl,
+		ilCtrl $ilCtrl,
+		ilToolbarGUI $ilToolbar,
+		ilLanguage $lng,
+		\ILIAS\UI\Factory $ui_factory,
+		\ILIAS\UI\Renderer $ui_renderer,
+		\GuzzleHttp\Psr7\ServerRequest $request
+	) {
+		$this->tpl = $tpl;
+		$this->ctrl = $ilCtrl;
+		$this->toolbar = $ilToolbar;
+		$this->lng = $lng;
+		$this->ui_factory = $ui_factory;
+		$this->ui_renderer = $ui_renderer;
+		$this->request = $request;
+	}
+
+	public function executeCommand()
+	{
+
+		$cmd = $this->ctrl->getCmd();
+
+		switch ($cmd) {
+			case "view":
+				$this->view();
+				break;
+
+			case "save":
+			case "delete":
+			case "delete_single":
+				$this->$cmd();
+				$this->ctrl->redirect($this, 'view');
+				break;
+
+			default:
+				throw new ilException("ilObjStudyProgrammeAutoCategoriesGUI: ".
+									  "Command not supported: $cmd");
+		}
+	}
+
+	/**
+	 * Render.
+	 */
+	protected function view()
+	{
+		$collected_modals = [];
+
+		$form = $this->getModalForm();
+		$modal = $this->getModal($form);
+		$this->getToolbar($modal->getShowSignal());
+		$collected_modals[] = $modal;
+
+
+		$table = new ilStudyProgrammeAutoCategoriesTableGUI($this, "view", "");
+		$data = [];
+		foreach($this->getObject()->getAutomaticContentCategories() as $ac) {
+			$form = $this->getModalForm($ac->getCategoryRefId());
+			$modal = $this->getModal($form);
+			$collected_modals[] = $modal;
+
+			$signal = $modal->getShowSignal();
+			$actions = $this->getItemAction(
+				$ac->getCategoryRefId(),
+				$modal->getShowSignal()
+			);
+			$data[] = [
+				$ac,
+				$this->ui_renderer->render($actions)
+			];
+		}
+		$table->setData($data);
+
+		$this->tpl->setContent(
+			$this->ui_renderer->render($collected_modals)
+			.$table->getHTML()
+		);
+	}
+
+	/**
+	 * Store data from (modal-)form.
+	 * @return string
+	 */
+	protected function save()
+	{
+		$form = $this->getModalForm()->withRequest($this->request);
+		$result = $form->getData();
+
+		$this->getObject()->storeAutomaticContentCategory(
+			(string)$result[self::F_TITLE],
+			(int)$result[self::F_CATEGORY_REF]
+		);
+	}
+
+	/**
+	 * Delete entries.
+	 */
+	protected function delete()
+	{
+		$post = $_POST;
+		$field = self::CHECKBOX_CATEGORY_REF_IDS;
+		if(array_key_exists($field, $post)) {
+			$ids = array_map('intval', $post[$field]);
+			$this->getObject()->deleteAutomaticContentCategories($ids);
+		}
+	}
+	/**
+	 * Delete single entry.
+	 */
+	protected function delete_single()
+	{
+		$get = $_GET;
+		$field = self::CHECKBOX_CATEGORY_REF_IDS;
+		if(array_key_exists($field, $get)) {
+			$ids = [(int)$get[$field]];
+			$this->getObject()->deleteAutomaticContentCategories($ids);
+		}
+	}
+
+
+	/**
+	 * Set ref-id of StudyProgramme before using this GUI.
+	 * @param int $prg_ref_id
+	 */
+	public function setRefId(int $prg_ref_id)
+	{
+		$this->prg_ref_id = $prg_ref_id;
+	}
+
+	/**
+	 * Get current StudyProgramme-object.
+	 * @return ilObjStudyProgramme
+	 */
+	protected function getObject()
+	{
+		if ($this->object === null ||
+			(int)$this->object->getRefId() !== $this->prg_ref_id
+		) {
+			$this->object = ilObjStudyProgramme::getInstanceByRefId($this->prg_ref_id);
+		}
+		return $this->object;
+	}
+
+	/**
+	 * Build a modal to add/edit a category.
+	 */
+	protected function getModal(
+		ILIAS\UI\Component\Input\Container\Form\Form $form
+	): \ILIAS\UI\Component\Modal\Modal
+	{
+		$submit_action = "";
+		$submit = $this->ui_factory->button()->primary(
+			$this->lng->txt('modal_categories_submit'),
+			$submit_action
+		);
+
+		$modal = $this->ui_factory->modal()->roundtrip(
+			$this->lng->txt('modal_categories_title'),
+			$form
+		);
+
+		return $modal;
+	}
+
+
+	/**
+	 * Build the modal's form.
+	 */
+	protected function getModalForm(int $category_ref_id = null): ILIAS\UI\Component\Input\Container\Form\Form
+	{
+		$factory = $this->ui_factory->input();
+		$url = $this->ctrl->getLinkTarget($this, "save", "", false, false);
+
+		$f_title = $factory->field()->text($this->lng->txt('title'));
+		$f_cat_ref = $factory->field()->numeric($this->lng->txt('Category'));
+		if(! is_null($category_ref_id)) {
+			$f_cat_ref = $f_cat_ref->withValue($category_ref_id);
+		}
+		$form = $factory->container()->form()->standard(
+			$url,
+			[
+				self::F_TITLE => $f_title,
+				self::F_CATEGORY_REF => $f_cat_ref
+			]
+		);
+		return $form;
+	}
+
+	/**
+	 * Setup toolbar.
+	 */
+	protected function getToolbar(\ILIAS\UI\Component\Signal $add_cat_signal)
+	{
+		$btn = $this->ui_factory->button()->primary($this->lng->txt('add_category'),'')
+			->withOnClick($add_cat_signal);
+		$this->toolbar->addComponent($btn);
+
+	}
+
+	protected function getItemAction(
+		int $cat_ref_id,
+		\ILIAS\UI\Component\Signal $signal
+	): \ILIAS\UI\Component\Dropdown\Standard {
+
+		$items = [];
+		$items[] =  $this->ui_factory->button()->shy($this->lng->txt('edit'), '')
+			->withOnClick($signal);
+
+		$this->ctrl->setParameter($this, self::CHECKBOX_CATEGORY_REF_IDS, $cat_ref_id);
+		$items[] =  $this->ui_factory->button()->shy(
+			$this->lng->txt('delete'),
+			$this->ctrl->getLinkTarget($this, 'delete_single')
+		);
+
+
+		$dd = $this->ui_factory->dropdown()->standard($items);
+		return $dd;
+
+	}
+}
