e700cbffd0c15806ed16f5ae8a41ee49f8785540
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2019-10-08 16:14:26 +0200

    included dbupdates for new prg - features

--- setup/sql/dbupdate_05.php
+++ setup/sql/dbupdate_05.php
@@ -1369,10 +1369,12 @@ $ilDB->modifyTableColumn('il_cert_template', 'version', array(
 <?php
 $ilDB->addIndex('rbac_log', ['created'], 'i2');
 ?>
+
 <#5522>
 <?php
-	$ilCtrlStructureReader->getStructure();
+$ilCtrlStructureReader->getStructure();
 ?>
+
 <#5523>
 <?php
 $q = "SELECT prg_settings.obj_id FROM prg_settings"
@@ -1400,6 +1402,7 @@ while($rec = $ilDB->fetchAssoc($res)) {
 }
 $ilDB->manipulate('UPDATE prg_settings SET lp_mode = 1 WHERE '.$ilDB->in('obj_id',$to_adjust,false,'integer'));
 ?>
+
 <#5524>
 <?php
 if(!$ilDB->tableExists('wfld_user_setting'))
@@ -2577,6 +2580,329 @@ if ($ilDB->tableColumnExists("lng_log", "identifier")) {
 }
 ?>
 <#5559>
+<?php
+global $DIC;
+$db = $DIC['ilDB'];
+if(!$db->tableColumnExists('prg_settings','deadline_period')) {
+	$db->addTableColumn(
+			'prg_settings',
+			'deadline_period',
+			[
+				'type' => 'integer',
+				'length' => 4,
+				'notnull' => true,
+				'default' => 0
+			]
+		);
+}
+if(!$db->tableColumnExists('prg_settings','deadline_date')) {
+	$db->addTableColumn(
+			'prg_settings',
+			'deadline_date',
+			[
+				'type' => 'timestamp',
+				'notnull' => false
+			]
+		);
+}
+?>
+
+<#5560>
+<?php
+global $DIC;
+$db = $DIC['ilDB'];
+if(!$db->tableColumnExists('prg_usr_progress','assignment_date')) {
+	$db->addTableColumn(
+			'prg_usr_progress',
+			'assignment_date',
+			[
+				'type' => 'timestamp',
+				'notnull' => false
+			]
+		);
+}
+?>
+
+<#5561>
+<?php
+global $DIC;
+$db = $DIC['ilDB'];
+if($db->tableColumnExists('prg_usr_progress','assignment_date') && $db->tableColumnExists('prg_usr_assignments','last_change')) {
+	$db->manipulate(
+		'UPDATE prg_usr_progress'
+		.'	JOIN prg_usr_assignments'
+		.'		ON prg_usr_assignments.id = prg_usr_progress.assignment_id'
+		.'	SET prg_usr_progress.assignment_date = prg_usr_assignments.last_change'
+	);
+}
+?>
+
+<#5562>
+<?php
+global $DIC;
+$db = $DIC['ilDB'];
+if(!$db->tableColumnExists('prg_usr_progress','completion_date')) {
+	$db->addTableColumn(
+			'prg_usr_progress',
+			'completion_date',
+			[
+				'type' => 'timestamp',
+				'notnull' => false
+			]
+		);
+}
+?>
+
+<#5563>
+<?php
+global $DIC;
+$db = $DIC['ilDB'];
+if(!$db->tableColumnExists('prg_settings','vq_period')) {
+	$db->addTableColumn(
+			'prg_settings',
+			'vq_period',
+			[
+				'type' => 'integer',
+				'length' => 4,
+				'notnull' => true,
+				'default' => -1
+			]
+		);
+}
+if(!$db->tableColumnExists('prg_settings','vq_date')) {
+	$db->addTableColumn(
+			'prg_settings',
+			'vq_date',
+			[
+				'type' => 'timestamp',
+				'notnull' => false
+			]
+		);
+}
+if(!$db->tableColumnExists('prg_settings','vq_restart_period')) {
+	$db->addTableColumn(
+			'prg_settings',
+			'vq_restart_period',
+			[
+				'type' => 'integer',
+				'length' => 4,
+				'notnull' => true,
+				'default' => -1
+			]
+		);
+}
+?>
+
+<#5564>
+<?php
+global $DIC;
+$db = $DIC['ilDB'];
+if(!$db->tableColumnExists('prg_usr_progress','vq_date')) {
+	$db->addTableColumn(
+			'prg_usr_progress',
+			'vq_date',
+			[
+				'type' => 'timestamp',
+				'notnull' => false
+			]
+		);
+}
+?>
+
+<#5565>
+<?php
+global $DIC;
+$db = $DIC['ilDB'];
+if(!$db->tableColumnExists('prg_usr_assignments','restart_date')) {
+	$db->addTableColumn(
+			'prg_usr_assignments',
+			'restart_date',
+			[
+				'type' => 'timestamp',
+				'notnull' => false
+			]
+		);
+}
+?>
+
+<#5566>
+<?php
+global $DIC;
+$db = $DIC['ilDB'];
+if(!$db->tableColumnExists('prg_usr_assignments','restarted_assignment_id')) {
+	$db->addTableColumn(
+			'prg_usr_assignments',
+			'restarted_assignment_id',
+			[
+				'type' => 'integer',
+				'notnull' => true,
+				'default' => -1
+			]
+		);
+}
+?>
+
+<#5567>
+<?php
+if(!$ilDB->tableExists('prg_auto_content')) {
+	$ilDB->createTable('prg_auto_content', array(
+		'prg_obj_id' => array(
+			'type' => 'integer',
+			'length' => 4,
+			'notnull' => true
+		),
+		'cat_ref_id' => array(
+			'type' => 'integer',
+			'length' => 4,
+			'notnull' => true
+		),
+		'last_usr_id' => array(
+			'type' => 'integer',
+			'length' => 4,
+			'notnull' => true
+		),
+		'last_edited' => array(
+			'type' => 'timestamp',
+			'notnull' => false
+		)
+	));
+	$ilDB->addPrimaryKey('prg_auto_content', ['prg_obj_id', 'cat_ref_id']);
+}
+?>
+
+<#5568>
+<?php
+require_once './Services/Migration/DBUpdate_3560/classes/class.ilDBUpdateNewObjectType.php';
+
+$type_id  = ilDBUpdateNewObjectType::addNewType('prgr', 'Study Programme Reference');
+
+ilDBUpdateNewObjectType::addRBACOperations($type_id, [
+	ilDBUpdateNewObjectType::RBAC_OP_EDIT_PERMISSIONS,
+	ilDBUpdateNewObjectType::RBAC_OP_VISIBLE,
+	ilDBUpdateNewObjectType::RBAC_OP_READ,
+	ilDBUpdateNewObjectType::RBAC_OP_WRITE,
+	ilDBUpdateNewObjectType::RBAC_OP_DELETE,
+	ilDBUpdateNewObjectType::RBAC_OP_COPY
+]);
+
+ilDBUpdateNewObjectType::addRBACCreate('create_prgr', 'Create Study Programme Reference', [
+	'prg'
+]);
+?>
+
+<#5569>
+<?php
+if (!$ilDB->tableExists('prg_auto_membership'))
+{
+	$ilDB->createTable('prg_auto_membership', array(
+		'prg_obj_id' => array(
+			'type' => 'integer',
+			'length' => 4,
+			'notnull' => true
+		),
+		'source_type' => array(
+			'type' => 'text',
+			'length' => 8,
+			'notnull' => true
+		),
+		'source_id' => array(
+			'type' => 'integer',
+			'length' => 4,
+			'notnull' => true
+		),
+		'enabled' => array(
+			'type' => 'integer',
+			'length' => 1,
+			'notnull' => true,
+			'default' => 0
+		),
+		'last_usr_id' => array(
+			'type' => 'integer',
+			'length' => 4,
+			'notnull' => true
+		),
+		'last_edited' => array(
+			'type' => 'timestamp',
+			'notnull' => false
+		)
+	));
+	$ilDB->addPrimaryKey('prg_auto_membership', ['prg_obj_id', 'source_type', 'source_id']);
+}
+?>
+
+<#5570>
+<?php
+	ilOrgUnitOperationContextQueries::registerNewContext(ilOrgUnitOperationContext::CONTEXT_PRG, ilOrgUnitOperationContext::CONTEXT_OBJECT);
+?>
+
+<#5571>
+<?php
+ilOrgUnitOperationQueries::registerNewOperation(
+	ilOrgUnitOperation::OP_VIEW_MEMBERS,
+	'View Memberships of other users',
+	ilOrgUnitOperationContext::CONTEXT_PRG
+);
+ilOrgUnitOperationQueries::registerNewOperation(
+	ilOrgUnitOperation::OP_READ_LEARNING_PROGRESS,
+	'View learning progress of other users',
+	ilOrgUnitOperationContext::CONTEXT_PRG
+);
+ilOrgUnitOperationQueries::registerNewOperation(
+	ilOrgUnitOperation::OP_VIEW_INDIVIDUAL_PLAN,
+	'View Individual Plans of other users',
+	ilOrgUnitOperationContext::CONTEXT_PRG
+);
+ilOrgUnitOperationQueries::registerNewOperation(
+	ilOrgUnitOperation::OP_EDIT_INDIVIDUAL_PLAN,
+	'Edit Individual Plans of other users',
+	ilOrgUnitOperationContext::CONTEXT_PRG
+);
+ilOrgUnitOperationQueries::registerNewOperation(
+	ilOrgUnitOperation::OP_MANAGE_MEMBERS,
+	'Manage Memberships of other users',
+	ilOrgUnitOperationContext::CONTEXT_PRG
+);
+?>
+
+<#5572>
+<?php
+global $DIC;
+$db = $DIC['ilDB'];
+if(!$db->tableColumnExists('prg_settings','access_ctrl_org_pos')) {
+	$db->addTableColumn(
+			'prg_settings',
+			'access_ctrl_org_pos',
+			[
+				'type' => 'integer',
+				'length' => 1,
+				'notnull' => true,
+				'default' => 0
+			]
+		);
+}
+?>
+
+<#5573>
+<?php
+global $DIC;
+$db = $DIC['ilDB'];
+if(!$db->tableColumnExists('prg_usr_progress','invalidated')) {
+	$db->addTableColumn(
+			'prg_usr_progress',
+			'invalidated',
+			[
+				'type' => 'integer',
+				'length' => 1,
+				'notnull' => false
+			]
+		);
+}
+?>
+
+<#5540>
+<?php
+$ilCtrlStructureReader->getStructure();
+?><#5559>
 <?php
 	$ilCtrlStructureReader->getStructure();
 ?>
